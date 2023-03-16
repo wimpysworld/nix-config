a8c919eef68b86e2341de1fcc73b2627daa589ce
Martin Wimpress <code@flexion.org>
2023-03-16 11:51:02 +0000

    chore: refactor flatpak.sh to coerce the theme used in flatpak apps
    I'm not actually sure if this script is useful yet.

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
 
-flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# Best effort to set the GTK theme for Flatpak apps
 
 case "${XDG_CURRENT_DESKTOP}" in
-  Pantheon)
-    flatpak override --user --env=GTK_THEME=io.elementary.stylesheet.bubblegum:dark
-    flatpak remote-add --user --if-not-exists appcenter  https://flatpak.elementary.io/appcenter.flatpakrepo
-    #flatpak remote-add --user --if-not-exists elementary https://flatpak.elementary.io/elementary.flatpakrepo
+  Cinnamon|GNOME|Pantheon)
+    COLOR_SCHEME=$(dconf read /org/gnome/desktop/interface/color-scheme | sed -e "s/'//g")
+    GTK_THEME=$(dconf read /org/gnome/desktop/interface/gtk-theme | sed -e "s/'//g")
+    ICON_THEME=$(dconf read /org/gnome/desktop/interface/icon-theme | sed -e "s/'//g")
+    XCURSOR_THEME=$(dconf read /org/gnome/desktop/interface/cursor-theme | sed -e "s/'//g")
+    if [ "${COLOR_SCHEME}" == "prefer-dark" ]; then
+      GTK_THEME="${GTK_THEME}:dark"
+    fi
     ;;
   MATE)
-    flatpak override --user --env=GTK_THEME=Yaru-magenta-dark
+    GTK_THEME=$(dconf read /org/mate/desktop/interface/gtk-theme | sed -e "s/'//g")
+    ICON_THEME=$(dconf read /org/mate/desktop/interface/icon-theme | sed -e "s/'//g")
+    XCURSOR_THEME=$(dconf read /org/mate/desktop/peripherals/mouse/cursor-theme | sed -e "s/'//g")
+    ;;
+  *)
+    GTK_THEME="Adwaita"
+    ICON_THEME="Adwaita"
+    XCURSOR_THEME="Adwaita"
     ;;
 esac
+
+flatpak override --user --env=GTK_THEME="${GTK_THEME}"
+flatpak override --user --env=ICON_THEME="${ICON_THEME}"
+flatpak override --user --env=XCURSOR_THEME="${XCURSOR_THEME}"
