78757312d642d4fecb67fc7f119b12dca4d577e1
Martin Wimpress <code@flexion.org>
2023-03-22 16:56:02 +0000

    feat: move some fish aliases to abbreviations
    Abbreviations auto expand when run, so good to see what's really happening when they are executed.

--- home-manager/_mixins/console/fish.nix
+++ home-manager/_mixins/console/fish.nix
@@ -2,6 +2,16 @@
   programs = {
     fish = {
       enable = true;
+      shellAbbrs = {
+        mkhostid = "head -c4 /dev/urandom | od -A none -t x4";
+        # https://github.com/NixOS/nixpkgs/issues/191128#issuecomment-1246030417
+        nix-hash-sha256 = "nix-hash --flat --base32 --type sha256";
+        nix-gc = "sudo nix-collect-garbage --delete-older-than 14d";
+        rebuild-home = "home-manager switch -b backup --flake $HOME/Zero/nix-config";
+        rebuild-host = "sudo nixos-rebuild switch --flake $HOME/Zero/nix-config";
+        rebuild-lock = "pushd $HOME/Zero/nix-config && nix flake lock --recreate-lock-file && popd";
+        rebuild-iso = "pushd $HOME/Zero/nix-config && nix build .#nixosConfigurations.iso.config.system.build.isoImage && popd";
+      };
       shellAliases = {
         cat = "bat";
         diff = "diffr";
@@ -9,9 +19,6 @@
         ipb = "ip --color --brief";
         less = "bat";
         man = "env PAGER=most man";
-        mkhostid = "head -c4 /dev/urandom | od -A none -t x4"
-        # https://github.com/NixOS/nixpkgs/issues/191128#issuecomment-1246030417
-        nix-hash-sha256 = "nix-hash --flat --base32 --type sha256";
         open = "xdg-open";
         pubip = "curl -s ifconfig.me/ip";
         #pubip = "curl -s https://api.ipify.org";
@@ -20,11 +27,6 @@
         moon = "curl -s wttr.in/Moon";
         wttr = "curl -scurl -s wttr.in && curl -s v2.wttr.in";
         wttr-bas = "curl -s wttr.in/basingstoke && curl -s v2.wttr.in/basingstoke";
-        nix-gc = "sudo nix-collect-garbage --delete-older-than 14d";
-        rebuild-home = "home-manager switch -b backup --flake $HOME/Zero/nix-config";
-        rebuild-host = "sudo nixos-rebuild switch --flake $HOME/Zero/nix-config";
-        rebuild-lock = "pushd $HOME/Zero/nix-config && nix flake lock --recreate-lock-file && popd";
-        rebuild-iso = "pushd $HOME/Zero/nix-config && nix build .#nixosConfigurations.iso.config.system.build.isoImage && popd";
       };
     };
   };
