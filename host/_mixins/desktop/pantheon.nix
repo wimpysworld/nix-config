e0a129a4f5a3b188b0c65e515e70980e5f125956
Martin Wimpress <code@flexion.org>
2023-03-22 01:40:53 +0000

    chore: switch to GTK greeter

--- host/_mixins/desktop/pantheon.nix
+++ host/_mixins/desktop/pantheon.nix
@@ -37,67 +37,43 @@
       enable = true;
       displayManager = {
         lightdm.enable = true;
-        # Use Slick greeter as the Pantheon greeter is not working
+        # Use GTK greeter as the Pantheon greeter is not working
         lightdm.greeters.pantheon.enable = false;
-        lightdm.greeters.slick = {
+        lightdm.greeters.gtk = {
           enable = true;
-          draw-user-backgrounds = false;
           cursorTheme.name = "elementary";
-          cursorTheme.size = 32;
           cursorTheme.package = pkgs.pantheon.elementary-icon-theme;
-          font.name = "Work Sans 12";
-          font.package = pkgs.work-sans;
-          iconTheme.package = pkgs.pantheon.elementary-icon-theme;
-          iconTheme.name = "elementary";
-          theme.package = pkgs.pantheon.elementary-gtk-theme;
-          theme.name = "io.elementary.stylesheet.bubblegum";
+          cursorTheme.size = 32;
+          iconTheme.name = "Yaru-magenta-dark";
+          iconTheme.package = pkgs.yaru-theme;
+          theme.name = "Yaru-magenta-dark";
+          theme.package = pkgs.yaru-theme;
+          indicators = [
+            "~session"
+            "~host"
+            "~spacer"
+            "~clock"
+            "~spacer"
+            "~a11y"
+            "~power"
+          ];
+          # https://github.com/Xubuntu/lightdm-gtk-greeter/blob/master/data/lightdm-gtk-greeter.conf
           extraConfig = ''
-# activate-numlock=Whether to activate numlock. This features requires the installation of numlockx. (true or false)
-activate-numlock=false
-# background=Background file to use, either an image path or a color (e.g. #772953)
-# background-color=Background color (e.g. #772953), set before wallpaper is seen
-# draw-user-backgrounds=Whether to draw user backgrounds (true or false)
-# draw-grid=Whether to draw an overlay grid (true or false)
-draw-grid=true
-# show-hostname=Whether to show the hostname in the menubar (true or false)
-show-hostname=true
-# show-power=Whether to show the power indicator in the menubar (true or false)
-show-power=true
-# show-a11y=Whether to show the accessibility options in the menubar (true or false)
-show-all1=true
-# show-keyboard=Whether to show the keyboard indicator in the menubar (true or false)
-show-keyboard=true
-# show-clock=Whether to show the clock in the menubar (true or false)
-show-clock=true
-# show-quit=Whether to show the quit menu in the menubar (true or false)
-show-quit=true
-# logo=Logo file to use
-# other-monitors-logo=Logo file to use for other monitors
-# xft-antialias=Whether to antialias Xft fonts (true or false)
-xft-antialias=true
-# xft-dpi=Resolution for Xft in dots per inch
-xft-dpi=96
-# xft-hintstyle=What degree of hinting to use (hintnone/hintslight/hintmedium/hintfull)
-xft-hintstyle=hintslight
-# xft-rgba=Type of subpixel antialiasing (none/rgb/bgr/vrgb/vbgr)
-xft-rgba=rgb
-# onscreen-keyboard=Whether to enable the onscreen keyboard (true or false)
-onscreen-keyboard=false
-# high-contrast=Whether to use a high contrast theme (true or false)
-high-contrast=false
-# screen-reader=Whether to enable the screen reader (true or false)
-screen-reader=true
-# play-ready-sound=A sound file to play when the greeter is ready
-# hidden-users=List of usernames (separated by semicolons) that are hidden until Ctr+Alt+Shift is pressed
-# group-filter=List of groups that users must be part of to be shown (empty list shows all users)
-# enable-hidpi=Whether to enable HiDPI support (on/off/auto)
-enable-hidpi=auto
-# only-on-monitor=Sets the monitor on which to show the login window, -1 means "follow the mouse"
-only-on-monitor=-1
-# stretch-background-across-monitors=Whether to stretch the background across multiple monitors (false by default)
-stretch-background-across-monitors=false
-# clock-format=What clock format to use (e.g., %H:%M or %l:%M %p)
-clock-format=%H:%M
+# background = Background file to use, either an image path or a color (e.g. #772953)
+font-name = Work Sans 12
+xft-antialias = true
+xft-dpi = 96
+xft-hintstyle = slight
+xft-rgba = rgb
+
+active-monitor = #cursor
+# position = x y ("50% 50%" by default)  Login window position
+# default-user-image = Image used as default user icon, path or #icon-name
+hide-user-image = false
+round-user-image = false
+highlight-logged-user = true
+panel-position = top
+clock-format = %a, %b %d  %H:%M
           '';
         };
       };
