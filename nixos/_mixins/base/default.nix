8d85449a6e623340ecfd7149e54053bb6ac25ea1
Martin Wimpress <martin@wimpress.org>
2023-06-26 10:36:02 +0100

    feat: only enable documentation I use

--- nixos/_mixins/base/default.nix
+++ nixos/_mixins/base/default.nix
@@ -8,6 +8,13 @@
     ../services/zerotier.nix
   ];
 
+  # don't install documentation i don't use
+  documentation.enable = true; # documentation of packages
+  documentation.nixos.enable = false; # nixos documentation
+  documentation.man.enable = true; # manual pages and the man command
+  documentation.info.enable = false; # info pages and the info command
+  documentation.doc.enable = false; # documentation distributed in packages' /share/doc
+
   environment.systemPackages = with pkgs; [
     binutils
     curl
