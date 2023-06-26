4d17cd4f40b5489798b66a736fc03e0e5481af75
Martin Wimpress <martin@wimpress.org>
2023-06-26 10:39:03 +0100

    chore: remove GTK greeter configuration from Pantheon

--- nixos/_mixins/desktop/pantheon.nix
+++ nixos/_mixins/desktop/pantheon.nix
@@ -43,45 +43,7 @@
       enable = true;
       displayManager = {
         lightdm.enable = true;
-        # Use GTK greeter as the Pantheon greeter is not working
         lightdm.greeters.pantheon.enable = true;
-#         lightdm.greeters.gtk = {
-#           enable = true;
-#           cursorTheme.name = "elementary";
-#           cursorTheme.package = pkgs.pantheon.elementary-icon-theme;
-#           cursorTheme.size = 32;
-#           iconTheme.name = "Yaru-magenta-dark";
-#           iconTheme.package = pkgs.yaru-theme;
-#           theme.name = "Yaru-magenta-dark";
-#           theme.package = pkgs.yaru-theme;
-#           indicators = [
-#             "~session"
-#             "~host"
-#             "~spacer"
-#             "~clock"
-#             "~spacer"
-#             "~a11y"
-#             "~power"
-#           ];
-#           # https://github.com/Xubuntu/lightdm-gtk-greeter/blob/master/data/lightdm-gtk-greeter.conf
-#           extraConfig = ''
-# # background = Background file to use, either an image path or a color (e.g. #772953)
-# font-name = Work Sans 12
-# xft-antialias = true
-# xft-dpi = 96
-# xft-hintstyle = slight
-# xft-rgba = rgb
-
-# active-monitor = #cursor
-# # position = x y ("50% 50%" by default)  Login window position
-# # default-user-image = Image used as default user icon, path or #icon-name
-# hide-user-image = false
-# round-user-image = false
-# highlight-logged-user = true
-# panel-position = top
-# clock-format = %a, %b %d  %H:%M
-#           '';
-#         };
       };
 
       desktopManager = {
