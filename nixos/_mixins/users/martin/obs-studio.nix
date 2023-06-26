bb1fa66803237e8eb6d2d1e1f185a89558c691cd
Martin Wimpress <martin@wimpress.org>
2023-06-26 18:16:22 +0100

    fix: correctly wrap OBS so the plugins are available

--- nixos/_mixins/users/martin/obs-studio.nix
+++ nixos/_mixins/users/martin/obs-studio.nix
@@ -1,25 +1,29 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs.unstable; [
-    obs-studio
-    obs-studio-plugins.obs-3d-effect
-    obs-studio-plugins.obs-command-source
-    obs-studio-plugins.obs-gradient-source
-    obs-studio-plugins.obs-gstreamer
-    obs-studio-plugins.obs-nvfbc
-    obs-studio-plugins.obs-move-transition
-    obs-studio-plugins.obs-mute-filter
-    obs-studio-plugins.obs-pipewire-audio-capture
-    #obs-studio-plugins.obs-rgb-levels-filter
-    obs-studio-plugins.obs-text-pthread
-    obs-studio-plugins.obs-scale-to-sound
-    obs-studio-plugins.advanced-scene-switcher
-    obs-studio-plugins.obs-shaderfilter
-    obs-studio-plugins.obs-source-clone
-    obs-studio-plugins.obs-source-record
-    obs-studio-plugins.obs-source-switcher
-    obs-studio-plugins.obs-transition-table
-    obs-studio-plugins.obs-vaapi
-    obs-studio-plugins.obs-vintage-filter
-    obs-studio-plugins.obs-websocket
+  # https://nixos.wiki/wiki/OBS_Studio
+  environment.systemPackages = [
+    (pkgs.wrapOBS {
+      plugins = with pkgs.unstable.obs-studio-plugins; [
+        obs-3d-effect
+        obs-command-source
+        obs-gradient-source
+        obs-gstreamer
+        obs-nvfbc
+        obs-move-transition
+        obs-mute-filter
+        obs-pipewire-audio-capture
+        #obs-rgb-levels-filter
+        obs-text-pthread
+        obs-scale-to-sound
+        advanced-scene-switcher
+        obs-shaderfilter
+        obs-source-clone
+        obs-source-record
+        obs-source-switcher
+        obs-transition-table
+        obs-vaapi
+        obs-vintage-filter
+        obs-websocket
+      ];
+    })
   ];
 }
