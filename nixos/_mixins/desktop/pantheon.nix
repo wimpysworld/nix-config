f3375a6bac56b9c093f35516ca0c32a78d58b5a7
Martin Wimpress <martin@wimpress.org>
2023-06-26 10:41:39 +0100

    fix: Enable Indicator Application Service in Pantheon

--- nixos/_mixins/desktop/pantheon.nix
+++ nixos/_mixins/desktop/pantheon.nix
@@ -11,7 +11,10 @@
       elementary-videos
     ];
 
-    # Add some elementary additional apps and include Yaru for syntax highlighting
+    # App indicator
+    # - https://github.com/NixOS/nixpkgs/issues/144045#issuecomment-992487775
+    pathsToLink = [ "/libexec" ];
+
     # Add additional apps and include Yaru for syntax highlighting
     systemPackages = with pkgs; [
       appeditor                   # elementary OS menu editor
@@ -56,6 +59,17 @@
           ];
         };
       };
+    };
+  };
+
+  # App indicator
+  # - https://github.com/NixOS/nixpkgs/issues/144045#issuecomment-992487775
+  systemd.user.services.indicatorapp = {
+    description = "indicator-application-gtk3";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.indicator-application-gtk3}/libexec/indicator-application/indicator-application-service";
     };
   };
 }
