2b07dc9fb4f38f83a77aa99a9517b573e28d646a
Martin Wimpress <martin@wimpress.org>
2023-06-26 15:25:24 +0100

    fix: correct format of Rhythmbox config

--- home-manager/_mixins/desktop/rhythmbox.nix
+++ home-manager/_mixins/desktop/rhythmbox.nix
@@ -3,19 +3,21 @@ with lib.hm.gvariant;
 {
   dconf.settings = {
     "org/gnome/rhythmbox/plugins" = {
-      active-plugins = ['rb', 'power-manager', 'mpris', 'iradio', 'generic-player', 'audiocd', 'android'];
+      active-plugins = [ "rb" "power-manager" "mpris" "iradio" "generic-player" "audiocd" "android" ];
     };
+    
     "org/gnome/rhythmbox/podcast" = {
-      download-interval = 'manual';
+      download-interval = "manual";
     };
+    
     "org/gnome/rhythmbox/rhythmdb" = {
-      locations = ['file:///home/martin/Studio/Music'];
+      locations = [ "file:///home/martin/Studio/Music" ];
       monitor-library = true;
     };
+    
     "org/gnome/rhythmbox/sources" = {
-      browser-views = 'genres-artists-albums';
-      visible-columns = ['post-time', 'duration', 'track-number', 'album', 'genre', 'beats-per-minute', 'play-count', 'artist'];
+      browser-views = "genres-artists-albums";
+      visible-columns = [ "post-time" "duration" "track-number" "album" "genre" "beats-per-minute" "play-count" "artist" ];
     };
   };
-
 }
