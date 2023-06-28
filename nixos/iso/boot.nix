778ed93ace96e746becd367ca83c18ecce748880
Martin Wimpress <code@flexion.org>
2023-06-28 23:52:59 +0100

    chore: update iso image hardware to be consistent with other hosts

--- nixos/iso/boot.nix
+++ nixos/iso/boot.nix
@@ -1,8 +1,27 @@
 { config, lib, pkgs, ... }: {
   boot = {
+    blacklistedKernelModules = lib.mkDefault [ ];
     consoleLogLevel = 3;
+    extraModulePackages = with config.boot.kernelPackages; [ ];
+    extraModprobeConfig = lib.mkDefault ''
+    '';
     initrd = {
+      availableKernelModules = [
+        "virtio_blk"
+        "virtio_pci"
+      ];
+      kernelModules = [ ];
       verbose = false;
+    };
+
+    kernelModules = [
+      "vhost_vsock"
+    ];
+
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv6.conf.all.forwarding" = 1;
     };
   };
 }
--- nixos/iso/hardware.nix
+++ nixos/iso/hardware.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+  imports = [
+    ../_mixins/services/pipewire.nix
+  ];
+
   console = {
     earlySetup = true;
     # Pixel sizes of the font: 12, 14, 16, 18, 20, 22, 24, 28, 32
@@ -7,4 +11,19 @@
     font = "ter-powerline-v18n";
     packages = [ pkgs.terminus_font pkgs.powerline-fonts ];
   };
+
+  environment.systemPackages = with pkgs; [ ];
+
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+  };
+
+  services = {
+    xserver.videoDrivers = [ ];
+  };
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
