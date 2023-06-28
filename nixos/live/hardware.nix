3bf952a43bef56c1d2d704d7dc767f1acc441c2c
Martin Wimpress <code@flexion.org>
2023-06-28 19:19:48 +0100

    fix: correct console font size for the VM install

--- nixos/live/hardware.nix
+++ nixos/live/hardware.nix
@@ -4,7 +4,7 @@
     earlySetup = true;
     # Pixel sizes of the font: 12, 14, 16, 18, 20, 22, 24, 28, 32
     # Followed by 'n' (normal) or 'b' (bold)
-    font = "ter-powerline-v22n";
+    font = "ter-powerline-v18n";
     packages = [ pkgs.terminus_font pkgs.powerline-fonts ];
   };
 }
