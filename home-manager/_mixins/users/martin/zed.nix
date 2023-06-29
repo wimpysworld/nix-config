299c492ab7e694ac2005821d5691238ae47738de
Martin Wimpress <code@flexion.org>
2023-06-29 18:23:48 +0100

    refactor: enable Syncthing for appropriate hosts via mixin

--- home-manager/_mixins/users/martin/zed.nix
+++ home-manager/_mixins/users/martin/zed.nix
@@ -1,12 +1,3 @@
-{ hostname, username, ... }: {
-  services.syncthing = {
-    enable = true;
-    extraOptions = [
-      "--config=/home/${username}/Syncthing/Config/${hostname}"
-      "--data=/home/${username}/Syncthing/Data/${hostname}"
-      "--home=/home/${username}/Syncthing"
-      "--gui-address=0.0.0.0:8384"
-    ];
-    tray.enable = true;
-  };
+{ ... }: {
+  imports = [ ../../services/syncthing.nix ]
 }
