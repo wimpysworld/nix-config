ae9e5843cde7ce16a1c71742cf6cce602b8d51ee
Martin Wimpress <code@flexion.org>
2023-06-29 12:00:44 +0100

    feat: add minimal and desktop/calamares iso images
    Also drops designare-headless and adds vm-mini.

--- flake.nix
+++ flake.nix
@@ -22,53 +22,44 @@
     home-manager,
     nixos-hardware,
     ... } @ inputs:
-    let
-      inherit (self) outputs;
+  let
+    inherit (self) outputs;
 
-      # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-      stateVersion = "23.05";
-      libx = import ./lib { inherit inputs outputs stateVersion; };
-    in
-    {
-      # home-manager switch -b backup --flake $HOME/Zero/nix-config
-      # nix build .#homeConfigurations."martin@ripper".activationPackage
-      homeConfigurations = {
-        # Workstations
-        "martin@designare" = libx.mkHome { hostname = "designare"; username = "martin"; desktop = "pantheon"; };
-        "martin@ripper"    = libx.mkHome { hostname = "ripper";    username = "martin"; desktop = "pantheon"; };
-        "martin@trooper"   = libx.mkHome { hostname = "trooper";   username = "martin"; desktop = "pantheon"; };
-        "martin@vm"        = libx.mkHome { hostname = "vm";        username = "martin"; desktop = "pantheon"; };
-        "martin@zed"       = libx.mkHome { hostname = "zed";       username = "martin"; desktop = "pantheon"; };
-        # Servers
-        "martin@designare-headless" = libx.mkHome { hostname = "designare"; username = "martin"; };
-        "martin@skull"              = libx.mkHome { hostname = "skull";     username = "martin"; };
-      };
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "23.05";
+    libx = import ./lib { inherit inputs outputs stateVersion; };
+  in {
+    # home-manager switch -b backup --flake $HOME/Zero/nix-config
+    # nix build .#homeConfigurations."martin@ripper".activationPackage
+    homeConfigurations = {
+      # Workstations
+      "martin@designare" = libx.mkHome { hostname = "designare"; username = "martin"; desktop = "pantheon"; };
+      "martin@ripper"    = libx.mkHome { hostname = "ripper";    username = "martin"; desktop = "pantheon"; };
+      "martin@trooper"   = libx.mkHome { hostname = "trooper";   username = "martin"; desktop = "pantheon"; };
+      "martin@vm"        = libx.mkHome { hostname = "vm";        username = "martin"; desktop = "pantheon"; };
+      "martin@zed"       = libx.mkHome { hostname = "zed";       username = "martin"; desktop = "pantheon"; };
+      # Servers
+      "martin@skull"     = libx.mkHome { hostname = "skull";     username = "martin"; };
+    };
 
-      nixosConfigurations = {
-        # nix build .#nixosConfigurations.iso.config.system.build.isoImage
-        iso = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs outputs stateVersion;
-            hostname = "iso"; username = "nixos"; desktop = "pantheon";
-          };
-          modules = [
-            (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares.nix")
-            ./nixos
-          ];
-        };
+    nixosConfigurations = {
+      # .iso images
+      # nix build .#nixosConfigurations.iso.config.system.build.isoImage
+      iso       = libx.mkHost { hostname = "iso";       username = "nixos";  desktop = "pantheon"; installer = nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares.nix"; };
+      # nix build .#nixosConfigurations.iso-mini.config.system.build.isoImage
+      iso-mini  = libx.mkHost { hostname = "iso";       username = "nixos";  installer = nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"; };
 
-        # sudo nixos-rebuild switch --flake $HOME/Zero/nix-config
-        # nix build .#nixosConfigurations.ripper.config.system.build.toplevel
-        # Workstations
-        designare = libx.mkHost { hostname = "designare"; username = "martin"; desktop = "pantheon"; };
-        ripper    = libx.mkHost { hostname = "ripper";    username = "martin"; desktop = "pantheon"; };
-        trooper   = libx.mkHost { hostname = "trooper";   username = "martin"; desktop = "pantheon"; };
-        vm        = libx.mkHost { hostname = "vm";        username = "martin"; desktop = "pantheon"; };
-        zed       = libx.mkHost { hostname = "zed";       username = "martin"; desktop = "pantheon"; };
-        # Servers
-        designare-headless = libx.mkmkHost { hostname = "designare"; username = "martin"; };
-        skull              = libx.mkmkHost { hostname = "skull";     username = "martin"; };
-      };
+      # sudo nixos-rebuild switch --flake $HOME/Zero/nix-config
+      # nix build .#nixosConfigurations.ripper.config.system.build.toplevel
+      # Workstations
+      designare = libx.mkHost { hostname = "designare"; username = "martin"; desktop = "pantheon"; };
+      ripper    = libx.mkHost { hostname = "ripper";    username = "martin"; desktop = "pantheon"; };
+      trooper   = libx.mkHost { hostname = "trooper";   username = "martin"; desktop = "pantheon"; };
+      vm        = libx.mkHost { hostname = "vm";        username = "martin"; desktop = "pantheon"; };
+      zed       = libx.mkHost { hostname = "zed";       username = "martin"; desktop = "pantheon"; };
+      # Servers
+      skull     = libx.mkHost { hostname = "skull";     username = "martin"; };
+      vm-mini   = libx.mkHost { hostname = "vm";        username = "martin"; };
     };
 
     # Devshell for bootstrapping; acessible via 'nix develop' or 'nix-shell' (legacy)
@@ -76,13 +67,14 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in import ./shell.nix { inherit pkgs; }
     );
-    
+
     # Custom packages and modifications, exported as overlays
     overlays = import ./overlays { inherit inputs; };
-    
+
     # Custom packages; acessible via 'nix build', 'nix shell', etc
     packages = libx.forAllSystems (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in import ./pkgs { inherit pkgs; }
     );
+  };
 }
