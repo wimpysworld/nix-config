5d1659e82c27697aabf24bbb6377593351f57c5c
Martin Wimpress <code@flexion.org>
2023-06-29 17:15:12 +0100

    wip: fix Syncthing configuration

--- home-manager/_mixins/users/martin/ripper.nix
+++ home-manager/_mixins/users/martin/ripper.nix
@@ -2,8 +2,8 @@
   services.syncthing = {
     enable = true;
     extraOptions = [
-      "--config=/home/${username}/Syncthing/Config/${hostname}";
-      "--data=/home/${username}/Syncthing/Data/${hostname}";
+      "--config=/home/${username}/Syncthing/Config/${hostname}"
+      "--data=/home/${username}/Syncthing/Data/${hostname}"
       "--home=/home/${username}/Syncthing"
       "--gui-address=0.0.0.0:8384"
     ];
