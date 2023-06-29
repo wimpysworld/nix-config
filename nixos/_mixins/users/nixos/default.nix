b179f215f96fb56a659337f28b05c42eb650cd43
Martin Wimpress <code@flexion.org>
2023-06-29 11:59:02 +0100

    fix: separate desktop and console configuration for iso images

--- nixos/_mixins/users/nixos/default.nix
+++ nixos/_mixins/users/nixos/default.nix
@@ -28,18 +28,5 @@ in
     shell = pkgs.fish;
   };
 
-  config.system.activationScripts.installerDesktop = let
-   homeDir = "/home/${username}/";
-   desktopDir = homeDir + "Desktop/";
-  in ''
-   mkdir -p ${desktopDir}
-   chown ${username} ${homeDir} ${desktopDir}
-   ln -sfT ${pkgs.gparted}/share/applications/gparted.desktop ${desktopDir + "gparted.desktop"}
-   ln -sfT ${pkgs.pantheon.elementary-terminal}/share/applications/io.elementary.terminal.desktop ${desktopDir + "io.elementary.terminal.desktop"}
-   ln -sfT ${pkgs.calamares-nixos}/share/applications/io.calamares.calamares.desktop ${desktopDir + "io.calamares.calamares.desktop"}
-  '';
-
-  config.isoImage.edition = lib.mkForce "${desktop}";
   config.system.stateVersion = lib.mkForce lib.trivial.release;
-  config.services.xserver.displayManager.autoLogin.user = "${username}";
 }
--- nixos/_mixins/users/nixos/desktop.nix
+++ nixos/_mixins/users/nixos/desktop.nix
@@ -1,3 +1,15 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ ];
+{ config, desktop, lib, pkgs, username, ... }: {
+  config.system.activationScripts.installerDesktop = let
+   homeDir = "/home/${username}/";
+   desktopDir = homeDir + "Desktop/";
+  in ''
+   mkdir -p ${desktopDir}
+   chown ${username} ${homeDir} ${desktopDir}
+   ln -sfT ${pkgs.gparted}/share/applications/gparted.desktop ${desktopDir + "gparted.desktop"}
+   ln -sfT ${pkgs.pantheon.elementary-terminal}/share/applications/io.elementary.terminal.desktop ${desktopDir + "io.elementary.terminal.desktop"}
+   ln -sfT ${pkgs.calamares-nixos}/share/applications/io.calamares.calamares.desktop ${desktopDir + "io.calamares.calamares.desktop"}
+  '';
+
+  config.isoImage.edition = lib.mkForce "${desktop}";
+  config.services.xserver.displayManager.autoLogin.user = "${username}";
 }
