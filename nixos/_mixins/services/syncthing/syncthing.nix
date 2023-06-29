08f9aad3e3979f9aa910fcb7113c98cf3e679bc6
Martin Wimpress <code@flexion.org>
2023-06-29 17:09:50 +0100

    fix: do not configure Syncthing via NixOS options

--- nixos/_mixins/services/syncthing/syncthing.nix
+++ nixos/_mixins/services/syncthing/syncthing.nix
@@ -2,17 +2,16 @@
   # https://nixos.wiki/wiki/Syncthing
   # https://wes.today/nixos-syncthing/
 
-  services.syncthing = {
-    configDir = "/home/${username}/Syncthing/Devices/${hostname}";
-    dataDir = "/home/${username}/Syncthing";
-    enable = true;
-    user = "${username}";
-    group = "${username}";
-    guiAddress = "0.0.0.0:8384";
-    openDefaultPorts = true;
-    overrideDevices = false;
-    overrideFolders = false;
-  };
+  # FIXME: This does not appear to work, using Home Manager for now.
+  #services.syncthing = {
+  #  configDir = "/home/${username}/Syncthing/Devices/${hostname}";
+  #  dataDir = "/home/${username}/Syncthing";
+  #  enable = true;
+  #  user = "${username}";
+  #  group = "${username}";
+  #  guiAddress = "0.0.0.0:8384";
+  #  openDefaultPorts = true;
+  #};
 
   networking = {
     firewall = {
