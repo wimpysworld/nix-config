e01e912162b8dcad569b2029b53ac6d74d736fe5
Martin Wimpress <code@flexion.org>
2023-06-29 18:22:56 +0100

    feat: add Syncthing via Home Manager service

---
+++ home-manager/_mixins/services/syncthing.nix
@@ -0,0 +1,13 @@
+{ hostname, username, ... }: {
+  services.syncthing = {
+    enable = true;
+    extraOptions = [
+      "--config=/home/${username}/Syncthing/Devices/${hostname}"
+      "--data=/home/${username}/Syncthing/DB/${hostname}"
+      "--gui-address=0.0.0.0:8384"
+      "--no-default-folder"
+      "--no-browser"
+    ];
+    tray.enable = true;
+  };
+}
