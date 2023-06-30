66533624e60280c1c4e2f02361a2ac30f4f4fe6a
Martin Wimpress <martin@wimpress.org>
2023-06-30 18:34:16 +0100

    refactor: do not use unstable for gh-markdown-preview; it is in 23.05

--- home-manager/_mixins/console/git.nix
+++ home-manager/_mixins/console/git.nix
@@ -2,7 +2,7 @@
   programs = {
     gh = {
       enable = true;
-      extensions = with pkgs.unstable; [ gh-markdown-preview ];
+      extensions = with pkgs; [ gh-markdown-preview ];
       settings = {
         git_protocol = "ssh";
         prompt = "enabled";
