719db3cd17d58839e974e3bd2179587d3d9d5426
Martin Wimpress <martin@wimpress.org>
2023-06-30 15:34:36 +0100

    fix: correct nvtop and order GPD P2 Max hardware.nix

--- nixos/p2-max/hardware.nix
+++ nixos/p2-max/hardware.nix
@@ -11,19 +11,17 @@
   }];
 
   # My GPD P2 Max has a US keyboard layout
-  console.keyMap = lib.mkForce "us";
-  services.xserver.layout = lib.mkForce "us";
-
   console = {
     earlySetup = true;
     # Pixel sizes of the font: 12, 14, 16, 18, 20, 22, 24, 28, 32
     # Followed by 'n' (normal) or 'b' (bold)
     font = "ter-powerline-v28n";
+    keyMap = lib.mkForce "us";
     packages = [ pkgs.terminus_font pkgs.powerline-fonts ];
   };
 
   environment.systemPackages = with pkgs; [
-    nvtop-intel
+    nvtop
   ];
 
   hardware = {
@@ -36,4 +34,6 @@
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  services.xserver.layout = lib.mkForce "us";
 }
