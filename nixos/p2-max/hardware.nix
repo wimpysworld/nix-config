1d707be2a9ce22693bb6c0d2dcbb00ef418c88bc
Martin Wimpress <martin@wimpress.org>
2023-06-30 15:24:00 +0100

    fix: apply correct keyboard layout for GPD P2 Max

--- nixos/p2-max/hardware.nix
+++ nixos/p2-max/hardware.nix
@@ -10,6 +10,10 @@
     size = 2048;
   }];
 
+  # My GPD P2 Max has a US keyboard layout
+  console.keyMap = lib.mkForce "us";
+  services.xserver.layout = lib.mkForce "us";
+
   console = {
     earlySetup = true;
     # Pixel sizes of the font: 12, 14, 16, 18, 20, 22, 24, 28, 32
