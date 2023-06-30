77419c9db9e4289e2676abe6e91dc3a36f706bc5
Martin Wimpress <martin@wimpress.org>
2023-06-30 23:26:50 +0100

    feat: add sshguard whitelist

--- nixos/_mixins/services/openssh.nix
+++ nixos/_mixins/services/openssh.nix
@@ -9,6 +9,12 @@
     };
     sshguard = {
       enable = true;
+      whitelist = [
+       "192.168.2.0/24"
+       "192.168.192.0/24"
+       "62.31.16.154"
+       "80.209.186.67"
+      ];
     };
   };
   programs.ssh.startAgent = true;
