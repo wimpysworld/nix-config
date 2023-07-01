342796af5266b182fdf665b2e226ec62f7cb3660
Martin Wimpress <martin@wimpress.org>
2023-07-01 11:30:59 +0100

    refactor: use platform rather the HostPlatform to avoid name collisions

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -1,9 +1,9 @@
 { inputs, outputs, stateVersion, ... }: {
   # Helper function for generating home-manager configs
-  mkHome = { hostname, username, desktop ? null, hostPlatform ? "x86_64-linux" }: inputs.home-manager.lib.homeManagerConfiguration {
-    pkgs = inputs.nixpkgs.legacyPackages.${hostPlatform};
+  mkHome = { hostname, username, desktop ? null, platform ? "x86_64-linux" }: inputs.home-manager.lib.homeManagerConfiguration {
+    pkgs = inputs.nixpkgs.legacyPackages.${platform};
     extraSpecialArgs = {
-      inherit inputs outputs desktop hostname hostPlatform username stateVersion;
+      inherit inputs outputs desktop hostname platform username stateVersion;
     };
     modules = [ ../home-manager ];
   };
