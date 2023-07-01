e2f422ab057a8c959e46512926b111fa5e67d4b7
Martin Wimpress <martin@wimpress.org>
2023-07-01 11:31:47 +0100

    feat: make sure /mnt/snapshots has a user accessible target

--- nixos/_mixins/base/default.nix
+++ nixos/_mixins/base/default.nix
@@ -63,5 +63,6 @@
   # Create dirs for home-manager
   systemd.tmpfiles.rules = [
     "d /nix/var/nix/profiles/per-user/${username} 0755 ${username} root"
+    "d /mnt/snapshot/${username} 0755 ${username} users"
   ];
 }
