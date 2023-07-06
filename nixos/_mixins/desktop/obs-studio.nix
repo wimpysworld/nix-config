294fc98a1968f93de26e42d1e3303cd08786be06
Martin Wimpress <martin@wimpress.org>
2023-07-06 09:37:15 +0100

    fix: install OBS Studio plugins that wrap OBS

--- nixos/_mixins/desktop/obs-studio.nix
+++ nixos/_mixins/desktop/obs-studio.nix
@@ -1,30 +1,29 @@
 { pkgs, ... }: {
   # https://nixos.wiki/wiki/OBS_Studio
   environment.systemPackages = [
-    pkgs.unstable.obs-studio
-    (pkgs.wrapOBS {
-      plugins = with pkgs.unstable.obs-studio-plugins; [
-        obs-3d-effect
-        obs-command-source
-        obs-gradient-source
-        obs-gstreamer
-        obs-nvfbc
-        obs-move-transition
-        obs-mute-filter
-        obs-pipewire-audio-capture
-        #obs-rgb-levels-filter
-        obs-text-pthread
-        obs-scale-to-sound
-        advanced-scene-switcher
-        obs-shaderfilter
-        obs-source-clone
-        obs-source-record
-        obs-source-switcher
-        obs-transition-table
-        obs-vaapi
-        obs-vintage-filter
-        obs-websocket
-      ];
-    })
+  (pkgs.wrapOBS {
+    plugins = with pkgs.unstable.obs-studio-plugins; [
+      obs-3d-effect
+      obs-command-source
+      obs-gradient-source
+      obs-gstreamer
+      obs-nvfbc
+      obs-move-transition
+      obs-mute-filter
+      obs-pipewire-audio-capture
+      #obs-rgb-levels-filter
+      obs-text-pthread
+      obs-scale-to-sound
+      advanced-scene-switcher
+      obs-shaderfilter
+      obs-source-clone
+      obs-source-record
+      obs-source-switcher
+      obs-transition-table
+      obs-vaapi
+      obs-vintage-filter
+      obs-websocket
+    ];
+  })
   ];
 }
