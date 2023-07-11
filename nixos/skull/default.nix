0bef4a4ae3322181b4b8125338611048a68c58bd
Martin Wimpress <martin@wimpress.org>
2023-07-11 21:07:28 +0100

    wip: add skull configuration

--- nixos/skull/default.nix
+++ nixos/skull/default.nix
@@ -12,23 +12,31 @@
     ../_mixins/virt
   ];
 
+  # disko does manage mounting of / /boot /home, but I want to mount by-partlabel
+  fileSystems."/" = lib.mkForce {
+    device = "/dev/disk/by-partlabel/root";
+    fsType = "xfs";
+    options = [ "defaults" "relatime" "nodiratime" ];
+  };
+
+  fileSystems."/boot" = lib.mkForce {
+    device = "/dev/disk/by-partlabel/ESP";
+    fsType = "vfat";
+  };
+
+  fileSystems."/home" = lib.mkForce {
+    device = "/dev/disk/by-partlabel/home";
+    fsType = "xfs";
+    options = [ "defaults" "relatime" "nodiratime" ];
+  };
+
   swapDevices = [{
     device = "/swap";
     size = 2048;
   }];
 
   boot = {
-    initrd.availableKernelModules = [
-        "ahci"
-        "nvme"
-        "rtsx_pci_sdmmc"
-        "sd_mod"
-        "sdhci_pci"
-        "uas"
-        "usbhid"
-        "usb_storage"
-        "xhci_pci"
-      ];
+    initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "uas" "sd_mod" ];
     kernelModules = [ "kvm-intel" ];
   };
 
