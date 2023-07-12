e848a497028f48457fe43ca694706a27a3493060
Martin Wimpress <martin@wimpress.org>
2023-07-12 09:17:59 +0100

    fix: use micro as the editor in gh

--- home-manager/_mixins/console/default.nix
+++ home-manager/_mixins/console/default.nix
@@ -130,6 +130,7 @@
       enable = true;
       extensions = with pkgs; [ gh-markdown-preview ];
       settings = {
+        editor = "micro";
         git_protocol = "ssh";
         prompt = "enabled";
       };
