e206c75e0242224cce0af41d7cb5668f74186391
Martin Wimpress <martin@wimpress.org>
2023-07-13 08:45:11 +0100

    chore: whitespace clean up

--- nixos/_mixins/desktop/obs-studio.nix
+++ nixos/_mixins/desktop/obs-studio.nix
@@ -3,7 +3,7 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
   boot.extraModprobeConfig = ''
     options v4l2loopback devices=1 video_nr=13 card_label="OBS Virtual Camera" exclusive_caps=1
-    '';
+  '';
 
   environment.systemPackages = [
   (pkgs.wrapOBS {
