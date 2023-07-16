973c4a07485716be3b39bc0ee7754d612f25b5cd
Martin Wimpress <martin@wimpress.org>
2023-07-16 09:02:03 +0100

    fix: actually make the rebuild-all shell abbreviation work

--- nixos/default.nix
+++ nixos/default.nix
@@ -222,7 +222,7 @@
       '';
       shellAbbrs = {
         nix-gc              = "sudo nix-collect-garbage --delete-older-than 14d";
-        rebuild-all         = "rebuild-host && rebuild-home";
+        rebuild-all         = "sudo nix-collect-garbage --delete-older-than 14d && sudo nixos-rebuild switch --flake $HOME/Zero/nix-config && home-manager switch -b backup --flake $HOME/Zero/nix-config";
         rebuild-home        = "home-manager switch -b backup --flake $HOME/Zero/nix-config";
         rebuild-host        = "sudo nixos-rebuild switch --flake $HOME/Zero/nix-config";
         rebuild-lock        = "pushd $HOME/Zero/nix-config && nix flake lock --recreate-lock-file && popd";
