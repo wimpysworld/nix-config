fda0b39cad3299418ca1ae22d5231766d5550e50
Martin Wimpress <martin@wimpress.org>
2023-07-20 08:33:51 +0100

    fix: make zerotier interface trusted and allow its UDP port

--- nixos/_mixins/services/zerotier.nix
+++ nixos/_mixins/services/zerotier.nix
@@ -1,4 +1,11 @@
-_: {
+{ config, ... }: {
+  networking = {
+    firewall = {
+      checkReversePath = "loose";
+      allowedUDPPorts = [ config.services.zerotierone.port ];
+      trustedInterfaces = [ "ztwfukvgqh" ];
+    };
+  };
   services.zerotierone = {
     enable = true;
     joinNetworks = [ "e4da7455b2decfb5" ];
