58fe8b3b162cd14cbe00588df5d61ed8eeb287f9
Martin Wimpress <martin@wimpress.org>
2023-08-10 12:19:40 +0100

    feat: disable unused audio devices

--- home-manager/_mixins/users/martin/hosts/ripper.nix
+++ home-manager/_mixins/users/martin/hosts/ripper.nix
@@ -10,6 +10,35 @@ with lib.hm.gvariant;
     ../../../desktop/sakura.nix
     ../../../desktop/vorta.nix
   ];
+
+  # Disable unused audio devices
+  # - alsa_card.usb-Generic_USB_Audio-00:  Motherboard (Realtek-ALC1220-VB-Desktop)
+  # - alsa_card.pci-0000_03_00.1:          NVIDIA (HDA NVidia)
+  # - alsa_card.pci-0000_23_00.1:          AMD (Navi 21/23 HDMI/DP Audio Controller)
+  # - alsa_card.pci-0000_4e_00.0:          Magewell (00-01 Pro Capture Quad HDMI)
+  # - alsa_card.pci-0000_4f_00.0:          Magewell (00-02 Pro Capture Quad HDMI)
+  # - alsa_card.pci-0000_50_00.0:          Magewell (00-03 Pro Capture Quad HDMI)
+  home = {
+    file.".config/wireplumber/main.lua.d/51-disable-cards.lua".text = ''
+      rule = {
+        matches = {
+          {
+            { "device.name", "equals", "alsa_card.usb-Generic_USB_Audio-00" },
+            { "device.name", "equals", "alsa_card.pci-0000_03_00.1" },
+            { "device.name", "equals", "alsa_card.pci-0000_23_00.1" },
+            { "device.name", "equals", "alsa_card.pci-0000_4e_00.0" },
+            { "device.name", "equals", "alsa_card.pci-0000_4f_00.0" },
+            { "device.name", "equals", "alsa_card.pci-0000_50_00.0" },
+          },
+        },
+        apply_properties = {
+          ["device.disabled"] = true,
+        },
+      }
+      table.insert(alsa_monitor.rules,rule)
+    '';
+  };
+
   dconf.settings = {
     "org/gnome/desktop/background" = {
       picture-options = "zoom";
--- home-manager/_mixins/users/martin/hosts/trooper.nix
+++ home-manager/_mixins/users/martin/hosts/trooper.nix
@@ -10,6 +10,27 @@ with lib.hm.gvariant;
     ../../../desktop/sakura.nix
     ../../../desktop/vorta.nix
   ];
+
+  # Disable unused audio devices
+  # - alsa_card.pci-0000_0a_00.1: NVIDIA (HDA NVidia)
+  home = {
+    file.".config/wireplumber/main.lua.d/51-disable-cards.lua".text = ''
+      rule = {
+        matches = {
+          {
+            { "device.name", "equals", "alsa_card.pci-0000_0a_00.1" },
+            { "device.name", "equals", "alsa_card.usb-AVerMedia_Technologies__Inc._Live_Streamer_CAM_513_5203711200146-00" },
+            { "device.name", "equals", "alsa_card.usb-Elgato_Cam_Link_4K_000592F643000-03" },
+          },
+        },
+        apply_properties = {
+          ["device.disabled"] = true,
+        },
+      }
+      table.insert(alsa_monitor.rules,rule)
+    '';
+  };
+
   dconf.settings = {
     "org/gnome/desktop/background" = {
       picture-options = "zoom";
