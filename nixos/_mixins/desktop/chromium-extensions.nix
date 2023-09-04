ad469644f4b180253513e482fc34a5cd55b9b123
Martin Wimpress <martin@wimpress.org>
2023-09-04 13:43:16 +0100

    refactor: put chromium extensions in a separate configuration

---
+++ nixos/_mixins/desktop/chromium-extensions.nix
@@ -0,0 +1,14 @@
+_: {
+  programs = {
+    chromium = {
+      extensions = [
+        "kbfnbcaeplbcioakkpcpgfkobkghlhen" # Grammarly
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+        "mdjildafknihdffpkfmmpnpoiajfjnjd" # Consent-O-Matic
+        "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock for YouTube
+        "gebbhagfogifgggkldgodflihgfeippi" # Return YouTube Dislike
+        "edlifbnjlicfpckhgjhflgkeeibhhcii" # Screenshot Tool
+      ];
+    };
+  };
+}
