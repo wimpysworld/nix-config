d8cee24fa07a50eb88af3f22715cdb47d311c07c
Martin Wimpress <martin@wimpress.org>
2023-09-05 22:18:42 +0100

    refactor: update the skull disk array configuration

--- nixos/skull/disk-array.nix
+++ nixos/skull/disk-array.nix
@@ -1,169 +1,253 @@
 # nix shell nixpkgs#gptfdisk
+
 # sudo nix run github:nix-community/disko -- --mode zap_create_mount ./disk-array.nix
-# Transcend MTS830S 4TB
-#sda           8:0    0   3.7T  0 disk 
-#sdb           8:16   0   3.7T  0 disk 
-#sdc           8:32   0   3.7T  0 disk 
-#sdd           8:48   0   3.7T  0 disk 
+# sudo nix run github:nix-community/disko -- --mode format ./disk-array.nix
+# sudo nix run github:nix-community/disko -- --mode mount ./disk-array.nix
 
-# WD Blue 2TB
-#sde           8:64   0   1.8T  0 disk 
-#sdf           8:80   0   1.8T  0 disk 
-#sdg           8:96   0   1.8T  0 disk 
-#sdh           8:112  0   1.8T  0 disk
+# Top Card: 4x Transcend MTS830S 2TB
+#/dev/disk/by-id/ata-TS2TMTS830S_I021610007
+#/dev/disk/by-id/ata-TS2TMTS830S_I021610011
+#/dev/disk/by-id/ata-TS2TMTS830S_I021610027
+#/dev/disk/by-id/ata-TS2TMTS830S_I021610056
 
-# Transcend MTS830S 2TB (tbd)
-#sd?
-#sd?
-#sd?
-#sd?
+# Middle Card: 4x Transcend MTS830S 4TB
+#/dev/disk/by-id/ata-TS4TMTS830S_H738980002
+#/dev/disk/by-id/ata-TS4TMTS830S_H760910070
+#/dev/disk/by-id/ata-TS4TMTS830S_H760910071
+#/dev/disk/by-id/ata-TS4TMTS830S_H760910072
 
-{ disks ? [ "/dev/sda" "/dev/sdb" "/dev/sdc" "/dev/sdd" "/dev/sde" "/dev/sdf" "/dev/sdg" "/dev/sdh" ], ... }:
+# Bottom Card: 4x WD Blue 2TB
+#/dev/disk/by-id/ata-WDC_WDS200T2B0B-00YS70_23024Z440206
+#/dev/disk/by-id/ata-WDC_WDS200T2B0B-00YS70_23024Z445606
+#/dev/disk/by-id/ata-WDC_WDS200T2B0B-00YS70_23024Z449709
+#/dev/disk/by-id/missing
+
+{ disks ? [ "/dev/disk/by-id/ata-TS4TMTS830S_H738980002"
+            "/dev/disk/by-id/ata-TS4TMTS830S_H760910070"
+            "/dev/disk/by-id/ata-TS4TMTS830S_H760910071"
+            "/dev/disk/by-id/ata-TS4TMTS830S_H760910072"
+            "/dev/disk/by-id/ata-TS2TMTS830S_I021610007"
+            "/dev/disk/by-id/ata-TS2TMTS830S_I021610011"
+            "/dev/disk/by-id/ata-TS2TMTS830S_I021610027"
+            "/dev/disk/by-id/ata-TS2TMTS830S_I021610056"
+            "/dev/disk/by-id/ata-WDC_WDS200T2B0B-00YS70_23024Z440206"
+            "/dev/disk/by-id/ata-WDC_WDS200T2B0B-00YS70_23024Z445606"
+            "/dev/disk/by-id/ata-WDC_WDS200T2B0B-00YS70_23024Z449709"
+            #/dev/disk/by-id/missing
+          ], ... }:
 let
   defaultXfsOpts = [ "defaults" "relatime" "nodiratime" ];
 in
 {
   disko.devices = {
     disk = {
-      sda = {
+      TS4-1 = {
         type = "disk";
         device = builtins.elemAt disks 0;
         content = {
           type = "table";
           format = "gpt";
           partitions = [{
-            name = "fours-sda";
+            name = "TS4-1";
             start = "0%";
             end = "100%";
             content = {
               type = "mdraid";
-              name = "fours";
+              name = "TS4";
             };
           }];
         };
       };
-      sdb = {
+      TS4-2 = {
         type = "disk";
         device = builtins.elemAt disks 1;
         content = {
           type = "table";
           format = "gpt";
           partitions = [{
-            name = "fours-sdb";
+            name = "TS4-2";
             start = "0%";
             end = "100%";
             content = {
               type = "mdraid";
-              name = "fours";
+              name = "TS4";
             };
           }];
         };
       };
-      sdc = {
+      TS4-3 = {
         type = "disk";
         device = builtins.elemAt disks 2;
         content = {
           type = "table";
           format = "gpt";
           partitions = [{
-            name = "fours-sdc";
+            name = "TS3-3";
             start = "0%";
             end = "100%";
             content = {
               type = "mdraid";
-              name = "fours";
+              name = "TS4";
             };
           }];
         };
       };
-      sdd = {
+      TS4-4 = {
         type = "disk";
         device = builtins.elemAt disks 3;
         content = {
           type = "table";
           format = "gpt";
           partitions = [{
-            name = "fours-sdd";
+            name = "TS4-4";
             start = "0%";
             end = "100%";
             content = {
               type = "mdraid";
-              name = "fours";
+              name = "TS4";
             };
           }];
         };
       };
-      sde = {
+      TS2-1 = {
         type = "disk";
         device = builtins.elemAt disks 4;
         content = {
           type = "table";
           format = "gpt";
           partitions = [{
-            name = "twos-sde";
+            name = "TS2-1";
             start = "0%";
             end = "100%";
             content = {
               type = "mdraid";
-              name = "twos";
+              name = "TS2";
             };
           }];
         };
       };
-      sdf = {
+      TS2-2 = {
         type = "disk";
         device = builtins.elemAt disks 5;
         content = {
           type = "table";
           format = "gpt";
           partitions = [{
-            name = "twos-sdf";
+            name = "TS2-2";
             start = "0%";
             end = "100%";
             content = {
               type = "mdraid";
-              name = "twos";
+              name = "TS2";
             };
           }];
         };
       };
-      sdg = {
+      TS2-3 = {
         type = "disk";
         device = builtins.elemAt disks 6;
         content = {
           type = "table";
           format = "gpt";
           partitions = [{
-            name = "twos-sdg";
+            name = "TS2-3";
             start = "0%";
             end = "100%";
             content = {
               type = "mdraid";
-              name = "twos";
+              name = "TS2";
             };
           }];
         };
       };
-      sdh = {
+      TS2-4 = {
         type = "disk";
         device = builtins.elemAt disks 7;
         content = {
           type = "table";
           format = "gpt";
           partitions = [{
-            name = "twos-sdh";
+            name = "TS2-4";
             start = "0%";
             end = "100%";
             content = {
               type = "mdraid";
-              name = "twos";
+              name = "TS2";
             };
           }];
         };
       };
+      WDS2-1 = {
+        type = "disk";
+        device = builtins.elemAt disks 8;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [{
+            name = "WDS2-1";
+            start = "0%";
+            end = "100%";
+            content = {
+              type = "mdraid";
+              name = "WDS2";
+            };
+          }];
+        };
+      };
+      WDS2-2 = {
+        type = "disk";
+        device = builtins.elemAt disks 9;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [{
+            name = "WDS2-2";
+            start = "0%";
+            end = "100%";
+            content = {
+              type = "mdraid";
+              name = "WDS2";
+            };
+          }];
+        };
+      };
+      WDS2-3 = {
+        type = "disk";
+        device = builtins.elemAt disks 10;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [{
+            name = "WDS2-3";
+            start = "0%";
+            end = "100%";
+            content = {
+              type = "mdraid";
+              name = "WDS2";
+            };
+          }];
+        };
+      };
+      #WDS2-4 = {
+      #  type = "disk";
+      #  device = builtins.elemAt disks 11;
+      #  content = {
+      #    type = "table";
+      #    format = "gpt";
+      #    partitions = [{
+      #      name = "WDS2-4";
+      #      start = "0%";
+      #      end = "100%";
+      #      content = {
+      #        type = "mdraid";
+      #        name = "WDS2";
+      #      };
+      #    }];
+      #  };
+      #};
     };
     mdadm = {
-      fours = {
+      TS4 = {
         type = "mdadm";
         level = 5;
         content = {
@@ -178,15 +262,15 @@ in
               # Overwirte the existing filesystem
               extraArgs = [ "-f" ];
               format = "xfs";
-              mountpoint = "/mnt/fours";
+              mountpoint = "/mnt/TS4";
               mountOptions = defaultXfsOpts;
             };
           }];
         };
       };
-      twos = {
+      TS2 = {
         type = "mdadm";
-        level = 6;
+        level = 5;
         content = {
           type = "table";
           format = "gpt";
@@ -199,7 +283,29 @@ in
               # Overwirte the existing filesystem
               extraArgs = [ "-f" ];
               format = "xfs";
-              mountpoint = "/mnt/twos";
+              mountpoint = "/mnt/TS2";
+              mountOptions = defaultXfsOpts;
+            };
+          }];
+        };
+      };
+      WDS2 = {
+        type = "mdadm";
+        # TODO: Temporary while testing a drive is missing
+        level = 0;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [{
+            name = "primary";
+            start = "0%";
+            end = "100%";
+            content = {
+              type = "filesystem";
+              # Overwirte the existing filesystem
+              extraArgs = [ "-f" ];
+              format = "xfs";
+              mountpoint = "/mnt/WDS2";
               mountOptions = defaultXfsOpts;
             };
           }];
