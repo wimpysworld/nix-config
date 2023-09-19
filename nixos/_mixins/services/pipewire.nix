9fcc6a812b8626228c9d29f410f392b2ee41313c
Martin Wimpress <martin@wimpress.org>
2023-09-19 01:30:46 +0100

    fix: prevent spa.alsa resyncs (crackling/popping) when using OBS Studio
    1024/48000 is 21ms latency, it is a touch on the high side but clean audio is better the crackling audio.

--- nixos/_mixins/services/pipewire.nix
+++ nixos/_mixins/services/pipewire.nix
@@ -7,6 +7,22 @@
   ] ++ lib.optionals (desktop != null) [
     pavucontrol
   ];
+
+  # https://nixos.wiki/wiki/PipeWire
+  # Debugging
+  #  - pw-top                              # see live stats
+  #  - journalctl -b0 --user -u pipewire   # see logs (spa resync in "bad")
+  environment.etc = {
+    "pipewire/pipewire.conf.d/92-fix-resync.conf".text = ''
+      context.properties = {
+        default.clock.rate = 48000
+        default.clock.quantum = 1024
+        default.clock.min-quantum = 1024
+        default.clock.max-quantum = 1024
+      }
+    '';
+  };
+
   hardware = {
     pulseaudio.enable = lib.mkForce false;
   };
