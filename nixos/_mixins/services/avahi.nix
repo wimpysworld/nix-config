d8ead0ff37d011c151aeacc10e4554d0324b0444
Martin Wimpress <martin@wimpress.org>
2023-10-02 09:09:45 +0100

    refactor: improve avahi

--- nixos/_mixins/services/avahi.nix
+++ nixos/_mixins/services/avahi.nix
@@ -1,8 +1,14 @@
-_: {
+{ desktop, ...}: {
   services = {
     avahi = {
       enable = true;
       nssmdns = true;
+      openFirewall = true;
+      publish = {
+        addresses = true;
+      	enable = true;
+      	workstation = if (builtins.isString desktop) then true else false;
+      };
     };
   };
 }
