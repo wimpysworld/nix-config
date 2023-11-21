1cefbdbe1cc502ef19ce29928ca7d4461c9d97e8
Martin Wimpress <martin@wimpress.org>
2023-11-21 10:43:05 +0000

    feat: add yt-dlp

--- home-manager/_mixins/console/default.nix
+++ home-manager/_mixins/console/default.nix
@@ -197,6 +197,20 @@
         max-width = 60;
       };
     };
+    yt-dlp = {
+      enable = true;
+      settings ={
+        audio-format = "best";
+        audio-quality = 0;
+        embed-chapters = true;
+        embed-metadata = true;
+        embed-subs = true;
+        embed-thumbnail = true;
+        remux-video = "aac>m4a/mov>mp4/mkv";
+        sponsorblock-mark = "sponsor";
+        sub-langs = "all";
+      };
+    };
   };
 
   services = {
