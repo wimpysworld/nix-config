99ea593595115a7f606ab528bc91e4d23938d16b
Martin Wimpress <martin@wimpress.org>
2023-11-21 10:56:53 +0000

    refactor: move all CLI/TUI configuration to home-manager/_mixins

--- home-manager/_mixins/users/martin/default.nix
+++ home-manager/_mixins/users/martin/default.nix
@@ -72,89 +72,14 @@
       disk_size="96G"
       iso="nixos-desktop/nixos.iso"
     '';
-    # A Modern Unix experience
-    # https://jvns.ca/blog/2022/04/12/a-list-of-new-ish--command-line-tools/
-    packages = with pkgs; [
-      asciinema # Terminal recorder
-      black # Code format Python
-      bmon # Modern Unix `iftop`
-      breezy # Terminal bzr client
-      butler # Terminal Itch.io API client
-      chafa # Terminal image viewer
-      chroma # Code syntax highlighter
-      clinfo # Terminal OpenCL info
-      curlie # Terminal HTTP client
-      dconf2nix # Nix code from Dconf files
-      debootstrap # Terminal Debian installer
-      diffr # Modern Unix `diff`
-      difftastic # Modern Unix `diff`
-      dogdns # Modern Unix `dig`
-      dua # Modern Unix `du`
-      duf # Modern Unix `df`
-      du-dust # Modern Unix `du`
-      editorconfig-core-c # EditorConfig Core
-      entr # Modern Unix `watch`
-      fast-cli # Terminal fast.com
-      fd # Modern Unix `find`
-      glow # Terminal Markdown renderer
-      gping # Modern Unix `ping`
-      hexyl # Modern Unix `hexedit`
-      httpie # Terminal HTTP client
-      hyperfine # Terminal benchmarking
-      iperf3 # Terminal network benchmarking
-      iw # Terminal WiFi info
-      jpegoptim # Terminal JPEG optimizer
-      jiq # Modern Unix `jq`
-      libva-utils # Terminal VAAPI info
-      lurk # Modern Unix `strace`
-      mdp # Terminal Markdown presenter
-      moar # Modern Unix `less`
-      mtr # Modern Unix `traceroute`
-      netdiscover # Modern Unix `arp`
-      nethogs # Modern Unix `iftop`
-      nixpkgs-review # Nix code review
-      nodePackages.prettier # Code format
-      nurl # Nix URL fetcher
-      nyancat # Terminal rainbow spewing feline
-      optipng # Terminal PNG optimizer
-      procs # Modern Unix `ps`
-      python310Packages.gpustat # Terminal GPU info
-      quilt # Terminal patch manager
-      ripgrep # Modern Unix `grep`
-      rustfmt # Code format Rust
-      sd # Modern Unix `sed`
-      shellcheck # Code lint Shell
-      shfmt # Code format Shell
-      speedtest-go # Terminal speedtest.net
-      tldr # Modern Unix `man`
-      tokei # Modern Unix `wc` for code
-      vdpauinfo # Terminal VDPAU info
-      wavemon # Terminal WiFi monitor
-      yq-go # Terminal `jq` for YAML
-    ];
     sessionVariables = {
       BZR_EMAIL = "Martin Wimpress <code@wimpress.io>";
       DEBFULLNAME = "Martin Wimpress";
       DEBEMAIL = "code@wimpress.io";
       DEBSIGN_KEYID = "8F04688C17006782143279DA61DF940515E06DA3";
-      PAGER = "moar";
     };
   };
   programs = {
-    fish = {
-      shellAliases = {
-        diff = "diffr";
-        fast = "fast -u";
-        glow = "glow --pager";
-        moon = "curl -s wttr.in/Moon";
-        pubip = "curl -s ifconfig.me/ip";
-        #pubip = "curl -s https://api.ipify.org";
-        speedtest = "speedtest-go";
-        wormhole = "wormhole-william";
-        wttr = "curl -s wttr.in && curl -s v2.wttr.in";
-        wttr-bas = "curl -s wttr.in/basingstoke && curl -s v2.wttr.in/basingstoke";
-      };
-    };
     git = {
       userEmail = "martin@wimpress.org";
       userName = "Martin Wimpress";
