73e10dd0507a92ab2684e1f06d94f8113c117a4b
Martin Wimpress <martin@wimpress.org>
2023-11-23 23:41:10 +0000

    feat: add cava

--- home-manager/_mixins/console/default.nix
+++ home-manager/_mixins/console/default.nix
@@ -168,6 +168,23 @@ in
         };
       };
     };
+    cava = {
+      enable = true;
+      settings = {
+        color = {
+          gradient = 1;
+          gradient_count = 8;
+          gradient_color_1 = "'#59cc33'";
+          gradient_color_2 = "'#80cc33'";
+          gradient_color_3 = "'#a6cc33'";
+          gradient_color_4 = "'#cccc33'";
+          gradient_color_5 = "'#cca633'";
+          gradient_color_6 = "'#cc8033'";
+          gradient_color_7 = "'#cc5933'";
+          gradient_color_8 = "'#cc3333'";
+        };
+      };
+    };
     dircolors = {
       enable = true;
       enableBashIntegration = true;
