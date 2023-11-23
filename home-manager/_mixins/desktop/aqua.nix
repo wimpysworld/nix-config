92a231f9556e3daaeb0ca647aeb8062395da92b3
Martin Wimpress <martin@wimpress.org>
2023-11-23 16:32:33 +0000

    feat: add support for Aqua (macOS) desktop

---
+++ home-manager/_mixins/desktop/aqua.nix
@@ -0,0 +1,39 @@
+{ lib, pkgs, ... }:
+let
+  inherit (pkgs.stdenv) isDarwin;
+in
+lib.mkIf isDarwin {
+  # Darwin specific configuration
+  targets.darwin.defaults = {
+    NSGlobalDomain = {
+      AppleLanguages = [ "en-GB" ];
+      AppleLocale = "en_GB";
+      AppleMeasurementUnits = "Centimeters";
+      AppleMetricUnits = true;
+      AppleTemperatureUnit = "Celsius";
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+    };
+    "com.apple.Safari" = {
+      AutoFillCreditCardData = false;
+      AutoFillPasswords = false;
+      AutoOpenSafeDownloads = false;
+      ShowOverlayStatusBar = true;
+    };
+    "com.apple.desktopservices" = {
+      DSDontWriteNetworkStores = true;
+      DSDontWriteUSBStores = true;
+    };
+    "com.apple.dock" = {
+      size-immutable = true;
+      tilesize = 64;
+    };
+    "com.apple.controlcenter" = {
+      BatteryShowPercentage = true;
+    };
+    search = "Google";
+  };
+}
