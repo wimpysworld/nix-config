9fd595747906d5a0a1b9beda6e07aa0903d821ca
Martin Wimpress <martin@wimpress.org>
2023-11-23 16:53:29 +0000

    fix: correct formatting error in aqua.nix

--- home-manager/_mixins/desktop/aqua.nix
+++ home-manager/_mixins/desktop/aqua.nix
@@ -4,35 +4,37 @@ let
 in
 lib.mkIf isDarwin {
   # Darwin specific configuration
-  targets.darwin.defaults = {
-    NSGlobalDomain = {
-      AppleLanguages = [ "en-GB" ];
-      AppleLocale = "en_GB";
-      AppleMeasurementUnits = "Centimeters";
-      AppleMetricUnits = true;
-      AppleTemperatureUnit = "Celsius";
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticDashSubstitutionEnabled = false;
-      NSAutomaticPeriodSubstitutionEnabled = false;
-      NSAutomaticQuoteSubstitutionEnabled = false;
-      NSAutomaticSpellingCorrectionEnabled = false;
-    };
-    "com.apple.Safari" = {
-      AutoFillCreditCardData = false;
-      AutoFillPasswords = false;
-      AutoOpenSafeDownloads = false;
-      ShowOverlayStatusBar = true;
-    };
-    "com.apple.desktopservices" = {
-      DSDontWriteNetworkStores = true;
-      DSDontWriteUSBStores = true;
-    };
-    "com.apple.dock" = {
-      size-immutable = true;
-      tilesize = 64;
-    };
-    "com.apple.controlcenter" = {
-      BatteryShowPercentage = true;
+  targets.darwin = {
+    defaults = {
+      NSGlobalDomain = {
+        AppleLanguages = [ "en-GB" ];
+        AppleLocale = "en_GB";
+        AppleMeasurementUnits = "Centimeters";
+        AppleMetricUnits = true;
+        AppleTemperatureUnit = "Celsius";
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+      };
+      "com.apple.Safari" = {
+        AutoFillCreditCardData = false;
+        AutoFillPasswords = false;
+        AutoOpenSafeDownloads = false;
+        ShowOverlayStatusBar = true;
+      };
+      "com.apple.desktopservices" = {
+        DSDontWriteNetworkStores = true;
+        DSDontWriteUSBStores = true;
+      };
+      "com.apple.dock" = {
+        size-immutable = true;
+        tilesize = 64;
+      };
+      "com.apple.controlcenter" = {
+        BatteryShowPercentage = true;
+      };
     };
     search = "Google";
   };
