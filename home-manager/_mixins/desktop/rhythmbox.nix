fef12510ef6e53b5f5d11e95e6096fdfab3c71ed
Martin Wimpress <martin@wimpress.org>
2023-11-23 16:41:32 +0000

    refactor: consolidate all rhythmbox dconf configuration

--- home-manager/_mixins/desktop/rhythmbox.nix
+++ home-manager/_mixins/desktop/rhythmbox.nix
@@ -17,6 +17,11 @@ lib.mkIf isLinux {
       download-interval = "manual";
     };
 
+    "org/gnome/rhythmbox/rhythmdb" = {
+      locations = [ "file://${config.home.homeDirectory}/Studio/Music" ];
+      monitor-library = true;
+    };
+
     "org/gnome/rhythmbox/sources" = {
       browser-views = "genres-artists-albums";
       visible-columns = [ "post-time" "duration" "track-number" "album" "genre" "beats-per-minute" "play-count" "artist" ];
