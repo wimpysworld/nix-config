cbeeef21e036af812ea01f62b7333ade4de50495
Martin Wimpress <martin@wimpress.org>
2023-11-26 21:33:40 +0000

    fix: correct hostnames for the Macbook Air M2

--- flake.nix
+++ flake.nix
@@ -52,8 +52,8 @@
         "martin@iso-console" = libx.mkHome { hostname = "iso-console"; username = "nixos"; };
         "martin@iso-desktop" = libx.mkHome { hostname = "iso-desktop"; username = "nixos"; desktop = "pantheon"; };
         # Workstations
-        "martin@airmac" = libx.mkHome { hostname = "air"; username = "martin"; platform = "aarch64-darwin"; desktop = "aqua";};
-        "martin@airnix" = libx.mkHome { hostname = "air"; username = "martin"; platform = "aarch64-linux"; desktop = "pantheon"; };
+        "martin@airmac" = libx.mkHome { hostname = "airmac"; username = "martin"; platform = "aarch64-darwin"; desktop = "aqua";};
+        "martin@airnix" = libx.mkHome { hostname = "airnix"; username = "martin"; platform = "aarch64-linux"; desktop = "pantheon"; };
         "martin@designare" = libx.mkHome { hostname = "designare"; username = "martin"; desktop = "pantheon"; };
         "martin@micropc" = libx.mkHome { hostname = "micropc"; username = "martin"; desktop = "pantheon"; };
         "martin@p1" = libx.mkHome { hostname = "p1"; username = "martin"; desktop = "pantheon"; };
