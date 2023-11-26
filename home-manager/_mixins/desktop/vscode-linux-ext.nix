77f2efeb407ac1da34c6e220ef3433abe17ce3c2
Martin Wimpress <martin@wimpress.org>
2023-11-26 21:44:50 +0000

    fix: cpptools and vsliveshare are not available for Darwin
    Put any Linux only VSCode extensions in their own config

---
+++ home-manager/_mixins/desktop/vscode-linux-ext.nix
@@ -0,0 +1,14 @@
+{ lib, pkgs, ... }:
+let
+  inherit (pkgs.stdenv) isLinux;
+in
+lib.mkIf isLinux {
+  programs = {
+    vscode = {
+      extensions = with pkgs; [
+        vscode-extensions.ms-vscode.cpptools
+        vscode-extensions.ms-vsliveshare.vsliveshare
+      ];
+    };
+  };
+}
