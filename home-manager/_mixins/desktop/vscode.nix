180f86c6fc4ec8c57df63ba0c5525f870d184bcf
Martin Wimpress <martin@wimpress.org>
2023-12-03 13:10:30 +0000

    feat: add better comments vscode extension

--- home-manager/_mixins/desktop/vscode.nix
+++ home-manager/_mixins/desktop/vscode.nix
@@ -67,6 +67,12 @@
           sha256 = "sha256-TwHuoXme0o6EeciA1lxhs5vmhGlDvaWlH8tjVmuSQH8";
         }
         {
+          name = "better-comments";
+          publisher = "aaron-bond";
+          version = "3.0.2";
+          sha256 = "sha256-hQmA8PWjf2Nd60v5EAuqqD8LIEu7slrNs8luc3ePgZc=";
+        }
+        {
           name = "better-csv-syntax";
           publisher = "jeff-hykin";
           version = "0.0.2";
