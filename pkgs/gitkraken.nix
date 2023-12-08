f557b9913b0211faf578239701ac97231c77f762
Martin Wimpress <martin@wimpress.org>
2023-12-08 13:25:23 +0000

    refactor: symlink the static NodeGit in GitKraken

--- pkgs/gitkraken.nix
+++ pkgs/gitkraken.nix
@@ -139,8 +139,8 @@ let
 
       pushd $out/share/${pname}/resources/app.asar.unpacked/node_modules/@axosoft/nodegit/build/Release
       mv nodegit-ubuntu-18.node nodegit-ubuntu-18-ssl-1.1.1.node
-      mv nodegit-ubuntu-18-ssl-static.node nodegit-ubuntu-18.node
-      chmod 755 nodegit-ubuntu-18.node
+      ln -s nodegit-ubuntu-18-ssl-static.node nodegit-ubuntu-18.node
+      chmod 755 nodegit-ubuntu-18-ssl-static.node
       popd
     '';
   };
