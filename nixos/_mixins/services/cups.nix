d0d40792379b12837840abe1f3defbb7b1c79b05
Martin Wimpress <martin@wimpress.org>
2024-01-24 23:44:37 +0000

    refactor: use hplip printer driver

--- nixos/_mixins/services/cups.nix
+++ nixos/_mixins/services/cups.nix
@@ -4,7 +4,6 @@
   ];
   services = {
     printing.enable = true;
-    #printing.drivers = with pkgs; [ gutenprint hplipWithPlugin ];
-    printing.drivers = with pkgs; [ gutenprint ];
+    printing.drivers = with pkgs; [ gutenprint hplip ];
   };
 }
