8de567bcea776ae7f4cf695af223f81a2ae72825
Martin Wimpress <martin@wimpress.org>
2024-01-28 15:54:38 +0000

    refactor: make package references in aliases declarative

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -23,7 +23,7 @@ in
 
   home = {
     activation.report-changes = config.lib.dag.entryAnywhere ''
-      if [ -e /run/current-system/boot.json ] && ! grep -q "LABEL=nixos-minimal" /run/current-system/boot.json; then
+      if [ -e /run/current-system/boot.json ] && ! ${pkgs.gnugrep}/bin/grep -q "LABEL=nixos-minimal" /run/current-system/boot.json; then
         ${pkgs.nvd}/bin/nvd diff $oldGenPath $newGenPath
       fi
     '';
