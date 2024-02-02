0f8b3c2a1b92d04f55e5b245dd58eb44eca56a4f
Martin Wimpress <martin@wimpress.org>
2024-02-02 12:37:08 +0000

    refactor: finalise configuration for phasma

---
+++ nixos/phasma/disks-snapshot.nix
@@ -0,0 +1,98 @@
+# nvme0n1 2TB:   NixOS
+# nvme1n1 4TB:   Home RAID-0
+# nvme2n1 4TB:   Home RAID-0
+# sda     4TB:   Backup RAID-0
+# sdb     4TB:   Backup RAID-0   
+# sdc     4TB:   Backup RAID-0
+{ disks ? [ "/dev/sda" "/dev/sdb" "/dev/sdc" ], ... }:
+let
+  defaultXfsOpts = [ "defaults" "relatime" "nodiratime" ];
+in
+{
+  disko.devices = {
+    disk = {
+      sda = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "snapshot-sda";
+              start = "0%";
+              end = "100%";
+              content = {
+                type = "mdraid";
+                name = "snapshot";
+              };
+            }
+          ];
+        };
+      };
+      sdb = {
+        type = "disk";
+        device = builtins.elemAt disks 1;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "snapshot-sdb";
+              start = "0%";
+              end = "100%";
+              content = {
+                type = "mdraid";
+                name = "snapshot";
+              };
+            }
+          ];
+        };
+      };
+      sdc = {
+        type = "disk";
+        device = builtins.elemAt disks 2;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "snapshot-sdc";
+              start = "0%";
+              end = "100%";
+              content = {
+                type = "mdraid";
+                name = "snapshot";
+              };
+            }
+          ];
+        };
+      };
+    };
+    mdadm = {
+      snapshot = {
+        type = "mdadm";
+        level = 0;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "snapshot";
+              start = "0%";
+              end = "100%";
+              content = {
+                type = "filesystem";
+                # Overwirte the existing filesystem
+                extraArgs = [ "-f" ];
+                format = "xfs";
+                mountpoint = "/mnt/snapshot";
+                mountOptions = defaultXfsOpts;
+              };
+            }
+          ];
+        };
+      };
+    };
+  };
+}
