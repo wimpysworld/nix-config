f9834f8522039c8dbc39d37c95802a1c1eacba4b
Martin Wimpress <martin@wimpress.org>
2024-02-02 12:02:02 +0000

    chore: update vader disabled audio devices

--- home-manager/_mixins/users/martin/hosts/vader.nix
+++ home-manager/_mixins/users/martin/hosts/vader.nix
@@ -6,32 +6,30 @@ with lib.hm.gvariant;
   ];
 
   # Disable unused audio devices
-  # - alsa_card.usb-Generic_USB_Audio-00:  Motherboard (Realtek-ALC1220-VB-Desktop)
-  # - alsa_card.pci-0000_03_00.1:          NVIDIA (HDA NVidia)
-  # - alsa_card.pci-0000_23_00.1:          AMD (Navi 21/23 HDMI/DP Audio Controller)
-  # - alsa_card.pci-0000_4e_00.0:          Magewell (00-01 Pro Capture Quad HDMI)
-  # - alsa_card.pci-0000_4f_00.0:          Magewell (00-02 Pro Capture Quad HDMI)
-  # - alsa_card.pci-0000_50_00.0:          Magewell (00-03 Pro Capture Quad HDMI)
-  home = {
-    #file.".config/wireplumber/main.lua.d/51-disable-cards.lua".text = ''
-    #  rule = {
-    #    matches = {
-    #      {
-    #        { "device.name", "equals", "alsa_card.usb-Generic_USB_Audio-00" },
-    #        { "device.name", "equals", "alsa_card.pci-0000_03_00.1" },
-    #        { "device.name", "equals", "alsa_card.pci-0000_23_00.1" },
-    #        { "device.name", "equals", "alsa_card.pci-0000_4e_00.0" },
-    #        { "device.name", "equals", "alsa_card.pci-0000_4f_00.0" },
-    #        { "device.name", "equals", "alsa_card.pci-0000_50_00.0" },
-    #      },
-    #    },
-    #    apply_properties = {
-    #      ["device.disabled"] = true,
-    #    },
-    #  }
-    #  table.insert(alsa_monitor.rules,rule)
-    #'';
-  };
+  # pactl list
+  # - alsa_card.pci-0000_30_00.1:          HDA NVidia T1000
+  # - alsa_card.pci-0000_33_00.1:          Navi 21/23 HDMI/DP Audio Controller
+  # - alsa_card.pci-0000_26_00.0:          Magewell (00-00 Pro Capture Quad HDMI)
+  # - alsa_card.pci-0000_27_00.0:          Magewell (00-01 Pro Capture Quad HDMI)
+  # - alsa_card.pci-0000_28_00.0:          Magewell (00-02 Pro Capture Quad HDMI) (keep, device capture is used by OBS Studio)
+  # - alsa_card.pci-0000_29_00.0:          Magewell (00-03 Pro Capture Quad HDMI)
+  home.file.".config/wireplumber/main.lua.d/51-disable-cards.lua".text = ''
+    rule = {
+      matches = {
+        {
+          { "device.name", "equals", "alsa_card.pci-0000_30_00.1" },
+          { "device.name", "equals", "alsa_card.pci-0000_33_00.1" },
+          { "device.name", "equals", "alsa_card.pci-0000_26_00.0" },
+          { "device.name", "equals", "alsa_card.pci-0000_27_00.0" },
+          { "device.name", "equals", "alsa_card.pci-0000_29_00.0" },
+        },
+      },
+      apply_properties = {
+        ["device.disabled"] = true,
+      },
+    }
+    table.insert(alsa_monitor.rules,rule)
+  '';
 
   dconf.settings = {
     "org/gnome/desktop/background" = {
