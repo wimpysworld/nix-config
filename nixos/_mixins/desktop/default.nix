49086aa0f9e56815dc182c94ca4c2611105144c2
Martin Wimpress <martin@wimpress.org>
2024-02-07 11:15:56 +0000

    refactor: separate base configuration from live iso media

--- nixos/_mixins/desktop/default.nix
+++ nixos/_mixins/desktop/default.nix
@@ -11,6 +11,46 @@
     };
   };
 
+  fonts = {
+    # Enable a basic set of fonts providing several font styles and families and reasonable coverage of Unicode.
+    enableDefaultPackages = false;
+    fontDir.enable = true;
+    packages = with pkgs; [
+      (nerdfonts.override { fonts = [ "FiraCode" "SourceCodePro" "UbuntuMono" ]; })
+      fira
+      fira-go
+      joypixels
+      liberation_ttf
+      noto-fonts-emoji
+      source-serif
+      ubuntu_font_family
+      work-sans
+    ];
+
+    fontconfig = {
+      antialias = true;
+      defaultFonts = {
+        serif = [ "Source Serif" ];
+        sansSerif = [ "Work Sans" "Fira Sans" "FiraGO" ];
+        monospace = [ "FiraCode Nerd Font Mono" "SauceCodePro Nerd Font Mono" ];
+        emoji = [ "Joypixels" "Noto Color Emoji" ];
+      };
+      enable = true;
+      hinting = {
+        autohint = false;
+        enable = true;
+        style = "slight";
+      };
+      subpixel = {
+        rgba = "rgb";
+        lcdfilter = "light";
+      };
+    };
+  };
+
+  # Accept the joypixels license
+  nixpkgs.config.joypixels.acceptLicense = true;
+
   hardware = {
     opengl = {
       enable = true;
