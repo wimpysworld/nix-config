5fe9c8d02bc958464a00bbe2ee3c73f8a178579e
Martin Wimpress <martin@wimpress.org>
2024-02-14 19:51:36 +0000

    feat: apply low latency tweaks to pipewire; inspired by musnix

--- nixos/_mixins/services/pipewire.nix
+++ nixos/_mixins/services/pipewire.nix
@@ -1,5 +1,11 @@
 { desktop, lib, pkgs, ... }:
 {
+  # Enable the threadirqs kernel parameter to reduce audio latency
+  # - Inpired by: https://github.com/musnix/musnix/blob/master/modules/base.nix#L56
+  boot = {
+    kernelParams = [ "threadirqs" ];
+  };
+
   environment.systemPackages = with pkgs; [
     pulseaudio
   ] ++ lib.optionals (desktop != null) [
@@ -9,7 +15,19 @@
   hardware = {
     pulseaudio.enable = lib.mkForce false;
   };
-  security.rtkit.enable = true;
+
+  security = {
+    # Allow members of the "audio" group to set RT priorities
+    # Inspired by musnix: https://github.com/musnix/musnix/blob/master/modules/base.nix#L87
+    pam.loginLimits = [
+      { domain = "@audio"; item = "memlock"; type = "-"   ; value = "unlimited"; }
+      { domain = "@audio"; item = "rtprio" ; type = "-"   ; value = "99"       ; }
+      { domain = "@audio"; item = "nofile" ; type = "soft"; value = "99999"    ; }
+      { domain = "@audio"; item = "nofile" ; type = "hard"; value = "99999"    ; }
+    ];
+    rtkit.enable = true;
+  };
+
   services = {
     pipewire = {
       enable = true;
@@ -18,5 +36,11 @@
       pulse.enable = true;
       wireplumber.enable = true;
     };
+    # Expose important timers the members of the "audio" group
+    # Inspired by musnix: https://github.com/musnix/musnix/blob/master/modules/base.nix#L94
+    udev.extraRules = ''
+      KERNEL=="rtc0", GROUP="audio"
+      KERNEL=="hpet", GROUP="audio"
+      '';
   };
 }
