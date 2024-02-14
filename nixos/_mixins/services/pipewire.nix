31ff6af9f1e90b643f552ae5e098959f3e3d9198
Martin Wimpress <martin@wimpress.org>
2024-02-14 19:50:29 +0000

    chore!: disable jack in pipewire

--- nixos/_mixins/services/pipewire.nix
+++ nixos/_mixins/services/pipewire.nix
@@ -14,7 +14,7 @@
     pipewire = {
       enable = true;
       alsa.enable = true;
-      jack.enable = true;
+      jack.enable = false;
       pulse.enable = true;
       wireplumber.enable = true;
     };
