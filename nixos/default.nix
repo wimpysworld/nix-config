4920b0cb98b2d7876c7478b21910902eda290d5f
Martin Wimpress <martin@wimpress.org>
2024-02-14 19:52:21 +0000

    feat: enable Multi-Gen LRU

--- nixos/default.nix
+++ nixos/default.nix
@@ -222,6 +222,24 @@
     secrets.test-key = {};
   };
 
+  # Enable Multi-Gen LRU:
+  # - https://docs.kernel.org/next/admin-guide/mm/multigen_lru.html
+  # - Inspired by: https://github.com/hakavlad/mg-lru-helper
+  systemd.services."mglru" = {
+    enable = true;
+    wantedBy = ["basic.target"];
+    script = ''
+      ${pkgs.coreutils-full}/bin/echo 1000 > /sys/kernel/mm/lru_gen/min_ttl_ms
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+    };
+    unitConfig = {
+      ConditionPathExists = "/sys/kernel/mm/lru_gen/enabled";
+      Description = "Configure Enable Multi-Gen LRU";
+    };
+  };
+
   systemd.tmpfiles.rules = [
     "d /nix/var/nix/profiles/per-user/${username} 0755 ${username} root"
     "d /mnt/snapshot/${username} 0755 ${username} users"
