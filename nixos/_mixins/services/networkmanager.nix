89327fcdbf6ffb2429095bd736eac0e7e49b51b3
Martin Wimpress <martin@wimpress.org>
2024-02-16 16:49:03 +0000

    feat: per user DNS configurations

--- nixos/_mixins/services/networkmanager.nix
+++ nixos/_mixins/services/networkmanager.nix
@@ -1,11 +1,40 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
+let
+  # Define DNS settings for specific users
+  # - https://cleanbrowsing.org/filters/
+  userDnsSettings = {
+    # Security Filter:
+    # - Blocks access to phishing, spam, malware and malicious domains.
+    martin = [ "185.228.168.9" "185.228.169.9" ];
+
+    # Adult Filter:
+    # - Blocks access to all adult, pornographic and explicit sites.
+    # - It does not block proxy or VPNs, nor mixed-content sites.
+    # - Sites like Reddit are allowed.
+    # - Google and Bing are set to the Safe Mode.
+    # - Malicious and Phishing domains are blocked.
+    louise = [ "185.228.168.10" "185.228.169.11" ];
+
+    # Family Filter:
+    # - Blocks access to all adult, pornographic and explicit sites.
+    # - It also blocks proxy and VPN domains that are used to bypass the filters.
+    # - Mixed content sites (like Reddit) are also blocked.
+    # - Google, Bing and Youtube are set to the Safe Mode.
+    # - Malicious and Phishing domains are blocked.
+    agatha = [ "185.228.168.168" "185.228.169.168" ];
+  };
+  # Default DNS settings if user not listed above
+  defaultDns = [ "1.1.1.1" "1.0.0.1" ];
+in
 {
   networking = {
     networkmanager = {
       enable = true;
-      # Use AdGuard Public DNS with ad/tracker blocking
-      #  - https://adguard-dns.io/en/public-dns.html
-      insertNameservers = [ "94.140.14.14" "94.140.15.15" ];
+      # Conditionally set Public DNS based on username, defaulting if user not matched
+      insertNameservers = if builtins.hasAttr username userDnsSettings then
+                             userDnsSettings.${username}
+                           else
+                             defaultDns;
       wifi = {
         backend = "iwd";
         powersave = false;
