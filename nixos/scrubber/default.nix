3a001f3f1056cf3ec179993c7d6a97653ea7532a
Martin Wimpress <martin@wimpress.org>
2024-02-22 09:15:16 +0000

    chore: drop fileSystem and swapDevices config for VMs

--- nixos/scrubber/default.nix
+++ nixos/scrubber/default.nix
@@ -8,22 +8,6 @@
     ../_mixins/services/pipewire.nix
   ];
 
-  # disko does manage mounting, but I want to mount by-label
-  fileSystems."/" = lib.mkForce {
-    device = "/dev/disk/by-label/root";
-    fsType = "bcachefs";
-    options = [ "defaults" "relatime" "nodiratime" ];
-  };
-
-  fileSystems."/boot" = lib.mkForce {
-    device = "/dev/disk/by-label/ESP";
-    fsType = "vfat";
-  };
-
-  swapDevices = lib.mkForce [{
-    device = "/dev/disk/by-label/swap";
-  }];
-
   boot = {
     initrd.availableKernelModules = [ "xhci_pci" "ohci_pci" "ehci_pci" "virtio_pci" "ahci" "usbhid" "sr_mod" "virtio_blk" ];
   };
