3de56094655dc83c29ed6af11a8b1a61090b66e3
Martin Wimpress <martin@wimpress.org>
2024-02-23 13:02:52 +0000

    feat: add boot-host script to apply but not activate a nixos rebuild

---
+++ nixos/_mixins/scripts/boot-host.nix
@@ -0,0 +1,13 @@
+{ pkgs }:
+
+pkgs.writeScriptBin "boot-host" ''
+#!${pkgs.stdenv.shell}
+
+if [ -e $HOME/Zero/nix-config ]; then
+  pushd $HOME/Zero/nix-config
+  sudo ${pkgs.unstable.nixos-rebuild}/bin/nixos-rebuild boot --flake .#
+  popd
+else
+  ${pkgs.coreutils-full}/bin/echo "ERROR! No nix-config found in $HOME/Zero/nix-config"
+fi
+''
