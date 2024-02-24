8697fa811657d92f601de54a83db4395c0ec89c6
Martin Wimpress <martin@wimpress.org>
2024-02-24 19:07:21 +0000

    chore: do not disable ZFS

--- nixos/_mixins/linux/latest.nix
+++ nixos/_mixins/linux/latest.nix
@@ -1,20 +1,9 @@
 { lib, pkgs, ... }:
 {
-  # Create a bootable ISO image with bcachefs but no ZFS.
+  # Create a bootable ISO image with bcachefs.
   # - https://nixos.wiki/wiki/Bcachefs
-  # - https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix
   boot.supportedFilesystems = [ "bcachefs" ];
   boot.kernelPackages = lib.mkOverride 0 pkgs.linuxPackages_latest;
-  # Makes `availableOn` fail for zfs, see <nixos/modules/profiles/base.nix>.
-  # This is a workaround since we cannot remove the `"zfs"` string from `supportedFilesystems`.
-  # The proper fix would be to make `supportedFilesystems` an attrset with true/false which we
-  # could then `lib.mkForce false`
-  nixpkgs.overlays = [(_final: super: {
-    zfs = super.zfs.overrideAttrs(_: {
-      meta.platforms = [];
-    });
-  })];
-
   environment.systemPackages = with pkgs; [
     unstable.bcachefs-tools
     keyutils
