fd3c45ec5ae57cf1a3385d81fac9a46769770f26
Martin Wimpress <martin@wimpress.org>
2024-02-26 10:11:53 +0000

    refactor: cups service now enables appropriate desktop integration

--- nixos/_mixins/services/cups.nix
+++ nixos/_mixins/services/cups.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }: {
+{ desktop, lib, pkgs, ... }: {
   imports = [
     ./avahi.nix
   ];
+
+  programs = lib.mkIf (desktop == "mate") {
+    system-config-printer.enable = true;
+  };
+
   services = {
     printing.enable = true;
     printing.drivers = with pkgs; [ gutenprint hplip ];
+    system-config-printer.enable = true;
   };
 }
