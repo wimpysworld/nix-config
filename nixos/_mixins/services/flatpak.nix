59ef94c6121b20e50c914e15dc815a7c8a673fcf
Martin Wimpress <martin@wimpress.org>
2024-02-26 10:12:58 +0000

    refactor: flatpak service conditionally enables appcenter

--- nixos/_mixins/services/flatpak.nix
+++ nixos/_mixins/services/flatpak.nix
@@ -1,10 +1,18 @@
-{ pkgs, ... }: {
+{ desktop, lib, pkgs, ... }: {
   services.flatpak.enable = true;
   systemd.services.configure-flathub-repo = {
     wantedBy = ["multi-user.target"];
     path = [ pkgs.flatpak ];
     script = ''
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
+  systemd.services.configure-appcenter-repo = lib.mkIf (desktop == "pantheon") {
+    wantedBy = ["multi-user.target"];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists appcenter https://flatpak.elementary.io/repo.flatpakrepo
     '';
   };
 }
