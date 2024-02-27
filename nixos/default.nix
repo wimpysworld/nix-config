8bd980e13aa0fb46be18a0d0a144aa14e71ffb77
Martin Wimpress <martin@wimpress.org>
2024-02-27 12:53:40 +0000

    refactor: consolidate default kernel configuration

--- nixos/default.nix
+++ nixos/default.nix
@@ -20,7 +20,6 @@ in
     (modulesPath + "/installer/scan/not-detected.nix")
     ./${hostname}
     ./_mixins/base
-    ./_mixins/kernel
     ./_mixins/scripts
     ./_mixins/users/root
   ]
@@ -38,9 +37,12 @@ in
       "rd.udev.log_level=3"
       "udev.log_priority=3"
     ];
+    kernelPackages = pkgs.linuxPackages_latest;
     kernel.sysctl = {
       "net.ipv4.ip_forward" = 1;
       "net.ipv6.conf.all.forwarding" = 1;
+      # Keep zram swap (lz4) latency in check
+      "vm.page-cluster" = 1;
     };
   };
 
@@ -278,6 +280,18 @@ in
       ConditionPathExists = "/sys/kernel/mm/lru_gen/enabled";
       Description = "Configure Enable Multi-Gen LRU";
     };
+  };
+
+  # Disable hiberate and hybrid-sleep when using zram.
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+  # Enable zram
+  # - https://github.com/ecdye/zram-config/blob/main/README.md#performance
+  # - https://www.reddit.com/r/Fedora/comments/mzun99/new_zram_tuning_benchmarks/
+  # - https://linuxreviews.org/Zram
+  zramSwap = {
+    algorithm = "lz4";
+    enable = true;
   };
 
   systemd.tmpfiles.rules = [
