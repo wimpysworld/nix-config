409e06a9bcbd7f249f0f1a5ba3f95997627dc4a3
Martin Wimpress <martin@wimpress.org>
2024-02-28 00:48:40 +0000

    fix: always enable dconf so desktop configuration always get applied

--- home-manager/_mixins/users/martin/hosts/phasma.nix
+++ home-manager/_mixins/users/martin/hosts/phasma.nix
@@ -40,7 +40,7 @@ with lib.hm.gvariant;
     '';
   };
 
-  dconf.settings = lib.mkIf (desktop != null) {
+  dconf.settings = {
     "org/gnome/desktop/background" = {
       picture-options = "zoom";
       picture-uri = "file://${config.home.homeDirectory}/Pictures/Determinate/DeterminateColorway-3440x1440.png";
