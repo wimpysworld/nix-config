10d4c6dcf14d444f350d17c9b9edac883acc62af
Martin Wimpress <martin@wimpress.org>
2024-02-28 11:15:43 +0000

    refactor: integrate evolution in mate configuration

--- nixos/_mixins/desktop/evolution.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  environment = {
-    systemPackages = with pkgs; [
-      evolutionWithPlugins
-    ];
-  };
-  programs.evolution.enable = true;
-  services.gnome.evolution-data-server.enable = true;
-}
