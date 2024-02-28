db20da3a98cbdf0840e2ecf199af723068baf22c
Martin Wimpress <martin@wimpress.org>
2024-02-28 01:35:13 +0000

    fix: correct regression where per-user profile directory was not created

--- nixos/default.nix
+++ nixos/default.nix
@@ -353,7 +353,7 @@ in
   };
 
   systemd.tmpfiles.rules = [
-    "d /mnt/snapshot/${username} 0755 ${username} users"
+    "d /nix/var/nix/profiles/per-user/${username} 0755 ${username} root"
   ];
 
   system = {
