8e4b44f405c824fca37fc87e4b8da8597a8e881a
Martin Wimpress <martin@wimpress.org>
2024-02-29 11:00:05 +0000

    fix: switch-all activates home-manager last
    Ensure that user dconf.settings are applied

--- nixos/_mixins/scripts/switch-all.nix
+++ nixos/_mixins/scripts/switch-all.nix
@@ -6,8 +6,8 @@ pkgs.writeScriptBin "switch-all" ''
 if [ -e $HOME/Zero/nix-config ]; then
   sudo ${pkgs.coreutils-full}/bin/true
   pushd $HOME/Zero/nix-config
-  ${pkgs.home-manager}/bin/home-manager switch -b backup --flake $HOME/Zero/nix-config
   sudo ${pkgs.unstable.nixos-rebuild}/bin/nixos-rebuild switch --flake .#
+  ${pkgs.home-manager}/bin/home-manager switch -b backup --flake $HOME/Zero/nix-config
   popd
 else
   ${pkgs.coreutils-full}/bin/echo "ERROR! No nix-config found in $HOME/Zero/nix-config"
