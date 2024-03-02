45f2617a159264bd3f3f345e5196c18b265f4448
Martin Wimpress <martin@wimpress.org>
2024-03-02 13:39:26 +0000

    fix: prevent mbrola-voices (~650MB) from getting installed on live media

--- nixos/iso-console/default.nix
+++ nixos/iso-console/default.nix
@@ -4,4 +4,11 @@
     ../_mixins/kernel/bcachefs.nix
     ../_mixins/kernel/no-zfs.nix
   ];
+
+  # Prevent mbrola-voices (~650MB) from being on the live media
+  nixpkgs.overlays = [(_final: super: {
+    espeak = super.espeak.override {
+      mbrolaSupport = false;
+    };
+  })];
 }
