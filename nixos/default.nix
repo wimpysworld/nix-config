efd24525a85e9d308208abb82ad75b9613dfbe8c
Martin Wimpress <martin@wimpress.org>
2024-03-14 17:47:02 +0000

    fix: update nixos-needsreboot

--- nixos/default.nix
+++ nixos/default.nix
@@ -376,7 +376,7 @@ in
         if [ -e /run/current-system/boot.json ] && ! ${pkgs.gnugrep}/bin/grep -q "LABEL=nixos-minimal" /run/current-system/boot.json; then
           ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
         fi
-        /run/current-system/sw/bin/nixos-reedsreboot
+        /run/current-system/sw/bin/nixos-needsreboot
       '';
     };
     nixos.label = lib.mkIf (isInstall) "-";
