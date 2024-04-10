c34d31369d5e4862ccccf02593834eca789ccf28
Martin Wimpress <martin@wimpress.org>
2024-04-10 10:09:33 +0100

    refactor: update OBS source clone plugin to use recent fixes

--- pkgs/obs-studio/plugins/obs-source-clone.nix
+++ pkgs/obs-studio/plugins/obs-source-clone.nix
@@ -5,15 +5,15 @@
 , obs-studio
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "obs-source-clone";
-  version = "0.1.4";
+  version = "0.1.4-unstable-2024-02-19";
 
   src = fetchFromGitHub {
     owner = "exeldro";
     repo = "obs-source-clone";
-    rev = version;
-    sha256 = "sha256-E2pHJO3cdOXmSlTVGsz4tncm9fMaa8Rhsq9YZDNidjs=";
+    rev = "d1524d5d932d6841a1fbd6061cc4a0033fb615b7";
+    hash = "sha256-W9IIIGQdreI2FQGii5NUB5tVHcqsiYAKTutOHEPCyms=";
   };
 
   nativeBuildInputs = [ cmake ];
