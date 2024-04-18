1fbd0714642fb8f435ded39dc59c0295c395e854
Martin Wimpress <martin@wimpress.org>
2024-04-18 10:39:39 +0100

    refactor: update build-iso script to use nom

--- nixos/_mixins/configs/build-iso.nix
+++ nixos/_mixins/configs/build-iso.nix
@@ -12,7 +12,7 @@ if [ -e $HOME/Zero/nix-config ]; then
   build_cores=$(printf "%.0f" $(echo "$all_cores * 0.75" | bc))
   pushd $HOME/Zero/nix-config 2>&1 > /dev/null
   echo "Building ISO ($1) with $build_cores cores"
-  nix build .#nixosConfigurations.iso-$1.config.system.build.isoImage -L --cores $build_cores
+  nom build .#nixosConfigurations.iso-$1.config.system.build.isoImage -L --cores $build_cores
   ISO=$(${pkgs.coreutils-full}/bin/head -n1 result/nix-support/hydra-build-products | ${pkgs.coreutils-full}/bin/cut -d'/' -f6)
   ${pkgs.coreutils-full}/bin/mkdir -p $HOME/Quickemu/nixos-$1 2>/dev/null
   ${pkgs.coreutils-full}/bin/cp result/iso/$ISO $HOME/Quickemu/nixos-$1/nixos.iso
