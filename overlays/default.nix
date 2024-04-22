8e3c9012041f3e930077324257113b350c375973
Martin Wimpress <martin@wimpress.org>
2024-04-22 14:19:37 +0100

    fix: override the linuxPackages package set with new mwprocapture hash
    https://github.com/NixOS/nixpkgs/pull/305990

--- overlays/default.nix
+++ overlays/default.nix
@@ -54,6 +54,19 @@
       patches = [ ./darwin.patch ];
     });
 
+    linuxPackages_latest = prev.linuxPackages_latest.extend (lpself: lpsuper: {
+      mwprocapture = lpsuper.mwprocapture.overrideAttrs ( _old: rec {
+        pname = "mwprocapture";
+        subVersion = "4390";
+        version = "1.3.0.${subVersion}";
+
+        src = prev.fetchurl {
+          url = "https://www.magewell.com/files/drivers/ProCaptureForLinux_${subVersion}.tar.gz";
+          sha256 = "sha256-a2cU7PYQh1KR5eeMhMNx2Sc3HHd7QvCG9+BoJyVPp1Y=";
+        };
+      });
+    });
+
     nelua = prev.nelua.overrideAttrs ( _old: rec {
       pname = "nelua";
       version = "unstable-2023-11-19";
