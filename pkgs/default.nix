48e0e446624148406c34bff7133b94f481a7a7a4
Martin Wimpress <martin@wimpress.org>
2024-05-30 12:37:39 +0100

    chore: remove gitkraken from local pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,6 @@
 # Build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 
 { pkgs ? (import ../nixpkgs.nix) { } }: {
-  gitkraken = pkgs.callPackage ./gitkraken { };
   libdatachannel = pkgs.callPackage ./libdatachannel { };
   lima-bin = pkgs.callPackage ./lima-bin { };
   obs-studio = pkgs.callPackage ./obs-studio { };
