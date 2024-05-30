d512cfcba24e1f316391d92553fbea248d9c44f4
Martin Wimpress <martin@wimpress.org>
2024-05-30 12:10:40 +0100

    chore: remove keybase and keybase-gui from overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,28 +9,6 @@
   # https://nixos.wiki/wiki/Overlays
   modifications = _final: prev: {
 
-    # https://github.com/NixOS/nixpkgs/issues/278277#issuecomment-1878292158
-    keybase = prev.keybase.overrideAttrs ( _old: rec {
-      pname = "keybase";
-      version = "6.2.4";
-      src = prev.fetchFromGitHub {
-        owner = "keybase";
-        repo = "client";
-        rev = "v${version}";
-        hash = "sha256-z7vpCUK+NU7xU9sNBlQnSy9sjXD7/m8jSRKfJAgyyN8=";
-      };
-    });
-
-    keybase-gui = prev.keybase-gui.overrideAttrs ( _old: rec {
-      pname = "keybase-gui";
-      version = "6.2.4";
-      versionSuffix = "20240101011938.ae7e4a1c15";
-      src = prev.fetchurl {
-        url = "https://s3.amazonaws.com/prerelease.keybase.io/linux_binaries/deb/keybase_${version + "-" + versionSuffix}_amd64.deb";
-        hash = "sha256-XyGb9F83z8+OSjxOaO5k+h2qIY78ofS/ZfTXki54E5Q=";
-      };
-    });
-
     librist = prev.librist.overrideAttrs ( _old: rec {
       pname = "librist";
       version = "0.2.10";
