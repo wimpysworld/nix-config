d8a79bcbbf8054feb6434ccccda8fb7f23d7acc8
Martin Wimpress <martin@wimpress.org>
2024-06-02 10:23:08 +0100

    feat: add wavebox and gitkraken local pkgs
    Wavebox is cleaned up, removing Qt5, Qt6 and Gtk4.

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,7 +8,6 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = _final: prev: {
-
     #linuxPackages_latest = prev.linuxPackages_latest.extend (_lpself: lpsuper: {
     #  mwprocapture = lpsuper.mwprocapture.overrideAttrs ( _old: rec {
     #    pname = "mwprocapture";
@@ -21,14 +20,14 @@
     #  });
     #});
 
-    wavebox = prev.wavebox.overrideAttrs ( _old: rec {
-      pname = "wavebox";
-      version = "10.125.28-2";
-      src = prev.fetchurl {
-        url = "https://download.wavebox.app/stable/linux/tar/Wavebox_${version}.tar.gz";
-        sha256 = "sha256-8X17WLa1q2c7FQD61e5wYZKOYnHA9sSvfGrtARACGZc=";
-      };
-    });
+    #wavebox = prev.wavebox.overrideAttrs ( _old: rec {
+    #  pname = "wavebox";
+    #  version = "10.125.28-2";
+    #  src = prev.fetchurl {
+    #    url = "https://download.wavebox.app/stable/linux/tar/Wavebox_${version}.tar.gz";
+    #    sha256 = "sha256-8X17WLa1q2c7FQD61e5wYZKOYnHA9sSvfGrtARACGZc=";
+    #  };
+    #});
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
