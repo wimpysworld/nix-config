bc3f34f6f765b51a0fc9c82f264d171df2e9fede
Martin Wimpress <martin@wimpress.org>
2024-06-02 14:14:57 +0100

    fix(pinentry): use pinentry-gnome3 for mate and pantheon

--- home-manager/_mixins/desktop/mate/default.nix
+++ home-manager/_mixins/desktop/mate/default.nix
@@ -47,7 +47,7 @@
   };
 
   services = {
-    gpg-agent.pinentryPackage = lib.mkForce pkgs.gcr;
+    gpg-agent.pinentryPackage = lib.mkForce pkgs.pinentry-gnome3;
     # https://nixos.wiki/wiki/Bluetooth#Using_Bluetooth_headsets_with_PulseAudio
     mpris-proxy.enable = true;
   };
--- home-manager/_mixins/desktop/pantheon/default.nix
+++ home-manager/_mixins/desktop/pantheon/default.nix
@@ -74,7 +74,7 @@ StartupNotify=false";
   };
 
   services = {
-    gpg-agent.pinentryPackage = lib.mkForce pkgs.gcr;
+    gpg-agent.pinentryPackage = lib.mkForce pkgs.pinentry-gnome3;
     # https://nixos.wiki/wiki/Bluetooth#Using_Bluetooth_headsets_with_PulseAudio
     mpris-proxy.enable = true;
   };
