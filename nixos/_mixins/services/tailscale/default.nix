709ad40ad84dfcb9d22e8728f113ac4c832151d3
Martin Wimpress <martin@wimpress.org>
2024-06-07 17:17:57 +0100

    feat: install trayscale on workstations, configure exit-nodes and operator

--- nixos/_mixins/services/tailscale/default.nix
+++ nixos/_mixins/services/tailscale/default.nix
@@ -1,8 +1,17 @@
-{ config, ... }: {
-  # drongo-gamma.ts.net
+{ config, desktop, hostname, lib, pkgs, username, ... }:
+let
+  isWorkstation = if (desktop != null) then true else false;
+in
+{
+  imports = lib.optional (builtins.pathExists (./. + "/${hostname}.nix")) ./${hostname}.nix;
+  environment = lib.mkIf (isWorkstation) {
+    systemPackages = with pkgs; [
+      trayscale
+    ];
+  };
   services.tailscale = {
       enable = true;
-      extraUpFlags = [ "--accept-routes" "--ssh" ];
+      extraUpFlags = [ "--accept-routes" "--operator ${username}" "--ssh" ];
       useRoutingFeatures = "client";
   };
   networking = {
