a1db9f84acf0db1d5155e96d9b30f9bcb0d84207
Martin Wimpress <martin@wimpress.org>
2024-06-10 20:25:23 +0100

    fix: add dependencies to build and switch scripts

--- nixos/_mixins/configs/build-host.nix
+++ nixos/_mixins/configs/build-host.nix
@@ -4,10 +4,10 @@ pkgs.writeScriptBin "build-host" ''
 #!${pkgs.stdenv.shell}
 
 if [ -e $HOME/Zero/nix-config ]; then
-  all_cores=$(nproc)
-  build_cores=$(printf "%.0f" $(echo "$all_cores * 0.75" | bc))
+  all_cores=$(${pkgs.coreutils-full}/bin/nproc)
+  build_cores=$(printf "%.0f" $(echo "$all_cores * 0.75" | ${pkgs.bc}/bin/bc))
   echo "Building NixOS with $build_cores cores"
-  nh os switch --ask ~/Zero/nix-config/ -- --cores $build_cores
+  ${pkgs.nh}/bin/nh os switch --ask ~/Zero/nix-config/ -- --cores $build_cores
 else
   ${pkgs.coreutils-full}/bin/echo "ERROR! No nix-config found in $HOME/Zero/nix-config"
 fi
