67e2650b162b08b1490cfe99b0295b84b70c4cbf
Martin Wimpress <martin@wimpress.org>
2024-06-13 19:11:27 +0100

    feat: caddy reverse proxy rules

--- nixos/_mixins/services/cockpit/default.nix
+++ nixos/_mixins/services/cockpit/default.nix
@@ -6,6 +6,10 @@
     port = 9090;
     settings = {
       WebService = {
+        Origins = "https://${hostname}.drongo-gama.ts.net wss://${hostname}.drongo-gama.ts.net http://127.0.0.1:9090";
+        ProtocolHeader = "X-Forwarded-Proto";
+        #ForwardedForHeader = "X-Forwarded-For";
+        UrlRoot = "/cockpit";
         AllowUnencrypted = true;
       };
     };
