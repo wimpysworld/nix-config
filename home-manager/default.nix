481b256bec856c049e30143c7a3cfc40da7c1dbb
Martin Wimpress <martin@wimpress.org>
2024-06-17 13:46:12 +0100

    feat: enable catppuccin for gitui

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -410,32 +410,8 @@ in
       ];
     };
     gitui = {
+      catppuccin.enable = true;
       enable = true;
-      theme = ''
-      (
-        selected_tab: Reset,
-        command_fg: Black,
-        selection_bg: Blue,
-        selection_fg: White,
-        cmdbar_bg: Yellow,
-        cmdbar_extra_lines_bg: Yellow,
-        disabled_fg: DarkGray,
-        diff_line_add: Green,
-        diff_line_delete: Red,
-        diff_file_added: LightGreen,
-        diff_file_removed: LightRed,
-        diff_file_moved: LightMagenta,
-        diff_file_modified: Yellow,
-        commit_hash: Magenta,
-        commit_time: LightCyan,
-        commit_author: Green,
-        danger_fg: Red,
-        push_gauge_bg: Blue,
-        push_gauge_fg: Reset,
-        tag_fg: LightMagenta,
-        branch_fg: LightYellow,
-      )
-      '';
     };
     gpg.enable = true;
     home-manager.enable = true;
