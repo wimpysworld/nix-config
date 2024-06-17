39db06dde797cb499b0f3d3895e57c1d97a3e385
Martin Wimpress <martin@wimpress.org>
2024-06-17 13:44:42 +0100

    feat: enable catppuccin for cava

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -281,21 +281,8 @@ in
       };
     };
     cava = {
+      catppuccin.enable = true;
       enable = isLinux;
-      settings = {
-        color = {
-          gradient = 1;
-          gradient_count = 8;
-          gradient_color_1 = "'#59cc33'";
-          gradient_color_2 = "'#80cc33'";
-          gradient_color_3 = "'#a6cc33'";
-          gradient_color_4 = "'#cccc33'";
-          gradient_color_5 = "'#cca633'";
-          gradient_color_6 = "'#cc8033'";
-          gradient_color_7 = "'#cc5933'";
-          gradient_color_8 = "'#cc3333'";
-        };
-      };
     };
     dircolors = {
       enable = true;
