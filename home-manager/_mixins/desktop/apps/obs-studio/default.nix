5b742286bf4f0a3eed8f9ae8c3ed9167e4943f82
Martin Wimpress <martin@wimpress.org>
2024-06-19 18:54:33 +0100

    feat: add catppuccin theme to obs-studio

--- home-manager/_mixins/desktop/apps/obs-studio/default.nix
+++ home-manager/_mixins/desktop/apps/obs-studio/default.nix
@@ -2,11 +2,18 @@
 let
   inherit (pkgs.stdenv) isLinux;
   isStreamstation = if (hostname == "phasma" || hostname == "vader") then true else false;
+  themes = pkgs.lib.cleanSource ./themes;
 in
 {
   # Deckmaster and the utilities I bind to the Stream Deck
   home = lib.mkIf (isStreamstation) {
-    file."/Studio/OBS/config/obs-studio/.keep".text = "";
+    file = {
+      "/Studio/OBS/config/obs-studio/.keep".text = "";
+      "${config.xdg.configHome}/obs-studio/themes" = {
+        source = themes;
+        recursive = true;
+      };
+    };
     packages = with pkgs; [
       alsa-utils
       bc
