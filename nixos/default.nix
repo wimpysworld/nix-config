6c54d83a7721da6d7dd02b19e142a5a8fddd4910
Martin Wimpress <martin@wimpress.org>
2024-06-20 11:39:36 +0100

    feat: add catpuccin to tty

--- nixos/default.nix
+++ nixos/default.nix
@@ -41,6 +41,9 @@ in
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
       "udev.log_priority=3"
+      "vt.default_red=30,243,166,249,137,245,148,186,88,243,166,249,137,245,148,166"
+      "vt.default_grn=30,139,227,226,180,194,226,194,91,139,227,226,180,194,226,173"
+      "vt.default_blu=46,168,161,175,250,231,213,222,112,168,161,175,250,231,213,200"
     ];
     kernelPackages = pkgs.linuxPackages_latest;
     kernel.sysctl = {
