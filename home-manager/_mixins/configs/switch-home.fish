ea977b4341b7cf3cf1f15ca7522d0c08a79dcfe7
Martin Wimpress <martin@wimpress.org>
2024-06-20 19:29:27 +0100

    fix: provide backup extension for files replaced when switching home

--- home-manager/_mixins/configs/switch-home.fish
+++ home-manager/_mixins/configs/switch-home.fish
@@ -5,7 +5,7 @@ function switch-home
         # Calculate 75% of the number of processing units
         set -l build_cores (math "round(0.75 * ($all_cores))")
         echo "Switch Home Manager with $build_cores cores"
-        nh home switch ~/Zero/nix-config/ -- --cores $build_cores
+        nh home switch --backup-extension backup ~/Zero/nix-config/ -- --cores $build_cores
     else
         echo "ERROR! No nix-config found in $HOME/Zero/nix-config"
     end
