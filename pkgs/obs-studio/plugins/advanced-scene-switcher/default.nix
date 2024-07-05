77e2a91385ca23de4d76f541779b63ccef0aca3e
Martin Wimpress <martin@wimpress.org>
2024-07-05 11:59:27 +0100

    feat(obs): update advanced-scene-switcher to 1.26.4

--- pkgs/obs-studio/plugins/advanced-scene-switcher/default.nix
+++ pkgs/obs-studio/plugins/advanced-scene-switcher/default.nix
@@ -23,13 +23,13 @@
 
 stdenv.mkDerivation rec {
   pname = "advanced-scene-switcher";
-  version = "1.25.5";
+  version = "1.26.4";
 
   src = fetchFromGitHub {
     owner = "WarmUpTill";
     repo = "SceneSwitcher";
     rev = version;
-    hash = "sha256-ROR+R1Zak8XkhFk1+Pyi0lB+JZI4SVtKGin4vem7NEE=";
+    hash = "sha256-qUKywOfUiGKBmyREns2LnWECGeFhdA4ecpgGJQAIKpU=";
   };
 
   nativeBuildInputs = [
