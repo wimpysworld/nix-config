d5464eda28de1524744acb9a16ebef45b7ca0400
Martin Wimpress <martin@wimpress.org>
2024-07-08 10:28:41 +0100

    refactor: make bluetooth a feature

---
+++ nixos/_mixins/features/bluetooth/default.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  hardware = {
+    # https://nixos.wiki/wiki/Bluetooth
+    bluetooth = {
+      enable = true;
+      package = pkgs.bluez;
+      settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+          Experimental = true;
+        };
+      };
+    };
+  };
+}
