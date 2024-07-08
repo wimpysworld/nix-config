b2dbca755279ff722862a1e5f9e4671da9e63895
Martin Wimpress <martin@wimpress.org>
2024-07-08 23:26:40 +0100

    refactor: make console a feature

---
+++ nixos/_mixins/features/console/default.nix
@@ -0,0 +1,38 @@
+{ config, hostname, lib, pkgs, ... }:
+let
+  isInstall = if (builtins.substring 0 4 hostname != "iso-") then true else false;
+in
+{
+  boot = {
+    # Catppuccin theme
+    kernelParams = [
+      "vt.default_red=30,243,166,249,137,245,148,186,88,243,166,249,137,245,148,166"
+      "vt.default_grn=30,139,227,226,180,194,226,194,91,139,227,226,180,194,226,173"
+      "vt.default_blu=46,168,161,175,250,231,213,222,112,168,161,175,250,231,213,200"
+    ];
+  };
+
+  catppuccin = {
+    accent = "blue";
+    flavor = "mocha";
+  };
+
+  console = {
+    font = "${pkgs.tamzen}/share/consolefonts/TamzenForPowerline10x20.psf";
+    packages = with pkgs; [ tamzen ];
+  };
+
+  services = {
+    kmscon = lib.mkIf (isInstall) {
+      enable = true;
+      hwRender = config.services.kmscon.enable;
+      fonts = lib.mkIf (config.services.kmscon.enable) [{
+        name = "FiraCode Nerd Font Mono";
+        package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
+      }];
+      extraConfig = ''
+        font-size=14
+      '';
+    };
+  };
+}
