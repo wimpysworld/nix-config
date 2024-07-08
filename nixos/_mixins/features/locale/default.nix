c51d5e912e7db73d26e5e0728f54d0a109a495e0
Martin Wimpress <martin@wimpress.org>
2024-07-08 23:10:16 +0100

    refactor: make locale a feature

---
+++ nixos/_mixins/features/locale/default.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:
+let
+  locale = "en_GB.utf8";
+  xkbLayout = "gb";
+  timezoneHome = "Europe/London";
+  timezoneAway = "America/New_York";
+in
+{
+  console.keyMap = lib.mkIf (config.console.font != null) "uk";
+  i18n = {
+    defaultLocale = locale;
+    extraLocaleSettings = {
+      LC_ADDRESS = locale;
+      LC_IDENTIFICATION = locale;
+      LC_MEASUREMENT = locale;
+      LC_MONETARY = locale;
+      LC_NAME = locale;
+      LC_NUMERIC = locale;
+      LC_PAPER = locale;
+      LC_TELEPHONE = locale;
+      LC_TIME = locale;
+    };
+  };
+  services.xserver.xkb.layout = xkbLayout;
+  time.timeZone = timezoneHome;
+
+  services = {
+    kmscon = lib.mkIf (config.services.kmscon.enable) {
+      extraConfig = ''
+        xkb-layout=${xkbLayout}
+      '';
+    };
+  };
+}
