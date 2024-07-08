11a0e2c56fcb1484f73a46c41d27aebce0c8ae92
Martin Wimpress <martin@wimpress.org>
2024-07-08 10:35:41 +0100

    refactor: make zram a feature

--- nixos/default.nix
+++ nixos/default.nix
@@ -29,6 +29,7 @@ in
     (modulesPath + "/installer/scan/not-detected.nix")
     ./${hostname}
     ./_mixins/features/bluetooth
+    ./_mixins/features/zram
     ./_mixins/configs
     ./_mixins/users
   ] ++ lib.optional (isWorkstation) ./_mixins/desktop;
@@ -48,10 +49,6 @@ in
       "vt.default_blu=46,168,161,175,250,231,213,222,112,168,161,175,250,231,213,200"
     ];
     kernelPackages = pkgs.linuxPackages_latest;
-    kernel.sysctl = {
-      # Keep zram swap (lz4) latency in check
-      "vm.page-cluster" = 1;
-    };
     # Only enable the systemd-boot on installs, not live media (.ISO images)
     loader = lib.mkIf (isInstall) {
       efi.canTouchEfiVariables = true;
@@ -326,36 +323,6 @@ in
       test-key = {};
       homepage-env = {};
     };
-  };
-
-  # Enable Multi-Gen LRU:
-  # - https://docs.kernel.org/next/admin-guide/mm/multigen_lru.html
-  # - Inspired by: https://github.com/hakavlad/mg-lru-helper
-  systemd.services."mglru" = {
-    enable = true;
-    wantedBy = ["basic.target"];
-    script = ''
-      ${pkgs.coreutils-full}/bin/echo 1000 > /sys/kernel/mm/lru_gen/min_ttl_ms
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-    };
-    unitConfig = {
-      ConditionPathExists = "/sys/kernel/mm/lru_gen/enabled";
-      Description = "Configure Enable Multi-Gen LRU";
-    };
-  };
-
-  # Disable hiberate and hybrid-sleep when using zram.
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
-  # Enable zram
-  # - https://github.com/ecdye/zram-config/blob/main/README.md#performance
-  # - https://www.reddit.com/r/Fedora/comments/mzun99/new_zram_tuning_benchmarks/
-  # - https://linuxreviews.org/Zram
-  zramSwap = {
-    algorithm = "lz4";
-    enable = true;
   };
 
   systemd.tmpfiles.rules = [
