7350b1abbbe6f18a2333bd3ba383ae615170c2e5
Martin Wimpress <martin@wimpress.org>
2024-07-09 23:37:26 +0100

    refactor: determinate hasAmdGPU from config.hardware.amdgpu.initrd.enable

--- nixos/_mixins/features/gpu/default.nix
+++ nixos/_mixins/features/gpu/default.nix
@@ -3,7 +3,7 @@ let
   isInstall = if (builtins.substring 0 4 hostname != "iso-") then true else false;
   isWorkstation = if (desktop != null) then true else false;
   hasNvidiaGPU = lib.elem "nvidia" config.services.xserver.videoDrivers;
-  hasAmdGPU = lib.elem "amdgpu" config.services.xserver.videoDrivers;
+  hasAmdGPU = config.hardware.amdgpu.initrd.enable;
 in
 lib.mkIf (isInstall) {
   environment = {
