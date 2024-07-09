0cc819a3467ecea1833baf8da889e6123c4b5b92
Martin Wimpress <martin@wimpress.org>
2024-07-09 07:40:02 +0100

    feat: use lxd and tailscale config to define trustedInterfaces

--- nixos/_mixins/features/snapcraft/default.nix
+++ nixos/_mixins/features/snapcraft/default.nix
@@ -6,10 +6,6 @@ in
 lib.mkIf (isInstall && isWorkstation) {
   # Install snapcraft and enable snapd (for running snaps) and lxd (for building snaps)
   environment.systemPackages = with pkgs; [ snapcraft ];
-  # Trust the lxd bridge interface, if lxd is enabled
-  networking.firewall = lib.mkIf (config.virtualisation.lxd.enable) {
-    trustedInterfaces = [ "lxdbr0" ];
-  };
   services.snap.enable = true;
   virtualisation.lxd.enable = true;
 }
