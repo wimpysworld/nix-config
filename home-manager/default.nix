b76d47d744d4612f2ae98f77f8be5f712dcdba8a
Martin Wimpress <martin@wimpress.org>
2024-07-12 20:02:53 +0100

    chore: remove some verbose comments

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -171,18 +171,14 @@ in
   news.display = "silent";
 
   nixpkgs = {
-    # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
-
-      # Add overlays exported from other flakes:
     ];
     # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
     };
   };
