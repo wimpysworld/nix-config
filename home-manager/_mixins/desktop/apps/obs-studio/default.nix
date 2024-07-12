0c700a65e9d80e58ddda94056c542965541d2e1c
Martin Wimpress <martin@wimpress.org>
2024-07-12 12:53:44 +0100

    refactor: consolidate obs-studio in home-manager

--- home-manager/_mixins/desktop/apps/obs-studio/default.nix
+++ home-manager/_mixins/desktop/apps/obs-studio/default.nix
@@ -13,11 +13,10 @@ let
   themes = pkgs.lib.cleanSource ./themes;
 in
 lib.mkIf (lib.elem hostname installOn) {
-  # Deckmaster and the utilities I bind to the Stream Deck
   home = {
     file = {
       "/Studio/OBS/config/obs-studio/.keep".text = "";
-      "${config.xdg.configHome}/obs-studio/themes" = {
+      "/Studio/OBS/config/obs-studio/themes" = {
         source = themes;
         recursive = true;
       };
@@ -25,6 +24,44 @@ lib.mkIf (lib.elem hostname installOn) {
     packages = with pkgs; [
       obs-cli
       obs-cmd
+    ];
+  };
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      advanced-scene-switcher
+      obs-3d-effect
+      obs-advanced-masks
+      obs-command-source
+      obs-composite-blur
+      obs-dvd-screensaver
+      obs-freeze-filter
+      obs-gradient-source
+      obs-gstreamer
+      obs-markdown
+      obs-move-transition
+      obs-multi-rtmp
+      obs-pipewire-audio-capture
+      obs-rgb-levels
+      obs-scale-to-sound
+      obs-scene-as-transition
+      obs-shaderfilter
+      obs-source-clone
+      obs-source-record
+      obs-source-switcher
+      obs-stroke-glow-shadow
+      obs-teleport
+      obs-text-pthread
+      obs-transition-table
+      obs-urlsource
+      obs-vaapi
+      obs-vertical-canvas
+      obs-vintage-filter
+      obs-webkitgtk
+      obs-websocket
+      pixel-art
+      waveform
     ];
   };
 
