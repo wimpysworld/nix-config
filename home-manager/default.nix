040dd1b5f7dc5424eaac2bdc7cb419c829c3127c
Martin Wimpress <martin@wimpress.org>
2024-07-12 20:02:28 +0100

    chore: remove obsolete home manager workaround

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -184,8 +184,6 @@ in
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
     };
   };
 
