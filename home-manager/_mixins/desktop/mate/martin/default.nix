dec294aa47d7c3fedd7331f5ffb5ea48d0c4af5b
Martin Wimpress <martin@wimpress.org>
2024-07-12 17:29:52 +0100

    refactor: isolate user overrides for mate desktop

---
+++ home-manager/_mixins/desktop/mate/martin/default.nix
@@ -0,0 +1,41 @@
+{ lib, ... }:
+{
+  # User specific dconf settings; only intended as override for NixOS dconf profile user database
+  dconf.settings = with lib.hm.gvariant; {
+    "net/launchpad/plank/docks/dock1" = {
+      dock-items = [
+        "brave-browser.dockitem"
+        "Wavebox.dockitem"
+        "org.telegram.desktop.dockitem"
+        "discord.dockitem"
+        "org.gnome.Fractal.dockitem"
+        "org.squidowl.halloy.dockitem"
+        "code.dockitem"
+        "GitKraken.dockitem"
+        "com.obsproject.Studio.dockitem"
+      ];
+    };
+
+    "org/mate/desktop/peripherals/keyboard/kbd" = {
+      options = [
+        "terminate\tterminate:ctrl_alt_bksp"
+        "caps\tcaps:none"
+      ];
+    };
+
+    "org/mate/marco/general" = {
+      num-workspaces = mkInt32 8;
+    };
+
+    "org/mate/marco/workspace-names" = {
+      name-1 = " Web ";
+      name-2 = " Work ";
+      name-3 = " Chat ";
+      name-4 = " Code ";
+      name-5 = " Term ";
+      name-6 = " Cast ";
+      name-7 = " Virt ";
+      name-8 = " Fun ";
+    };
+  };
+}
