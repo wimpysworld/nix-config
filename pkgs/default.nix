64e61dff4bed98d0b107f1dd1de10c494853cf43
Martin Wimpress <martin@wimpress.org>
2024-07-12 20:44:33 +0100

    feat: add joplin-desktop local package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,12 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # Build them using 'nix build .#example' or (legacy) 'nix-build -A example'
-
 {
   pkgs ? (import ../nixpkgs.nix) { },
 }:
 {
   catppuccin-gtk = pkgs.callPackage ./catppuccin-gtk { };
   gitkraken = pkgs.callPackage ./gitkraken { };
+  joplin-desktop = pkgs.callPackage ./joplin-desktop { };
   obs-studio = pkgs.callPackage ./obs-studio { };
   obs-studio-plugins = pkgs.callPackage ./obs-studio/plugins { };
   wavebox = pkgs.callPackage ./wavebox { };
