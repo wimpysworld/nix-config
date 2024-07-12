4fbde0574086c8b6cc0a06ff2d2bb107206e1dea
Martin Wimpress <martin@wimpress.org>
2024-07-12 19:30:05 +0100

    refactor: make zoom a user conditional desktop app

--- nixos/_mixins/desktop/apps/default.nix
+++ nixos/_mixins/desktop/apps/default.nix
@@ -10,5 +10,6 @@ _: {
     ./utilities
     ./video-production
     ./web-browsers
+    ./zoom
   ];
 }
---
+++ nixos/_mixins/desktop/apps/zoom/default.nix
@@ -0,0 +1,14 @@
+{
+  lib,
+  pkgs,
+  username,
+  ...
+}:
+let
+  installFor = [ "martin" ];
+in
+lib.mkIf (lib.elem username installFor) {
+  environment.systemPackages = with pkgs; [
+    zoom-us
+  ];
+}
