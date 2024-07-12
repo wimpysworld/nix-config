c18529bb550eaa5e7ca9c1d23ccf635e9af6effa
Martin Wimpress <martin@wimpress.org>
2024-07-12 16:09:21 +0100

    refactor: make mpris-proxy a general desktop capability

--- home-manager/_mixins/desktop/mate/default.nix
+++ home-manager/_mixins/desktop/mate/default.nix
@@ -2,8 +2,6 @@
 {
   services = {
     gpg-agent.pinentryPackage = lib.mkForce pkgs.pinentry-gnome3;
-    # https://nixos.wiki/wiki/Bluetooth#Using_Bluetooth_headsets_with_PulseAudio
-    mpris-proxy.enable = true;
   };
 
   systemd.user.services = {
