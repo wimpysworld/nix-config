9434b463844fdc72d9e1a0417c8784ffd22ac288
Martin Wimpress <martin@wimpress.org>
2024-07-14 13:19:56 +0100

    refactor: do not create stub directories on lima VMs

--- home-manager/_mixins/users/martin/default.nix
+++ home-manager/_mixins/users/martin/default.nix
@@ -8,6 +8,7 @@
 }:
 let
   inherit (pkgs.stdenv) isLinux;
+  isLima = builtins.substring 0 5 hostname == "lima-";
   isStreamstation = hostname == "phasma" || hostname == "vader";
 in
 {
@@ -40,52 +41,68 @@ in
       [ALIASES]
     ";
     file.".face".source = ./face.png;
-    file."Development/salsa.debian.org/.envrc".text = "export DEB_VENDOR=Debian";
-    file."Development/git.launchpad.net/.envrc".text = "export DEB_VENDOR=Ubuntu";
-    file."Development/github.com/ubuntu/.envrc".text = "export DEB_VENDOR=Ubuntu";
-    file."Development/github.com/ubuntu-mate/.envrc".text = "export DEB_VENDOR=Ubuntu";
-    file."Quickemu/nixos-console.conf".text = ''
-      #!/run/current-system/sw/bin/quickemu --vm
-      guest_os="linux"
-      disk_img="nixos-console/disk.qcow2"
-      disk_size="96G"
-      iso="nixos-console/nixos.iso"
-    '';
-    file."Quickemu/nixos-gnome.conf".text = ''
-      #!/run/current-system/sw/bin/quickemu --vm
-      guest_os="linux"
-      disk_img="nixos-gnome/disk.qcow2"
-      disk_size="96G"
-      iso="nixos-gnome/nixos.iso"
-      width="1920"
-      height="1080"
-    '';
-    file."Quickemu/nixos-mate.conf".text = ''
-      #!/run/current-system/sw/bin/quickemu --vm
-      guest_os="linux"
-      disk_img="nixos-mate/disk.qcow2"
-      disk_size="96G"
-      iso="nixos-mate/nixos.iso"
-      width="1920"
-      height="1080"
-    '';
-    file."Quickemu/nixos-pantheon.conf".text = ''
-      #!/run/current-system/sw/bin/quickemu --vm
-      guest_os="linux"
-      disk_img="nixos-pantheon/disk.qcow2"
-      disk_size="96G"
-      iso="nixos-pantheon/nixos.iso"
-      width="1920"
-      height="1080"
-    '';
-    file."/Development/.keep".text = "";
-    file."/Games/.keep".text = "";
-    file."/Quickemu/nixos-console/.keep".text = "";
-    file."/Quickemu/nixos-gnome/.keep".text = "";
-    file."/Quickemu/nixos-mate/.keep".text = "";
-    file."/Quickemu/nixos-pantheon/.keep".text = "";
-    file."/Scripts/.keep".text = "";
-    file."/Websites/.keep".text = "";
+    file."Development/salsa.debian.org/.envrc" = lib.mkIf (!isLima) {
+      text = ''export DEB_VENDOR=Debian'';
+    };
+    file."Development/git.launchpad.net/.envrc" = lib.mkIf (!isLima) {
+      text = ''export DEB_VENDOR=Ubuntu'';
+    };
+    file."Development/github.com/ubuntu/.envrc" = lib.mkIf (!isLima) {
+      text = ''export DEB_VENDOR=Ubuntu'';
+    };
+    file."Development/github.com/ubuntu-mate/.envrc" = lib.mkIf (!isLima) {
+      text = ''export DEB_VENDOR=Ubuntu'';
+    };
+    file."Quickemu/nixos-console.conf" = lib.mkIf (!isLima) {
+      text = ''
+        #!/run/current-system/sw/bin/quickemu --vm
+        guest_os="linux"
+        disk_img="nixos-console/disk.qcow2"
+        disk_size="96G"
+        iso="nixos-console/nixos.iso"
+      '';
+    };
+    file."Quickemu/nixos-gnome.conf" = lib.mkIf (!isLima) {
+      text = ''
+        #!/run/current-system/sw/bin/quickemu --vm
+        guest_os="linux"
+        disk_img="nixos-gnome/disk.qcow2"
+        disk_size="96G"
+        iso="nixos-gnome/nixos.iso"
+        width="1920"
+        height="1080"
+      '';
+    };
+    file."Quickemu/nixos-mate.conf" = lib.mkIf (!isLima) {
+      text = ''
+        #!/run/current-system/sw/bin/quickemu --vm
+        guest_os="linux"
+        disk_img="nixos-mate/disk.qcow2"
+        disk_size="96G"
+        iso="nixos-mate/nixos.iso"
+        width="1920"
+        height="1080"
+      '';
+    };
+    file."Quickemu/nixos-pantheon.conf" = lib.mkIf (!isLima) {
+      text = ''
+        #!/run/current-system/sw/bin/quickemu --vm
+        guest_os="linux"
+        disk_img="nixos-pantheon/disk.qcow2"
+        disk_size="96G"
+        iso="nixos-pantheon/nixos.iso"
+        width="1920"
+        height="1080"
+      '';
+    };
+    file."/Development/.keep" = lib.mkIf (!isLima) { text = ""; };
+    file."/Games/.keep" = lib.mkIf (!isLima) { text = ""; };
+    file."/Quickemu/nixos-console/.keep" = lib.mkIf (!isLima) { text = ""; };
+    file."/Quickemu/nixos-gnome/.keep" = lib.mkIf (!isLima) { text = ""; };
+    file."/Quickemu/nixos-mate/.keep" = lib.mkIf (!isLima) { text = ""; };
+    file."/Quickemu/nixos-pantheon/.keep" = lib.mkIf (!isLima) { text = ""; };
+    file."/Scripts/.keep" = lib.mkIf (!isLima) { text = ""; };
+    file."/Websites/.keep" = lib.mkIf (!isLima) { text = ""; };
     file."/Zero/.keep".text = "";
     file.".ssh/allowed_signers".text = ''
       martin@wimpress.org ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAywaYwPN4LVbPqkc+kUc7ZVazPBDy4LCAud5iGJdr7g9CwLYoudNjXt/98Oam5lK7ai6QPItK6ECj5+33x/iFpWb3Urr9SqMc/tH5dU1b9N/9yWRhE2WnfcvuI0ms6AXma8QGp1pj/DoLryPVQgXvQlglHaDIL1qdRWFqXUO2u30X5tWtDdOoR02UyAtYBttou4K0rG7LF9rRaoLYP9iCBLxkMJbCIznPD/pIYa6Fl8V8/OVsxYiFy7l5U0RZ7gkzJv8iNz+GG8vw2NX4oIJfAR4oIk3INUvYrKvI2NSMSw5sry+z818fD1hK+soYLQ4VZ4hHRHcf4WV4EeVa5ARxdw== Martin Wimpress
