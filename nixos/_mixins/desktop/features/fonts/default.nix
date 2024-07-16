d54bf6f96eadc26c87a6bd88dbeb65ef1889300b
Martin Wimpress <martin@wimpress.org>
2024-07-16 12:29:51 +0100

    feat: add unscii font

--- nixos/_mixins/desktop/features/fonts/default.nix
+++ nixos/_mixins/desktop/features/fonts/default.nix
@@ -41,9 +41,10 @@
         impact-label-font
         mocha-mattari-font
         poppins-font
-        ubuntu_font_family
         spaceport-2006-font
         twitter-color-emoji
+        ubuntu_font_family
+        unscii
         zx-spectrum-7-font
       ];
 
