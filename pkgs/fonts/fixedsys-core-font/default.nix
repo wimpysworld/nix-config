8659b1965186d7b11b2f9c78ec8053db515d665e
Martin Wimpress <martin@wimpress.org>
2024-07-16 11:36:56 +0100

    feat: replace upstream fixedsys fonts with local patched versions
    The local versions add Nerd Font and Braille support.

--- pkgs/fonts/fixedsys-core-font/default.nix
+++ pkgs/fonts/fixedsys-core-font/default.nix
@@ -1,5 +1,4 @@
 {
-  fetchFromGitHub,
   lib,
   stdenvNoCC,
 }:
@@ -8,15 +7,10 @@ stdenvNoCC.mkDerivation rec {
   pname = "fixedsys_core";
   version = "0.6";
 
-  src = fetchFromGitHub {
-    owner = "delinx";
-    repo = "Fixedsys-Core";
-    rev = "01399b8d1e85c74bb8eb97de442fa51c17e39add";
-    hash = "sha256-9vP0pRFOgquEZS8TjiHfPmYUX/fcoOtgdCKZlptSY9o=";
-  };
+  src = lib.cleanSource ./fonts;
 
   installPhase = ''
-    install -m444 -Dt $out/share/fonts/truetype/${pname} "FixedsysCore-Regular.ttf"
+    install -m444 -Dt $out/share/fonts/truetype/${pname} *ttf
   '';
 
   meta = {
@@ -25,6 +19,8 @@ stdenvNoCC.mkDerivation rec {
       Fixedsys Core is a love letter to the Fixedsys Excelsior font, an attempt
       to reinvent the feel and look of a truly unique font for modern
       high-resolution displays.
+
+      This version is patched to include Nerd Font and Braille glyphs.
     '';
     homepage = "https://github.com/delinx/Fixedsys-Core";
     license = lib.licenses.free;
