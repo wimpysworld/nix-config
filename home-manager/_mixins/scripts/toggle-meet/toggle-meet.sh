61dc279e7f023ad5d770930163d387ff66ca4589
Martin Wimpress <martin@wimpress.org>
2024-07-18 17:43:10 +0100

    feat: add toggle-meet to home manager scripts

---
+++ home-manager/_mixins/scripts/toggle-meet/toggle-meet.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set +u
+
+WINDOW_ID="$(xdotool search '^Meet ' 2>/dev/null)"
+if [ -n "${WINDOW_ID}" ]; then
+  case "${1}" in
+      camera)
+        xdotool windowfocus "${WINDOW_ID}"
+        xdotool getactivewindow key ctrl+e
+        ;;
+      mic)
+        xdotool windowfocus "${WINDOW_ID}"
+        xdotool getactivewindow key ctrl+d
+        ;;
+      *) echo "Usage: ${0} {camera|mic}";;
+  esac
+fi
