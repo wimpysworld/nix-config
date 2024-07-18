1ab54bcee4968b2917c9e2aa64eb926d5c435b72
Martin Wimpress <martin@wimpress.org>
2024-07-18 17:33:00 +0100

    feat: add pw-quantum to home manager sciprts

---
+++ home-manager/_mixins/scripts/pw-quantum/default.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+let
+  name = builtins.baseNameOf (builtins.toString ./.);
+  shellApplication = pkgs.writeShellApplication {
+    inherit name;
+    runtimeInputs = with pkgs; [ coreutils-full ];
+    text = builtins.readFile ./${name}.sh;
+  };
+in
+{
+  home.packages = with pkgs; [ shellApplication ];
+}
