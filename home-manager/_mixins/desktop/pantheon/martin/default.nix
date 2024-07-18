2199fb5c27ba9c12453df84ae66a86a524b1c6a5
Martin Wimpress <martin@wimpress.org>
2024-07-18 14:10:47 +0100

    refactor: update wallpapers

--- home-manager/_mixins/desktop/pantheon/martin/default.nix
+++ home-manager/_mixins/desktop/pantheon/martin/default.nix
@@ -48,11 +48,11 @@
       }
       // lib.optionalAttrs (hostname == "phasma") {
         picture-uri = "file:///etc/backgrounds/DeterminateColorway-3440x1440.png";
-        picture-uri-dark = "file:///etc/backgrounds/DeterminateColorway-1920x1200.png";
+        picture-uri-dark = "file:///etc/backgrounds/DeterminateColorway-3440x1440.png";
       }
       // lib.optionalAttrs (hostname == "sidious") {
         picture-uri = "file:///etc/backgrounds/DeterminateColorway-3840x2160.png";
-        picture-uri-dark = "file:///etc/backgrounds/DeterminateColorway-1920x1200.png";
+        picture-uri-dark = "file:///etc/backgrounds/DeterminateColorway-3840x2160.png";
       }
       // lib.optionalAttrs (hostname == "tanis") {
         picture-uri = "file:///etc/backgrounds/DeterminateColorway-1920x1200.png";
