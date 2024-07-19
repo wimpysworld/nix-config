6368dc9fbcffd59a23596af0dc9eba27607ef8fc
Martin Wimpress <martin@wimpress.org>
2024-07-19 07:07:47 +0100

    feat: add obs-backup to home manager scripts

---
+++ home-manager/_mixins/scripts/backup-obs/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+let
+  name = builtins.baseNameOf (builtins.toString ./.);
+  shellApplication = pkgs.writeShellApplication {
+    inherit name;
+    runtimeInputs = with pkgs; [
+      coreutils-full
+      rsync
+    ];
+    text = builtins.readFile ./${name}.sh;
+  };
+in
+{
+  home.packages = with pkgs; [ shellApplication ];
+}
