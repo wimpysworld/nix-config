ab20d4d4b49f5549ff0874c885ea4f04edabbd12
Martin Wimpress <martin@wimpress.org>
2024-07-26 09:24:42 +0100

    chore: remove nix.nixPath from nixos
    determinate nixos module manages nix.nixPath

--- nixos/default.nix
+++ nixos/default.nix
@@ -104,10 +104,6 @@
     # To make nix3 commands consistent with your flake
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
 
-    # This will additionally add your inputs to the system's legacy channels
-    # Making legacy nix commands consistent as well, awesome!
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
     optimise.automatic = true;
     settings = {
       auto-optimise-store = true;
