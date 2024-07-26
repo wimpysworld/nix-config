4f01763dd33d16727b175f3430097ee43d480bbf
Martin Wimpress <martin@wimpress.org>
2024-07-26 09:23:47 +0100

    chore: remove nix.registry from home manager
    determinate home manager module manages nix.registry

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -177,9 +177,6 @@ in
   };
 
   nix = {
-    # This will add each flake input as a registry
-    # To make nix3 commands consistent with your flake
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
     settings = {
       auto-optimise-store = true;
       experimental-features = [
