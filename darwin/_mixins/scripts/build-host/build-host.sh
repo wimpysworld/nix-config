9af2e012c99188fbb3c0c09cbf78443490a96bc5
Martin Wimpress <martin@wimpress.org>
2024-07-27 08:01:57 +0100

    feat: add build-host to darwin

---
+++ darwin/_mixins/scripts/build-host/build-host.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+HOST=$(hostname)
+if [ -e ~/Zero/nix-config ]; then
+    all_cores=$(nproc)
+    build_cores=$(printf "%.0f" "$(echo "${all_cores} * 0.75" | bc)")
+    echo "Building nix-darwin ❄️  with ${build_cores} cores"
+    pushd ~/Zero/nix-config
+    nom build ".#darwinConfigurations.${HOST}.config.system.build.toplevel" --cores "${build_cores}"
+    popd
+else
+    echo "ERROR! No nix-config found in ~/Zero/nix-config"
+fi
---
+++ darwin/_mixins/scripts/default.nix
@@ -0,0 +1,10 @@
+{ lib, ... }:
+let
+  currentDir = ./.; # Represents the current directory
+  isDirectoryAndNotTemplate = name: type: type == "directory" && name != "_template";
+  directories = lib.filterAttrs isDirectoryAndNotTemplate (builtins.readDir currentDir);
+  importDirectory = name: import (currentDir + "/${name}");
+in
+{
+  imports = lib.mapAttrsToList (name: _: importDirectory name) directories;
+}
