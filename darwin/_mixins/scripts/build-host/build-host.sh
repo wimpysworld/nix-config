1147f8eaaef0476c1a8f11640788af8b3c2c6140
Martin Wimpress <martin@wimpress.org>
2024-07-27 18:53:02 +0100

    style: correct spacing in darwin host scripts

--- darwin/_mixins/scripts/build-host/build-host.sh
+++ darwin/_mixins/scripts/build-host/build-host.sh
@@ -4,7 +4,7 @@ HOST=$(hostname)
 if [ -e ~/Zero/nix-config ]; then
     all_cores=$(nproc)
     build_cores=$(printf "%.0f" "$(echo "${all_cores} * 0.75" | bc)")
-    echo "Building nix-darwin ❄️  with ${build_cores} cores"
+    echo "Building nix-darwin ❄️ with ${build_cores} cores"
     pushd ~/Zero/nix-config
     nom build ".#darwinConfigurations.${HOST}.config.system.build.toplevel" --cores "${build_cores}"
     popd
--- darwin/_mixins/scripts/switch-host/switch-host.sh
+++ darwin/_mixins/scripts/switch-host/switch-host.sh
@@ -3,7 +3,7 @@
 if [ -e "${HOME}/Zero/nix-config" ]; then
     all_cores=$(nproc)
     build_cores=$(printf "%.0f" "$(echo "${all_cores} * 0.75" | bc)")
-    echo "Switch nix-darwin ❄️  with ${build_cores} cores"
+    echo "Switch nix-darwin ❄️ with ${build_cores} cores"
     nix run nix-darwin -- switch --flake "${HOME}/Zero/nix-config" --cores "${build_cores}" -L
 else
     echo "ERROR! No nix-config found in ${HOME}/Zero/nix-config"
