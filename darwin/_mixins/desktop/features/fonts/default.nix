34baaa9e35588826ece9a05a1c819da1242fad24
Martin Wimpress <martin@wimpress.org>
2024-07-29 13:51:02 +0100

    feat: add fonts to darwin

---
+++ darwin/_mixins/desktop/features/fonts/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+{
+  fonts = {
+    packages =
+      with pkgs;
+      [
+        (nerdfonts.override {
+          fonts = [
+            "FiraCode"
+            "NerdFontsSymbolsOnly"
+          ];
+        })
+        fira
+        liberation_ttf
+        noto-fonts-emoji
+        source-serif
+        symbola
+        work-sans
+        bebas-neue-2014-font
+        bebas-neue-pro-font
+        bebas-neue-rounded-font
+        bebas-neue-semi-rounded-font
+        boycott-font
+        commodore-64-pixelized-font
+        digital-7-font
+        dirty-ego-font
+        fixedsys-core-font
+        fixedsys-excelsior-font
+        impact-label-font
+        mocha-mattari-font
+        poppins-font
+        spaceport-2006-font
+        twitter-color-emoji
+        ubuntu_font_family
+        unscii
+        zx-spectrum-7-font
+      ];
+  };
+}
