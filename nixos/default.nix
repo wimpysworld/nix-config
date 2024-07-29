87597c882d10b25f3fcc6b3e2b8c19e72124c66b
Martin Wimpress <martin@wimpress.org>
2024-07-29 11:25:14 +0100

    refactor: simplify nix.settings

--- nixos/default.nix
+++ nixos/default.nix
@@ -105,32 +105,20 @@ in
     };
   };
 
-  nix = let
-    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-  in {
-    optimise.automatic = isLinux;
+  nix = {
+    optimise.automatic = true;
     settings = {
-      auto-optimise-store = isLinux;
+      auto-optimise-store = true;
       experimental-features = [
         "nix-command"
         "flakes"
       ];
-      # Opinionated: disable global registry
-      flake-registry = "";
-      # Workaround for https://github.com/NixOS/nix/issues/9574
-      nix-path = config.nix.nixPath;
       trusted-users = [
         "root"
         "${username}"
       ];
       warn-dirty = false;
     };
-    # Opinionated: disable channels
-    channel.enable = false;
-
-    # Opinionated: make flake registry and nix path match flake inputs
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "${platform}";
