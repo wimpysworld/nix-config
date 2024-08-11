bfb6f2ce53a033b9f01846f4c5408674044df575
Martin Wimpress <martin@wimpress.org>
2024-08-11 13:17:55 +0100

    fix: set nvidia driver version to 555.58.02 so it builds with Linux 6.10

--- nixos/phasma/default.nix
+++ nixos/phasma/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   inputs,
   lib,
   pkgs,
@@ -54,6 +55,18 @@
   hardware = {
     mwProCapture.enable = true;
     nvidia = {
+      # NVIDIA driver FTBFS with Linux 6.10
+      # https://github.com/NixOS/nixpkgs/issues/332350
+      # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/642
+      # https://discourse.nixos.org/t/builder-for-nvidia-x11-550-78-6-10-drv-failed-with-exit-code-2/49360
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "555.58.02";
+        sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+        sha256_aarch64 = "sha256-wb20isMrRg8PeQBU96lWJzBMkjfySAUaqt4EgZnhyF8=";
+        openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
+        settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+        persistencedSha256 = "sha256-a1D7ZZmcKFWfPjjH1REqPM5j/YLWKnbkP9qfRyIyxAw=";
+      };
       prime = {
         amdgpuBusId = "PCI:34:0:0";
         nvidiaBusId = "PCI:31:0:0";
