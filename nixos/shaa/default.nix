1d7d96661d4a3bfaf4a8eefb361019767338682b
Martin Wimpress <martin@wimpress.org>
2024-08-11 14:52:51 +0100

    refactor: enable amd-pstate on shaa

--- nixos/shaa/default.nix
+++ nixos/shaa/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   inputs,
   lib,
   pkgs,
@@ -6,10 +7,11 @@
 }:
 {
   # TODO: There is an issue where the LED light on the mic button is always on.
-  # This udev rule will turn it off.
   # - https://wiki.archlinux.org/title/Lenovo_ThinkPad_T14_(AMD)_Gen_3#Mute_Mic_LED_always_on
   imports = [
-    inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14s-amd-gen1
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
     ./disks.nix
   ];
 
@@ -28,9 +30,17 @@
       "kvm-amd"
     ];
     kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+    # Force use of the thinkpad_acpi driver for backlight control.
+    # This allows the backlight save/load systemd service to work.
     # Thinkpad T14s AMD Gen 1 does not wake up from suspend using 'deep' sleep.
-    kernelParams = [ "mem_sleep_default=s2idle" ];
+    kernelParams = [ "acpi_backlight=native" "mem_sleep_default=s2idle" ];
   };
 
+  hardware ={
+    trackpoint = {
+      enable = lib.mkDefault true;
+      emulateWheel = lib.mkDefault config.hardware.trackpoint.enable;
+    };
+  };
   services.fprintd.enable = lib.mkDefault true;
 }
