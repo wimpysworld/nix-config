7ad8235a4f69c1bf8fd02e3e650ec91049eccad8
Martin Wimpress <martin@wimpress.org>
2024-08-11 12:44:13 +0100

    docs: add comments about T14s Gen 1

--- nixos/shaa/default.nix
+++ nixos/shaa/default.nix
@@ -5,6 +5,9 @@
   ...
 }:
 {
+  # TODO: There is an issue where the LED light on the mic button is always on.
+  # This udev rule will turn it off.
+  # - https://wiki.archlinux.org/title/Lenovo_ThinkPad_T14_(AMD)_Gen_3#Mute_Mic_LED_always_on
   imports = [
     inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14s-amd-gen1
     ./disks.nix
@@ -25,6 +28,7 @@
       "kvm-amd"
     ];
     kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+    # Thinkpad T14s AMD Gen 1 does not wake up from suspend using 'deep' sleep.
     kernelParams = [ "mem_sleep_default=s2idle" ];
   };
 
