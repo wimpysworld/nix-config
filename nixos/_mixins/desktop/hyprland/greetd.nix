7c81a456650e1efdb9f177d485e50d78f2aa276e
Martin Wimpress <martin@wimpress.org>
2024-08-18 01:26:55 +0100

    fix: run regreet via sway
    More reliable, no screen flickering, more seamless.

--- nixos/_mixins/desktop/hyprland/greetd.nix
+++ nixos/_mixins/desktop/hyprland/greetd.nix
@@ -5,10 +5,23 @@
   ...
 }:
 {
+  # Use a minimal Sway to run regreet
+  environment = {
+    etc."greetd/regreet-sway".text = ''
+      output * scale 1
+      output * bg #1E1E2D solid_color
+      output * bg /etc/backgrounds/DeterminateColorway-1920x1080.png fill
+      seat seat0 xcursor_theme catppuccin-mocha-blue-cursors 48
+      exec "${pkgs.greetd.regreet}/bin/regreet; ${pkgs.sway}/bin/swaymsg exit"
+    '';
+  };
   programs = {
     regreet = {
       enable = true;
       settings = {
+        appearance = {
+          greeting_msg = "This is ${hostname}!";
+        };
         background = {
           path = "/etc/backgrounds/DeterminateColorway-1920x1080.png";
           # How the background image covers the screen if the aspect ratio doesn't match
@@ -16,20 +29,16 @@
           # Refer to: https://docs.gtk.org/gtk4/enum.ContentFit.html
           fit = "Cover";
         };
+        commands = {
+          reboot = [ "${pkgs.systemd}/bin/systemctl" "reboot" ];
+          poweroff = [ "${pkgs.systemd}/bin/systemctl" "poweroff" ];
+        };
         GTK = {
           application_prefer_dark_theme = true;
           cursor_theme_name = "catppuccin-mocha-blue-cursors";
           font_name = "Work Sans 16";
           icon_theme_name = "Papirus-Dark";
           theme_name = "catppuccin-mocha-blue-standard+default";
-        };
-        commands = {
-          reboot = [ "${pkgs.systemd}/bin/systemctl" "reboot" ];
-          poweroff = [ "${pkgs.systemd}/bin/systemctl" "poweroff" ];
-        };
-        appearance = {
-          # The message that initially displays on startup
-          greeting_msg = "Welcome to ${hostname}!";
         };
         # cursorTheme = {
         #   name = "catppuccin-mocha-blue-cursors";
@@ -58,24 +67,15 @@
       };
     };
   };
-  security = {
-    pam = {
-      services = {
-        # unlock gnome keyring automatically with greetd
-        greetd.enableGnomeKeyring = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.sway}/bin/sway --config /etc/greetd/regreet-sway";
+        user = "greeter";
       };
     };
-  };
-
-  services = {
-    greetd = {
-      settings = {
-        initial_session = {
-          command = "${pkgs.unstable.hyprland}/bin/Hyprland > /dev/null 2>&1";
-          user = "${username}";
-        };
-      };
-      vt = 7;
-    };
+    vt = 7;
   };
 }
