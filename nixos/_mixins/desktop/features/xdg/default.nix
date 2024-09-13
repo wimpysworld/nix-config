2880b3eb051db6c54a16766b1fb297977fb5b687
Martin Wimpress <martin@wimpress.org>
2024-09-13 09:59:38 +0100

    fix: correctly provision extraPortals
    xdg-desktop-portal-gnome configures xdg-desktop-portal-gtk

--- nixos/_mixins/desktop/features/xdg/default.nix
+++ nixos/_mixins/desktop/features/xdg/default.nix
@@ -9,11 +9,12 @@
         pkgs.xdg-desktop-portal-gnome
       ] ++ lib.optionals (desktop == "hyprland") [
         pkgs.xdg-desktop-portal-hyprland
+        pkgs.xdg-desktop-portal-gtk
       ] ++ lib.optionals (desktop == "mate") [
         pkgs.xdg-desktop-portal-xapp
+        pkgs.xdg-desktop-portal-gtk
       ] ++ lib.optionals (desktop == "pantheon") [
         pkgs.pantheon.xdg-desktop-portal-pantheon
-      ] ++ [
         pkgs.xdg-desktop-portal-gtk
       ];
       config = {
