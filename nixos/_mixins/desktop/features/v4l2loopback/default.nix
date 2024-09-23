f8bbb6681dfbfdd22ca8c85cd7d41ca7da7eb091
Martin Wimpress <martin@wimpress.org>
2024-09-23 10:37:55 +0100

    fix: register a v4l2loopback device at boot

--- nixos/_mixins/desktop/features/v4l2loopback/default.nix
+++ nixos/_mixins/desktop/features/v4l2loopback/default.nix
@@ -5,6 +5,10 @@
     extraModprobeConfig = ''
       options v4l2loopback devices=1 video_nr=13 card_label="OBS Virtual Camera" exclusive_caps=1
     '';
+    # Register a v4l2loopback device at boot
+    kernelModules = [
+      "v4l2loopback"
+    ];
   };
   security.polkit.enable = true;
 }
