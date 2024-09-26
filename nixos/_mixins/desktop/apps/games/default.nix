53aa7fe5eef917a779a38ac13b239be34d9857e6
Martin Wimpress <martin@wimpress.org>
2024-09-26 16:59:23 +0100

    feat: add uiScale override to defold

--- nixos/_mixins/desktop/apps/games/default.nix
+++ nixos/_mixins/desktop/apps/games/default.nix
@@ -15,7 +15,9 @@ lib.mkIf (lib.elem username installFor && isInstall && isWorkstation) {
   environment = {
     systemPackages = [
       inputs.antsy-alien-attack-pico.packages.${platform}.default
-      pkgs.defold
+      (pkgs.defold.override {
+        uiScale = "1.25";
+      })
       pkgs.pico8
     ];
   };
