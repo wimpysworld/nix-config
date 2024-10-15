991d45ed67d8b5ef1cd745bace2094a05ac612b3
Martin Wimpress <martin@wimpress.org>
2024-10-15 14:10:19 +0100

    fix: hardcode server timezone to UTC

--- nixos/_mixins/features/time/default.nix
+++ nixos/_mixins/features/time/default.nix
@@ -1,21 +1,36 @@
-_: {
+{
+  hostname,
+  lib,
+  ...
+}:
+let
+  isServer = hostname == "malak" || hostname == "revan";
+  useGeoclue = !isServer;
+in
+{
   location = {
     provider = "geoclue2";
   };
 
   services = {
-    automatic-timezoned.enable = true;
+    automatic-timezoned.enable = useGeoclue;
     geoclue2 = {
       enable = true;
       # https://github.com/NixOS/nixpkgs/issues/321121
-      geoProviderUrl = "https://beacondb.net/v1/geolocate";
-      submissionNick = "geoclue";
-      submissionUrl = "https://beacondb.net/v2/geosubmit";
+      geoProviderUrl = "https://api.positon.xyz/v1/geolocate?key=test";
+      submissionUrl = "https://api.positon.xyz/v2/geosubmit?key=test";
+      submitData = false;
     };
-    localtimed.enable = true;
+    localtimed.enable = useGeoclue;
   };
+
+  # Prevent "Failed to open /etc/geoclue/conf.d/:" errors
+  systemd.tmpfiles.rules = [
+    "d /etc/geoclue/conf.d 0755 root root"
+  ];
 
   time = {
     hardwareClockInLocalTime = true;
+    timeZone = lib.mkIf isServer "UTC";
   };
 }
