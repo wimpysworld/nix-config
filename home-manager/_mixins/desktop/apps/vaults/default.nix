78eb711628b332f97a8bfbd2d27288283aa41739
Martin Wimpress <martin@wimpress.org>
2024-10-17 14:15:38 +0100

    feat: add gocryptfs, vaults and sirikali for out of band secrets

---
+++ home-manager/_mixins/desktop/apps/vaults/default.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  pkgs,
+  username,
+  ...
+}:
+let
+  installFor = [ "martin" ];
+  inherit (pkgs.stdenv) isDarwin isLinux;
+in
+lib.mkIf (lib.elem username installFor) {
+  home.packages = with pkgs; lib.optionals isLinux [
+    vaults
+  ] ++ lib.optionals isDarwin [
+    sirikali
+  ];
+}
