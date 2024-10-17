2ee4dbf087de625acf54494c301eac7651088dde
Martin Wimpress <martin@wimpress.org>
2024-10-17 09:53:32 +0100

    fix: add required files to nix-store

--- home-manager/_mixins/scripts/send-required/send-required.sh
+++ home-manager/_mixins/scripts/send-required/send-required.sh
@@ -21,5 +21,7 @@ fi
 
 echo "Sending Cider to ${REMOTE_HOST}"
 rsync -a --info=progress2 "$HOME/Zero/nix-config/pkgs/cider/Cider-linux-appimage-x64.AppImage" "${USER}@${REMOTE_HOST}:Zero/nix-config/pkgs/cider/"
+ssh "${REMOTE_HOST}" nix-store --add-fixed sha256 "$HOME/Zero/nix-config/pkgs/cider/Cider-linux-appimage-x64.AppImage"
 echo "Sending PICO-8 to ${REMOTE_HOST}"
 rsync -a --info=progress2 "$HOME/Zero/nix-config/pkgs/pico8/pico-8_0.2.6b_amd64.zip" "${USER}@${REMOTE_HOST}:Zero/nix-config/pkgs/pico8/"
+ssh "${REMOTE_HOST}" nix-store --add-fixed sha256 "$HOME/Zero/nix-config/pkgs/pico8/pico-8_0.2.6b_amd64.zip"
