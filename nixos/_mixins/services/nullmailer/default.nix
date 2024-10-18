e26cebf31a96129521b01b6a42ec36784f29fb19
Martin Wimpress <martin@wimpress.org>
2024-10-18 15:11:11 +0100

    feat: add nullmailer

---
+++ nixos/_mixins/services/nullmailer/default.nix
@@ -0,0 +1,33 @@
+{ config, hostname, lib, username, ... }:
+let
+  domain = "wimpys.world";
+  installOn = [ "malak" "revan" ];
+in
+lib.mkIf (lib.elem hostname installOn) {
+  environment ={
+    shellAliases = {
+      mail-log = "journalctl _SYSTEMD_UNIT=nullmailer.service";
+    };
+  };
+  services.nullmailer = {
+    config = {
+      me = "${domain}";
+      defaultdomain = "${domain}";
+      allmailfrom = "${username}@${domain}";
+      adminaddr = "${username}@${domain}";
+    };
+    enable = true;
+    remotesFile = config.sops.secrets.mailjet.path;
+  };
+  sops = {
+    secrets = {
+      mailjet = {
+        group = config.services.nullmailer.group;
+        mode = "0600";
+        owner = config.services.nullmailer.user;
+        path = "/etc/nullmailer/mailjet";
+        sopsFile = ../../../../secrets/mailjet.yaml;
+      };
+    };
+  };
+}
