e6c24ad85fffa4e2644f1be6171b28f0bec4ff02
Martin Wimpress <martin@wimpress.org>
2024-11-14 01:14:39 +0000

    feat: add custom-caddy overlay

---
+++ overlays/custom-caddy.nix
@@ -0,0 +1,65 @@
+{ pkgs, ... }:
+let
+  inherit (pkgs)
+    buildGoModule
+    cacert
+    caddy
+    go
+    lib
+    stdenv
+    xcaddy
+    ;
+in
+caddy.override {
+  buildGoModule =
+    args:
+    buildGoModule (
+      args
+      // {
+        src = stdenv.mkDerivation rec {
+          pname = "caddy-using-xcaddy-${xcaddy.version}";
+          inherit (caddy) version;
+
+          dontUnpack = true;
+          dontFixup = true;
+
+          nativeBuildInputs = [
+            cacert
+            go
+          ];
+
+          plugins = [
+            "github.com/WeidiDeng/caddy-cloudflare-ip"
+          ];
+
+          configurePhase = ''
+            export GOCACHE=$TMPDIR/go-cache
+            export GOPATH="$TMPDIR/go"
+            export XCADDY_SKIP_BUILD=1
+          '';
+
+          buildPhase = ''
+            ${xcaddy}/bin/xcaddy build "${caddy.src.rev}" ${
+              lib.concatMapStringsSep " " (plugin: "--with ${plugin}") plugins
+            }
+            cd buildenv*
+            go mod vendor
+          '';
+
+          installPhase = ''
+            cp -r --reflink=auto . $out
+          '';
+
+          outputHash = "sha256-DfnMI8iafeC7IL6N+se06W3MQF8RWMBFhtr7GzZCoiA=";
+          outputHashMode = "recursive";
+        };
+
+        subPackages = [ "." ];
+        ldflags = [
+          "-s"
+          "-w"
+        ]; # # don't include version info twice
+        vendorHash = null;
+      }
+    );
+}
