fdd771107f03229843424961171ae1d2ecfe4b2d
Martin Wimpress <martin@wimpress.org>
2024-11-27 13:55:31 +0000

    feat: degrade build-all and switch-all tools gracefully

--- darwin/_mixins/scripts/switch-all/switch-all.sh
+++ darwin/_mixins/scripts/switch-all/switch-all.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-# if nh-home is in the PATH then use it
+STAMP=$(date +%Y%m%d-%H%M%S)
+
+# Switch home-manager, using the best available command
 if command -v nh-home &> /dev/null; then
-    nh-home build
+    nh-home switch
+elif command -v nh &> /dev/null; then
+    nh home switch --backup-extension "${STAMP}" "${HOME}/Zero/nix-config/"
+elif command -v home-manager &> /dev/null; then
+    home-manager switch --backup-extension "${STAMP}" --flake "$HOME/Zero/nix-config" -L
 else
-    nix run nixpkgs#home-manager -- switch --flake "$HOME/Zero/nix-config" -L
+    nix run nixpkgs#home-manager -- switch --backup-extension "${STAMP}" --flake "$HOME/Zero/nix-config" -L
 fi
 switch-host
