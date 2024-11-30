9fca0d0acbe23e6f7c1d639e13b4695b012ce725
Martin Wimpress <martin@wimpress.org>
2024-11-30 18:35:55 +0000

    fix: update satty configuration to use wl-clipboard

--- home-manager/_mixins/desktop/hyprland/hyprshot/default.nix
+++ home-manager/_mixins/desktop/hyprland/hyprshot/default.nix
@@ -18,7 +18,7 @@
         # Select the tool on startup [possible values: pointer, crop, line, arrow, rectangle, text, marker, blur, brush]
         initial-tool = "pointer"
         # Configure the command to be called on copy, for example `wl-copy`
-        copy-command = "wl-copy"
+        copy-command = "${pkgs.wl-clipboard}/bin/wl-copy"
         # Increase or decrease the size of the annotations
         annotation-size-factor = 2
         # Filename to use for saving action: https://docs.rs/chrono/latest/chrono/format/strftime/index.html
