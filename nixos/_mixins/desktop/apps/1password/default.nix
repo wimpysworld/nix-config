72020b0fce3f5459fe7a3a72690198672d7ecde9
Martin Wimpress <martin@wimpress.org>
2024-12-07 21:27:45 +0000

    fix: do not install _1password-gui on darwin

--- nixos/_mixins/desktop/apps/1password/default.nix
+++ nixos/_mixins/desktop/apps/1password/default.nix
@@ -6,10 +6,13 @@
 }:
 let
   installFor = [ "martin" ];
+  inherit (pkgs.stdenv) isLinux;
 in
 lib.mkIf (lib.elem username installFor) {
+  # _1password-gui is marked as broken on nix-darwin
+  # - https://github.com/NixOS/nixpkgs/issues/254944
   environment.systemPackages = with pkgs; [
     _1password-cli
-    _1password-gui
+    (lib.mkIf (isLinux) _1password-gui)
   ];
 }
