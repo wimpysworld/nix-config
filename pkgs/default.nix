6c7db50dd466cec6f5a86418ab41bf4e8a869347
Martin Wimpress <martin@wimpress.org>
2024-12-07 19:58:53 +0000

    refactor: flatten local font packages namespace

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -14,12 +14,30 @@ in
   jan = pkgs.callPackage ./jan { };
   station = pkgs.callPackage ./station { };
   nerd-font-patcher = pkgs.callPackage ./nerd-font-patcher { };
-  local-fonts = pkgs.recurseIntoAttrs (pkgs.callPackage ./fonts { });
   local-obs = pkgs.recurseIntoAttrs (pkgs.callPackage ./obs-plugins { });
 
   # Local packages to prevent unintended upgrades or carrying patches
   gotosocial = pkgs.callPackage ./gotosocial { };
   owncast = pkgs.callPackage ./owncast { };
+
+  # Local fonts
+  # - https://yildiz.dev/posts/packing-custom-fonts-for-nixos/
+  bebas-neue-2014-font = pkgs.callPackage ./fonts/bebas-neue-2014-font { };
+  bebas-neue-2018-font = pkgs.callPackage ./fonts/bebas-neue-2018-font { };
+  bebas-neue-pro-font = pkgs.callPackage ./fonts/bebas-neue-pro-font { };
+  bebas-neue-rounded-font = pkgs.callPackage ./fonts/bebas-neue-rounded-font { };
+  bebas-neue-semi-rounded-font = pkgs.callPackage ./fonts/bebas-neue-semi-rounded-font { };
+  boycott-font = pkgs.callPackage ./fonts/boycott-font { };
+  commodore-64-pixelized-font = pkgs.callPackage ./fonts/commodore-64-pixelized-font { };
+  digital-7-font = pkgs.callPackage ./fonts/digital-7-font { };
+  dirty-ego-font = pkgs.callPackage ./fonts/dirty-ego-font { };
+  fixedsys-core-font = pkgs.callPackage ./fonts/fixedsys-core-font { };
+  fixedsys-excelsior-font = pkgs.callPackage ./fonts/fixedsys-excelsior-font { };
+  impact-label-font = pkgs.callPackage ./fonts/impact-label-font { };
+  mocha-mattari-font = pkgs.callPackage ./fonts/mocha-mattari-font { };
+  poppins-font = pkgs.callPackage ./fonts/poppins-font { };
+  spaceport-2006-font = pkgs.callPackage ./fonts/spaceport-2006-font { };
+  zx-spectrum-7-font = pkgs.callPackage ./fonts/zx-spectrum-7-font { };
 
   # Non-redistributable packages - only evaluated outside CI
   cider = if !isCI then pkgs.callPackage ./cider { } else {};
