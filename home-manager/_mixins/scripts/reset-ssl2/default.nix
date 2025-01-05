a42bf154545fe261ff2be4c45d05f52b3a2ef77b
Martin Wimpress <martin@wimpress.org>
2025-01-05 21:55:55 +0000

    refactor: update reset-ssl2

--- home-manager/_mixins/scripts/reset-ssl2/default.nix
+++ home-manager/_mixins/scripts/reset-ssl2/default.nix
@@ -1,22 +1,17 @@
-{
-  hostname,
-  lib,
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 let
-  installOn = [
-    "phasma"
-    "vader"
-  ];
   name = builtins.baseNameOf (builtins.toString ./.);
   shellApplication = pkgs.writeShellApplication {
     inherit name;
     runtimeInputs = with pkgs; [
       coreutils-full
+      gnugrep
       usb-reset
+      usbutils
     ];
     text = builtins.readFile ./${name}.sh;
   };
 in
-lib.mkIf (lib.elem hostname installOn) { home.packages = with pkgs; [ shellApplication ]; }
+{
+  home.packages = with pkgs; [ shellApplication ];
+}
