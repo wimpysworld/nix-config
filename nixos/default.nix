a396abe78e1341472dc1c7eb1278a5f1859b921a
Martin Wimpress <martin@wimpress.org>
2025-01-23 13:55:40 +0000

    feat: set systemd DefaultTimeoutStopSec to 10s

--- nixos/default.nix
+++ nixos/default.nix
@@ -221,11 +221,14 @@
 
   # Create symlink to /bin/bash
   # - https://github.com/lima-vm/lima/issues/2110
-  systemd.tmpfiles.rules = [
-    "L+ /bin/bash - - - - ${pkgs.bash}/bin/bash"
-    "d /nix/var/nix/profiles/per-user/${username} 0755 ${username} root"
-    "d /var/lib/private/sops/age 0755 root root"
-  ];
+  systemd = {
+    extraConfig = "DefaultTimeoutStopSec=10s";
+    tmpfiles.rules = [
+      "L+ /bin/bash - - - - ${pkgs.bash}/bin/bash"
+      "d /nix/var/nix/profiles/per-user/${username} 0755 ${username} root"
+      "d /var/lib/private/sops/age 0755 root root"
+    ];
+  };
 
   system = {
     activationScripts = {
