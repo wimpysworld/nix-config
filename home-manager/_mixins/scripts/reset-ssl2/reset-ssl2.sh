f82dde317daf5f1b3f205e708c10b9f63a2b5cf1
Martin Wimpress <martin@wimpress.org>
2025-01-23 10:24:05 +0000

    fix: make reset-ssl2 shellcheck compliant

--- home-manager/_mixins/scripts/reset-ssl2/reset-ssl2.sh
+++ home-manager/_mixins/scripts/reset-ssl2/reset-ssl2.sh
@@ -11,7 +11,7 @@ esac
 sudo true
 # If usb-reset is installed, use it
 if [ -x "$(command -v usb-reset)" ]; then
-    sudo usb-reset ${VENDOR}:${PRODUCT}
+    sudo usb-reset "${VENDOR}":"${PRODUCT}"
 elif [ -x "$(command -v usbreset)" ]; then
     # Get the bus and device number of the SSL 2
     SSL2="$(lsusb -d ${VENDOR}: | grep "SSL 2")"
@@ -19,7 +19,7 @@ elif [ -x "$(command -v usbreset)" ]; then
     #Bus 001 Device 009: ID 31e9:0002 Solid State Logic SSL 2+
     BUS="$(echo "${SSL2}" | cut -d' ' -f 2)"
     DEV="$(echo "${SSL2}" | cut -d' ' -f 4 | cut -d':' -f 1)"
-    sudo usbreset ${BUS}/${DEV}
+    sudo usbreset "${BUS}"/"${DEV}"
 else
     echo "ERROR! usb-reset or usbreset is not installed. Please install it."
     exit 1
