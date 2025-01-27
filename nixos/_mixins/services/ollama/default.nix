22952a24c960919a1be77000f6129a9a236da568
Martin Wimpress <martin@wimpress.org>
2025-01-27 12:51:28 +0000

    refactor: update ollama LLMs

--- nixos/_mixins/services/ollama/default.nix
+++ nixos/_mixins/services/ollama/default.nix
@@ -29,7 +29,6 @@ in
       })
       (lib.optionalAttrs installOpenWebUI {
         open-webui-log = "journalctl _SYSTEMD_UNIT=open-webui.service";
-        tika-log = "journalctl _SYSTEMD_UNIT=tika.service";
       })
     ];
     systemPackages = lib.mkIf hasAcceleration (with pkgs; [
@@ -46,18 +45,16 @@ in
         "nemotron-mini:4b"
         "nomic-embed-text:latest"
       ] else [
-        "codegemma:7b"                  #8k
         "codestral:22b"                 #32k
-        "gemma2:9b"                     #4k
+        "deepseek-r1:14b"               #128k
         "llama3-chatqa:8b"              #8k     RAG
         "llama3.2-vision:11b"
         "llama3.1:8b"                   #128k
-        "mistral-small:22b"             #128k
+        "mistral-nemo:12b"              #128k
         "nemotron-mini:4b"              #4k     RAG
         "nomic-embed-text:latest"
-        "phi3:14b"                      #128k
+        "phi4:14b"                      #16K
         "qwen2.5-coder:14b"             #128k
-        "solar-pro:22b"                 #4k
       ];
     };
     open-webui = {
@@ -79,20 +76,20 @@ in
         ENABLE_SEARCH_QUERY = "true";
         ENABLE_SIGNUP = "true";
         IMAGE_GENERATION_ENGINE = "openai";
-        MODEL_FILTER_LIST = "nemotron-mini:4b;codegemma:7b;codestral:22b;gemma2:9b;llama3-chatqa:8b;llama3.2-vision:11b;llama3.1:8b;mistral-nemo:12b;mistral-small:22b;nemotron-mini:4b;phi3:14b;qwen2.5-coder:14b;gpt-4;gpt-4o;gpt-4o-mini;o1-mini;claude-3-5-haiku-latest;claude-3-5-sonnet-latest";
+        MODEL_FILTER_LIST = "codestral:22b;deepseek-r1:14b;llama3-chatqa:8b;llama3.2-vision:11b;llama3.1:8b;mistral-nemo:12b;nemotron-mini:4b;phi4:14b;qwen2.5-coder:14b;gpt-4;gpt-4o;gpt-4o-mini;o1-mini;claude-3-5-haiku-latest;claude-3-5-sonnet-latest";
         OLLAMA_BASE_URLS = baseUrls;
-        #RAG_EMBEDDING_BATCH_SIZE = "16";
-        #RAG_EMBEDDING_ENGINE = "ollama";
-        #RAG_EMBEDDING_MODEL = "nomic-embed-text:latest";
+        RAG_EMBEDDING_BATCH_SIZE = "16";
+        RAG_EMBEDDING_ENGINE = "ollama";
+        RAG_EMBEDDING_MODEL = "nomic-embed-text:latest";
         # https://github.com/open-webui/open-webui/issues/7333#issuecomment-2512287381
-        #RAG_OLLAMA_BASE_URL = "http://config.services.ollama.host}:${toString config.services.ollama.port}";
-        #RAG_TEXT_SPLITTER = "token";
+        RAG_OLLAMA_BASE_URL = "http://config.services.ollama.host}:${toString config.services.ollama.port}";
+        RAG_TEXT_SPLITTER = "token";
         RAG_WEB_SEARCH_ENGINE = "brave";
         RAG_WEB_SEARCH_RESULT_COUNT = "5";
         RAG_WEB_SEARCH_CONCURRENT_REQUESTS = "2";
         RESET_CONFIG_ON_START = "true";
-        TASK_MODEL = "gemma2:9b";
-        #TIKA_SERVER_URL = "http://${config.services.tika.listenAddress}:${toString config.services.tika.port}";
+        TASK_MODEL = "llama3.1:8b";
+        TIKA_SERVER_URL = "http://${config.services.tika.listenAddress}:${toString config.services.tika.port}";
         USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) OpenWebUI/${pkgs.open-webui.version} Chrome/131.0.0.0 Safari/537.36";
         WEBUI_NAME = "${sithLord} Chat";
         WEBUI_URL = if (config.services.tailscale.enable && config.services.caddy.enable) then
