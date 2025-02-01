b5a8f402d094fb9cb13e6c992104fa6d8414c506
Martin Wimpress <martin@wimpress.org>
2025-02-01 17:02:23 +0000

    chore: make revan use dhcp

--- nixos/_mixins/features/network/revan.nix
+++ nixos/_mixins/features/network/revan.nix
@@ -1,18 +1,18 @@
 { lib, ... }:
 {
   networking = {
-    defaultGateway = "192.168.2.1";
+    #defaultGateway = "192.168.2.1";
     firewall = {
       trustedInterfaces = [ "eth0" ];
     };
     interfaces.eth0.mtu = 1462;
-    interfaces.eth0.ipv4.addresses = [
-      {
-        address = "192.168.2.18";
-        prefixLength = 24;
-      }
-    ];
-    useDHCP = lib.mkForce false;
+    #interfaces.eth0.ipv4.addresses = [
+    #  {
+    #    address = "192.168.2.18";
+    #    prefixLength = 24;
+    #  }
+    #];
+    useDHCP = lib.mkForce true;
     usePredictableInterfaceNames = false;
   };
 }
