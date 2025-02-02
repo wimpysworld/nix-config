f2bfc46f1f6b26726cfe9fc46d98ddd8472f0229
Martin Wimpress <martin@wimpress.org>
2025-02-02 21:56:51 +0000

    chore: change revan ip address

--- nixos/_mixins/features/network/revan.nix
+++ nixos/_mixins/features/network/revan.nix
@@ -1,18 +1,18 @@
 { lib, ... }:
 {
   networking = {
-    #defaultGateway = "192.168.2.1";
+    defaultGateway = "10.10.10.1";
     firewall = {
       trustedInterfaces = [ "eth0" ];
     };
     interfaces.eth0.mtu = 1462;
-    #interfaces.eth0.ipv4.addresses = [
-    #  {
-    #    address = "192.168.2.18";
-    #    prefixLength = 24;
-    #  }
-    #];
-    useDHCP = lib.mkForce true;
+    interfaces.eth0.ipv4.addresses = [
+      {
+        address = "10.10.10.10";
+        prefixLength = 24;
+      }
+    ];
+    useDHCP = lib.mkForce false;
     usePredictableInterfaceNames = false;
   };
 }
