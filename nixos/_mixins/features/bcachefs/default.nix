ff9199316d4185b503abf804eef16f4494cb5526
Martin Wimpress <martin@wimpress.org>
2025-02-07 17:13:26 +0000

    chore: remove bcachefs workarounds and experiments

--- nixos/_mixins/features/bcachefs/default.nix
+++ nixos/_mixins/features/bcachefs/default.nix
@@ -7,19 +7,13 @@
 }:
 let
   installOn = [
-    "crawler"
-    "dagger"
     "shaa"
     "sidious"
   ];
 in
 lib.mkIf (lib.elem hostname installOn || isISO) {
-  # Create a bootable ISO image with bcachefs.
+  # Bootable ISO images include bcachefs tools
   # - https://wiki.nixos.org/wiki/Bcachefs
-  boot = {
-    kernelPackages = lib.mkOverride 0 pkgs.linuxPackages_6_12;
-    supportedFilesystems = [ "bcachefs" ];
-  };
   environment.systemPackages = with pkgs; [
     bcachefs-tools
     keyutils
--- nixos/iso-console/default.nix
+++ nixos/iso-console/default.nix
@@ -3,15 +3,6 @@ _: {
     (_final: super: {
       # Prevent mbrola-voices (~650MB) from being on the live media
       espeak = super.espeak.override { mbrolaSupport = false; };
-
-      # Makes `availableOn` fail for zfs, see <nixos/modules/profiles/base.nix>.
-      # This is a workaround since we cannot remove the `"zfs"` string from `supportedFilesystems`.
-      # The proper fix would be to make `supportedFilesystems` an attrset with true/false which we
-      # could then `lib.mkForce false`
-      # - https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix
-      zfs = super.zfs.overrideAttrs (_: {
-        meta.platforms = [ ];
-      });
     })
   ];
 }
