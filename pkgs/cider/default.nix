1a85744d00e0a5b9a955ab92470e90ccff5a5292
Martin Wimpress <martin@wimpress.org>
2025-03-03 11:34:03 +0000

    chore: update Cider to 2.6.1

--- pkgs/cider/default.nix
+++ pkgs/cider/default.nix
@@ -1,18 +1,18 @@
 {
   lib,
   appimageTools,
-  fetchurl,
   makeWrapper,
   requireFile,
 }:
 let
-  version = "2.6.0";
-  pname = "cider";
+  version = "2.6.1";
+  pname = "Cider";
   src = requireFile rec {
     name = "cider-linux-x64.AppImage";
     url= "https://cidercollective.itch.io/cider";
+    # nix-store --add-fixed sha256 pkgs/cider/cider-linux-x64.AppImage
     # sha256sum /nix/store/deadb33f-cider-linux-x64.AppImage
-    sha256 = "ca16d4deeddc59c7be6b55c0d671d2f8590d3576c29c3afb0c1da8ba54fd7776";
+    sha256 = "6ee1ee9d4b45419d7860d1e7831dc7c2a9b94689f013a0bf483876c6b4d65062";
   };
   appimageContents = appimageTools.extractType2 {
     inherit pname version src;
@@ -26,7 +26,7 @@ appimageTools.wrapType2 {
   # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1077345#10
   extraInstallCommands = ''
     source "${makeWrapper}/nix-support/setup-hook"
-      wrapProgram $out/bin/${pname} \
+      wrapProgram $out/bin/Cider \
         --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --disable-features=UseMultiPlaneFormatForSoftwareVideo --disable-gpu-memory-buffer-video-frames}}"
       install -m 444 -D ${appimageContents}/Cider.desktop \
         $out/share/applications/Cider.desktop
@@ -39,7 +39,7 @@ appimageTools.wrapType2 {
     downloadPage = "https://cidercollective.itch.io/cider";
     homepage = "https://cider.sh/";
     license = lib.licenses.unfree;
-    mainProgram = "cider";
+    mainProgram = "Cider";
     maintainers = with lib.maintainers; [ flexiondotorg ];
     platforms = [ "x86_64-linux" ];
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
