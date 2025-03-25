56a2d87b7f94c9b2c463cda1b0cf8a8d3daf6c1c
Martin Wimpress <martin@wimpress.org>
2025-03-25 11:52:16 +0000

    fix(vscode): prevent FTBFS

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
 
     catppuccin.url = "https://flakehub.com/f/catppuccin/nix/*";
     catppuccin-vsc.url = "https://flakehub.com/f/catppuccin/vscode/*";
-    catppuccin-vsc.inputs.nixpkgs.follows = "nixpkgs";
+    catppuccin-vsc.inputs.nixpkgs.follows = "nixpkgs-unstable";
     disko.url = "https://flakehub.com/f/nix-community/disko/1.11.0.tar.gz";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "https://flakehub.com/f/NixOS/nixos-hardware/*";
