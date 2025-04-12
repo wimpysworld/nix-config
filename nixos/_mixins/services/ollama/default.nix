57634f58195535e172a20119b6a0d09ad25b81b6
Martin Wimpress <martin@wimpress.org>
2025-04-12 09:52:05 +0100

    refactor(ollama): update LLM selection

--- nixos/_mixins/services/ollama/default.nix
+++ nixos/_mixins/services/ollama/default.nix
@@ -41,21 +41,21 @@ in
       enable = hasAcceleration;
       host = if hostname == "revan" then "127.0.0.1" else "0.0.0.0";
       package = pkgs.unstable.ollama;
-      loadModels = if hostname == "revan" then [
+      loadModels = [
+        "gemma3:12b"                #128k (multi-modal)
         "mxbai-embed-large:335m"
-        "nemotron-mini:4b"
         "nomic-embed-text:latest"
-      ] else [
-        "codestral:22b"                 #32k
-        "deepseek-r1:14b"               #128k
-        "llama3.2-vision:11b"
-        "mistral-nemo:12b"              #128k
-        "mxbai-embed-large:335m"
-        "nemotron-mini:4b"
-        "nomic-embed-text:latest"
-        "phi4:14b"                      #16K
-        "phi4:14b-q8_0"                 #16K
-        "qwen2.5-coder:14b"             #128k
+        "phi4-mini:3.8b"            #128k (task)
+      ] ++ lib.optionals (hostname == "revan") [
+        #"deepseek-r1:32b"          #128k
+        #"qwq:latest"                    
+      ] ++ lib.optionals (hostname == "vader" || hostname == "phasma") [
+        "codestral:22b"             #32k  (code)
+        "deepcoder:14b"             #128k (code) 
+        "deepseek-r1:14b"           #128k (code)
+        #"mistral-small3.1:latest"   #128k (multi-modal)
+        "phi4:14b"                  #16k  (general)
+        "qwen2.5-coder:14b"         #128k (code)
       ];
     };
     open-webui = {
@@ -65,7 +65,7 @@ in
         CHUNK_SIZE = "1536";
         CHUNK_OVERLAP = "128";
         #CONTENT_EXTRACTION_ENGINE = "tika";
-        DEFAULT_MODELS = "phi4:14b";
+        DEFAULT_MODELS = "gemma3:12b";
         DEFAULT_USER_ROLE = "user";
         ENABLE_EVALUATION_ARENA_MODELS = "false";
         ENABLE_IMAGE_GENERATION = "true";
@@ -77,7 +77,9 @@ in
         ENABLE_SEARCH_QUERY = "true";
         ENABLE_SIGNUP = "true";
         IMAGE_GENERATION_ENGINE = "openai";
-        MODEL_FILTER_LIST = "codestral:22b;deepseek-r1:14b;llama3.2-vision:11b;mistral-nemo:12b;nemotron-mini:4b;phi4:14b;phi4:14b-q8_0;qwen2.5-coder:14b;gpt-4;gpt-4o;gpt-4o-mini;o1-mini;claude-3-5-haiku-latest;claude-3-5-sonnet-latest";
+        MODEL_FILTER_LIST = if hostname == "revan" 
+          then "gemma3:12b;phi4-mini:3.8b"
+          else "codestral:22b;deepcoder:14b;deepseek-r1:14b;gemma3:12b:mistral-small3.1:latest;phi4:14b;phi4-mini:3.8b;qwen2.5-coder:14b";
         OLLAMA_BASE_URLS = baseUrls;
         RAG_EMBEDDING_BATCH_SIZE = "16";
         RAG_EMBEDDING_ENGINE = "ollama";
@@ -89,7 +91,7 @@ in
         RAG_WEB_SEARCH_RESULT_COUNT = "5";
         RAG_WEB_SEARCH_CONCURRENT_REQUESTS = "2";
         RESET_CONFIG_ON_START = "true";
-        TASK_MODEL = "phi4:14b";
+        TASK_MODEL = "phi4-mini:latest";
         TIKA_SERVER_URL = "http://${config.services.tika.listenAddress}:${toString config.services.tika.port}";
         USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) OpenWebUI/${pkgs.open-webui.version} Chrome/131.0.0.0 Safari/537.36";
         WEBUI_NAME = "${sithLord} Chat";
