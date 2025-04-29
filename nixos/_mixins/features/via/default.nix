c575440ca32d5264a21990406e83760ffa29f7a1
Martin Wimpress <martin@wimpress.org>
2025-04-29 17:49:28 +0100

    feat(via/qmk): add cstm80 to udev

--- nixos/_mixins/features/via/default.nix
+++ nixos/_mixins/features/via/default.nix
@@ -4,9 +4,10 @@
     # Provides users with access to VIA
     # https://get.vial.today/manual/linux-udev.html
     udev.extraRules = ''
-      #KERNEL=="hidraw*", SUBSYSTEM=="hidraw", TAG+="uaccess", TAG+="udev-acl", GROUP="input", MODE="0660"
       #0x320f 0x5055
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="320f", ATTRS{idProduct}=="5055", TAG+="uaccess", TAG+="udev-acl", GROUP="input", MODE="0660", SYMLINK+="crush80"
+      #0x359b 0x0004
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="359b", ATTRS{idProduct}=="0004", TAG+="uaccess", TAG+="udev-acl", GROUP="input", MODE="0660", SYMLINK+="CSTM80"
     '';
   };
   users.users.${username} = {
