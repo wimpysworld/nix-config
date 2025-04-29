712083ff011d2c0bcfc5720abbc2782a1319207f
Martin Wimpress <martin@wimpress.org>
2025-04-29 17:06:48 +0100

    feat: configures open-webui with qwen3 models
    Configures open-webui to use qwen3 models by default. This includes setting the default model, task model, and updating the model filter list. Also adds qwen3 to the list of available models for the revan.

--- nixos/_mixins/services/ollama/default.nix
+++ nixos/_mixins/services/ollama/default.nix
@@ -41,30 +41,19 @@ in
       enable = hasAcceleration;
       host = if hostname == "revan" then "127.0.0.1" else "0.0.0.0";
       loadModels = [
-        "falcon3:7b"                  #32k  (task)
         "gemma3:12b"                  #128k (multi-modal)
         "mxbai-embed-large:latest"    #     (embedding)
         "nomic-embed-text:latest"     #     (embedding)
         "phi4-mini:3.8b"              #128k (task)
+        "qwen3:4b"                    #32k  (task)
       ] ++ lib.optionals (hostname == "revan") [
-        #"codestral:22b"               #32k  (code)
         #"cogito:32b"                  #128k (stem)
-        #"deepcoder:14b"               #128k (code) 
-        #"deepseek-r1:32b"             #128k (reasoning)
-        #"mistral-small3.1:24b"        #128k (multi-modal)
         #"phi-4:14b"                   #16k  (general)
-        #"qwen2.5:32b-instruct"        #128k (general)
-        #"qwen2.5-coder:32b"           #128k (code)
-        #"qwq:latest"                  #128k (reasoning)
+        #"qwen3:32b"                   #128k (general)
       ] ++ lib.optionals (hostname == "vader" || hostname == "phasma") [
-        "codestral:22b"               #32k  (code)
         "cogito:14b"                  #128k (stem)
-        "deepcoder:14b"               #128k (code) 
-        "deepseek-r1:14b"             #128k (reasoning)
-        "mistral-small3.1:24b"        #128k (multi-modal)
         "phi-4:14b"                   #16k  (general)
-        "qwen2.5:14b-instruct"        #128k (general)
-        "qwen2.5-coder:14b"           #128k (code)
+        "qwen3:14b"                   #128k (general)
       ];
     };
     open-webui = {
@@ -86,7 +75,7 @@ in
         ENABLE_SEARCH_QUERY = "true";
         ENABLE_SIGNUP = "true";
         IMAGE_GENERATION_ENGINE = "openai";
-        MODEL_FILTER_LIST = "codestral:22b;deepcoder:14b;deepseek-r1:14b;falcon3:7b;gemma3:12b:mistral-small3.1:latest;vanilj/phi-4-unsloth:latest;phi4-mini:3.8b;qwen2.5:14b-instruct;qwen2.5-coder:14b";
+        MODEL_FILTER_LIST = "cogito:14b;gemma3:12b;phi-4:14b;phi4-mini:3.8b;qwen3:4b;qwen3:14b";
         OLLAMA_BASE_URLS = baseUrls;
         RAG_EMBEDDING_BATCH_SIZE = "16";
         RAG_EMBEDDING_ENGINE = "ollama";
@@ -98,7 +87,7 @@ in
         RAG_WEB_SEARCH_RESULT_COUNT = "10";
         RAG_WEB_SEARCH_CONCURRENT_REQUESTS = "2";
         RESET_CONFIG_ON_START = "true";
-        TASK_MODEL = "phi4-mini:latest";
+        TASK_MODEL = "qwen3:4b";
         TIKA_SERVER_URL = "http://${config.services.tika.listenAddress}:${toString config.services.tika.port}";
         USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) OpenWebUI/${pkgs.open-webui.version} Chrome/131.0.0.0 Safari/537.36";
         WEBUI_NAME = "${sithLord} Chat";
