fdbf769ce051dd1aa9a3f8c453b7a670e2cd7cf0
Martin Wimpress <martin@wimpress.org>
2025-05-12 10:21:41 +0100

    chore: add crush80 2.4g udev rule

--- nixos/_mixins/features/via/default.nix
+++ nixos/_mixins/features/via/default.nix
@@ -4,8 +4,10 @@
     # Provides users with access to VIA
     # https://get.vial.today/manual/linux-udev.html
     udev.extraRules = ''
-      #0x320f 0x5055
+      #0x320f 0x5055 Crush80
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="320f", ATTRS{idProduct}=="5055", TAG+="uaccess", TAG+="udev-acl", GROUP="input", MODE="0660", SYMLINK+="crush80"
+      #0x320f 0x5088 Crush80-2.4G
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="320f", ATTRS{idProduct}=="5088", TAG+="uaccess", TAG+="udev-acl", GROUP="input", MODE="0660", SYMLINK+="crush80-24g"
       #0x359b 0x0004
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="359b", ATTRS{idProduct}=="0004", TAG+="uaccess", TAG+="udev-acl", GROUP="input", MODE="0660", SYMLINK+="CSTM80"
     '';
