0ef4bb3e9f27197fd7150b1e26e4097d18583c94
Martin Wimpress <martin@wimpress.org>
2025-05-15 22:40:56 +0100

    feat(cider): no longer needs requireFile as it is Taproom account locked

--- pkgs/cider/default.nix
+++ pkgs/cider/default.nix
@@ -1,18 +1,15 @@
 {
   lib,
   appimageTools,
+  fetchurl,
   makeWrapper,
-  requireFile,
 }:
 let
-  version = "2.6.1";
+  version = "3.0.2";
   pname = "Cider";
-  src = requireFile rec {
-    name = "cider-linux-x64.AppImage";
-    url= "https://cidercollective.itch.io/cider";
-    # nix-store --add-fixed sha256 pkgs/cider/cider-linux-x64.AppImage
-    # sha256sum /nix/store/deadb33f-cider-linux-x64.AppImage
-    sha256 = "6ee1ee9d4b45419d7860d1e7831dc7c2a9b94689f013a0bf483876c6b4d65062";
+  src = fetchurl {
+    url = "https://warez.wimpys.world/cider-v${version}-linux-x64.AppImage";
+    hash = "sha256-XVBhMgSNJAYTRpx5GGroteeOx0APIzuHCbf+kINT2eU=";
   };
   appimageContents = appimageTools.extractType2 {
     inherit pname version src;
@@ -36,7 +33,7 @@ appimageTools.wrapType2 {
 
   meta = {
     description = "Cider is a new cross-platform Apple Music experience";
-    downloadPage = "https://cidercollective.itch.io/cider";
+    downloadPage = "https://taproom.cider.sh/downloads";
     homepage = "https://cider.sh/";
     license = lib.licenses.unfree;
     mainProgram = "Cider";
