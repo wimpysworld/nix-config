b629252fbaafc40e923a5366674d6501075f5799
Martin Wimpress <martin@wimpress.org>
2025-06-06 11:04:40 +0100

    chore: add Evoworks EVO80 support
     Adds udev rules for Evoworks EVO80 device to enable proper HID handling

--- nixos/_mixins/features/via/default.nix
+++ nixos/_mixins/features/via/default.nix
@@ -8,8 +8,10 @@
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="320f", ATTRS{idProduct}=="5055", TAG+="uaccess", TAG+="udev-acl", GROUP="input", MODE="0660", SYMLINK+="crush80"
       #0x320f 0x5088 Crush80-2.4G
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="320f", ATTRS{idProduct}=="5088", TAG+="uaccess", TAG+="udev-acl", GROUP="input", MODE="0660", SYMLINK+="crush80-24g"
-      #0x359b 0x0004
+      #0x359b 0x0004 Drop CSTM80
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="359b", ATTRS{idProduct}=="0004", TAG+="uaccess", TAG+="udev-acl", GROUP="input", MODE="0660", SYMLINK+="CSTM80"
+      #0x36b0 0x300e Evoworks EVO80
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36b0", ATTRS{idProduct}=="300e", TAG+="uaccess", TAG+="udev-acl", GROUP="input", MODE="0660", SYMLINK+="evo80"
     '';
   };
   users.users.${username} = {
