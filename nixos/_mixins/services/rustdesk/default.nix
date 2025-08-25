ffa12414190b29d2016abb1be3d899e80e506998
Martin Wimpress <martin@wimpress.org>
2025-08-25 11:52:17 +0100

    feat: add rustdesk

---
+++ nixos/_mixins/services/rustdesk/default.nix
@@ -0,0 +1,13 @@
+{ hostname, lib, pkgs, ... }:
+let
+  installOn = [ "vader" "phasma" ];
+in
+lib.mkIf (lib.elem hostname installOn) {
+  services.rustdesk-server = {
+    enable = true;
+    openFirewall = true;
+    relay.enable = true;
+    signal.enable = true;
+    signal.relayHosts = [ "vader" "phasma" ];
+  };
+}
