ef496bcbddc542a9ef765acf836206c8c8e603b8
Martin Wimpress <martin@wimpress.org>
2025-08-25 14:48:10 +0100

    refactor: migrate Cider package to use the .deb

--- pkgs/cider/default.nix
+++ pkgs/cider/default.nix
@@ -1,34 +1,118 @@
 {
   lib,
-  appimageTools,
+  stdenv,
   fetchurl,
   makeWrapper,
+  addDriverRunpath,
+  autoPatchelfHook,
+  wrapGAppsHook,
+  alsa-lib,
+  cairo,
+  cups,
+  dbus,
+  expat,
+  fontconfig,
+  freetype,
+  gdk-pixbuf,
+  glib,
+  gtk3,
+  libdrm,
+  libglvnd,
+  libxkbcommon,
+  mesa,
+  nspr,
+  nss,
+  pango,
+  vulkan-loader,
+  xorg,
 }:
 let
   version = "3.1.0";
-  pname = "Cider";
-  src = fetchurl {
-    url = "https://warez.wimpys.world/cider-v${version}-linux-x64.AppImage";
-    hash = "sha256-vjwfpKm02f8jePWMnhlPN/OIhB3UUjnon8LUjBWMX0Q=";
-  };
-  appimageContents = appimageTools.extractType2 {
-    inherit pname version src;
-  };
+  pname = "cider";
 in
-appimageTools.wrapType2 {
-  inherit pname version src;
-  # Disable UseMultiPlaneFormatForSoftwareVideo on Wayland
-  # https://github.com/Legcord/Legcord/issues/741
-  # Disable GPU memory buffer for video frames
-  # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1077345#10
-  extraInstallCommands = ''
-    source "${makeWrapper}/nix-support/setup-hook"
-      wrapProgram $out/bin/Cider \
-        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --disable-features=UseMultiPlaneFormatForSoftwareVideo --disable-gpu-memory-buffer-video-frames}}"
-      install -m 444 -D ${appimageContents}/Cider.desktop \
-        $out/share/applications/Cider.desktop
-      install -m 444 -D ${appimageContents}/Cider.png \
-        $out/share/icons/hicolor/256x256/apps/Cider.png
+stdenv.mkDerivation {
+  inherit pname version;
+
+  src = fetchurl {
+    url = "https://warez.wimpys.world/cider-v${version}-linux-x64.deb";
+    hash = "sha256-3+I5yahVcvK4YyhMfWJIpLeq1uCrhWmrzuH7/cgLwuI=";
+  };
+
+  nativeBuildInputs = [
+    makeWrapper
+    autoPatchelfHook
+    wrapGAppsHook
+    addDriverRunpath
+  ];
+
+  buildInputs = [
+    alsa-lib
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    gtk3
+    libdrm
+    libglvnd
+    libxkbcommon
+    mesa
+    nspr
+    nss
+    pango
+    vulkan-loader
+    xorg.libX11
+    xorg.libXext
+    xorg.libxcb
+  ];
+
+  unpackPhase = ''
+    runHook preUnpack
+    ar x $src
+    tar --no-same-permissions --no-same-owner -xf data.tar.*
+    runHook postUnpack
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    # Create directory structure
+    mkdir -p $out/{bin,lib,share/{applications,icons/hicolor/256x256/apps}}
+
+    # Copy the application files
+    cp -r usr/lib/cider $out/lib/
+    ln -s $out/lib/cider/Cider $out/bin/cider
+
+    # Install desktop integration
+    cp usr/share/applications/cider.desktop $out/share/applications/
+    cp usr/share/pixmaps/cider.png $out/share/icons/hicolor/256x256/apps/
+
+    runHook postInstall
+  '';
+
+
+  # Enhanced GPU acceleration and driver path setup
+  postInstall = ''
+    # Add driver paths to the binary for GPU acceleration
+    addDriverRunpath $out/lib/cider/Cider
+  '';
+
+  postFixup = ''
+    # Create comprehensive library path for OpenGL/Vulkan support
+    libPath="${lib.makeLibraryPath [
+      libglvnd
+    ]}"
+
+    # Enhanced wrapper with proper OpenGL environment
+    wrapProgram $out/lib/cider/Cider \
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
+      --set LD_LIBRARY_PATH "$libPath" \
+      --set LIBGL_DRIVERS_PATH "${mesa}/lib/dri" \
+      --set __EGL_VENDOR_LIBRARY_DIRS "${mesa}/share/glvnd/egl_vendor.d" \
+      --set VK_LAYER_PATH "${vulkan-loader}/share/vulkan/explicit_layer.d"
   '';
 
   meta = {
@@ -36,7 +120,7 @@ appimageTools.wrapType2 {
     downloadPage = "https://taproom.cider.sh/downloads";
     homepage = "https://cider.sh/";
     license = lib.licenses.unfree;
-    mainProgram = "Cider";
+    mainProgram = "cider";
     maintainers = with lib.maintainers; [ flexiondotorg ];
     platforms = [ "x86_64-linux" ];
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
