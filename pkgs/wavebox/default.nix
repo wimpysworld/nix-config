59308876e1954ca121bc603c7ba6ab4dec2d4a20
Martin Wimpress <martin@wimpress.org>
2025-09-07 09:17:20 +0100

    chore: update wavebox to version 10.139.23-2
    Updates wavebox package from version 10.139.20-2 to 10.139.23-2 for both Linux and macOS platforms with corresponding hash updates for the new release artifacts.

--- pkgs/wavebox/default.nix
+++ pkgs/wavebox/default.nix
@@ -172,11 +172,11 @@ let
 
   linux = stdenv.mkDerivation (finalAttrs: {
     inherit pname meta passthru;
-    version = "10.139.20-2";
+    version = "10.139.23-2";
 
     src = fetchurl {
       url = "https://download.wavebox.app/stable/linux/deb/amd64/wavebox_${finalAttrs.version}_amd64.deb";
-      hash = "sha256-/Zee2JoVlqmRJXnN7igx6u6vMd4DvPUzdtnkmb167oQ=";
+      hash = "sha256-qP3gHEg6ZbrKoqLmQAlZdqPpu67OXoYK/fcur2pdyAk=";
     };
 
     # With strictDeps on, some shebangs were not being patched correctly
@@ -264,7 +264,7 @@ let
 
   darwin = stdenvNoCC.mkDerivation (finalAttrs: {
     inherit pname meta passthru;
-    version = "10.139.20.2";
+    version = "10.139.23.2";
 
     src = fetchurl {
       url = "https://download.wavebox.app/stable/macuniversal/Install%20Wavebox%20${finalAttrs.version}.dmg";
