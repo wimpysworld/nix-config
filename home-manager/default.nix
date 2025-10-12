08a9b9869cd352219d52e6fd389b26b82ed93ee0
Martin Wimpress <martin@wimpress.org>
2025-10-12 17:37:20 +0100

    refactor: merge fonts mixin into main home-manager config
    - Consolidates font configuration directly into the main home-manager module
    - Removes separate fonts mixin to simplify module structure
    - Maintains all existing font packages and fontconfig settings
    - Removes fonts import from module imports list

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -27,7 +27,6 @@ in
     inputs.vscode-server.nixosModules.home
     ./_mixins/ai
     ./_mixins/development
-    ./_mixins/fonts
     ./_mixins/scripts
     ./_mixins/services
     ./_mixins/terminal
@@ -54,12 +53,99 @@ in
         "/home/${username}.linux"
       else
         "/home/${username}";
+    file.".config/fontconfig/fonts.conf".text = ''
+      <?xml version="1.0"?>
+      <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+      <fontconfig>
+        <match target="font">
+          <edit name="antialias" mode="assign">
+            <bool>true</bool>
+          </edit>
+          <edit name="hinting" mode="assign">
+            <bool>true</bool>
+          </edit>
+          <edit name="hintstyle" mode="assign">
+            <const>hintslight</const>
+          </edit>
+          <edit name="rgba" mode="assign">
+            <const>rgb</const>
+          </edit>
+          <edit name="lcdfilter" mode="assign">
+            <const>lcddefault</const>
+          </edit>
+        </match>
+      </fontconfig>
+    '';
+    packages =
+      with pkgs;
+      [
+        nerd-fonts.fira-code
+        font-awesome
+        noto-fonts-emoji
+        noto-fonts-monochrome-emoji
+        symbola
+        work-sans
+      ]
+      ++ lib.optionals isWorkstation [
+        bebas-neue-2014-font
+        bebas-neue-pro-font
+        bebas-neue-rounded-font
+        bebas-neue-semi-rounded-font
+        bw-fusiona-font
+        boycott-font
+        commodore-64-pixelized-font
+        corefonts
+        digital-7-font
+        dirty-ego-font
+        fira-go
+        fira-sans
+        fixedsys-core-font
+        fixedsys-excelsior-font
+        impact-label-font
+        lato
+        liberation_ttf
+        mocha-mattari-font
+        nerd-fonts.space-mono
+        nerd-fonts.symbols-only
+        poppins-font
+        source-serif
+        spaceport-2006-font
+        ubuntu_font_family
+        unscii
+        zx-spectrum-7-font
+      ];
     sessionVariables = {
       COLORTERM = "truecolor";
       EDITOR = "micro";
       SUDO_EDITOR = "micro";
       SYSTEMD_EDITOR = "micro";
       VISUAL = "micro";
+    };
+  };
+
+  fonts = {
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [
+          "Source Serif"
+          "Noto Color Emoji"
+        ];
+        sansSerif = [
+          "Work Sans"
+          "Noto Color Emoji"
+        ];
+        monospace = [
+          "FiraCode Nerd Font Mono"
+          "Font Awesome 6 Free"
+          "Font Awesome 6 Brands"
+          "Symbola"
+          "Noto Emoji"
+        ];
+        emoji = [
+          "Noto Color Emoji"
+        ];
+      };
     };
   };
 
