b80336d304250e43604162d2f7376b4ca371750a
Martin Wimpress <martin@wimpress.org>
2026-01-03 12:42:21 +0000

    refactor(zed): simplify JSON configuration setup
    Removes complex file override settings for JSONC files
    
    - Eliminates parser and trailing comma configuration overrides
    - Retains essential JSON5 and JSONL extension support
    - Simplifies Zed editor configuration structure

--- home-manager/_mixins/development/json/default.nix
+++ home-manager/_mixins/development/json/default.nix
@@ -17,21 +17,10 @@
       };
     };
     zed-editor = lib.mkIf config.programs.zed-editor.enable {
-      userSettings = {
-        overrides = [
-          {
-            files = [ "*.jsonc" ];
-            options = {
-              parser = "json";
-              trailingComma = "none";
-            };
-          }
-        ];
-        extensions = [
-          "json5"
-          "jsonl"
-        ];
-      };
+      extensions = [
+        "json5"
+        "jsonl"
+      ];
     };
   };
 }
