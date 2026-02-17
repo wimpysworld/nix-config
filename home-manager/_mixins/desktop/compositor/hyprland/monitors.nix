998c047052e3b3da9750748a6e98623a3516eaf4
Martin Wimpress <code@wimpress.io>
2026-02-17 01:27:33 +0000

    feat(desktop/compositor): standardise workspace names and update waybar icons
    - Replace verbose workspace names (Web, Work, Chat, Code, etc.) with
      numeric labels (1–8, 9, 10 -> 0) in Hyprland monitor presets
    - Update waybar workspace format-icons to a new, consistent glyph set
    - Files changed:
      - home-manager/_mixins/desktop/compositor/hyprland/monitors.nix
      -
    home-manager/_mixins/desktop/compositor/components/waybar/default.nix
    
    Signed-off-by: Martin Wimpress <code@wimpress.io>

--- home-manager/_mixins/desktop/compositor/hyprland/monitors.nix
+++ home-manager/_mixins/desktop/compositor/hyprland/monitors.nix
@@ -6,68 +6,68 @@ _: {
       "HDMI-A-1, 2560x1600@120Hz, 1920x0, 1.25"
     ];
     workspace = [
-      "1, name:Web, persistent:true, monitor:DP-1"
-      "2, name:Work, persistent:true, monitor:DP-1"
-      "3, name:Chat, persistent:true, monitor:DP-1"
-      "4, name:Code, persistent:true, monitor:DP-1"
-      "5, name:Git, persistent:true, monitor:DP-1"
-      "6, name:Note, persistent:true, monitor:DP-1"
-      "7, name:Make, persistent:true, monitor:DP-1"
-      "8, name:Fun, persistent:true, monitor:DP-1"
-      "9, name:Shell, persistent:true, monitor:HDMI-A-1, layoutopt:orientation:left"
-      "10,name:Dummy, persistent:true, monitor:DP-2"
+      "1, name:1, persistent:true, monitor:DP-1"
+      "2, name:2, persistent:true, monitor:DP-1"
+      "3, name:3, persistent:true, monitor:DP-1"
+      "4, name:4, persistent:true, monitor:DP-1"
+      "5, name:5, persistent:true, monitor:DP-1"
+      "6, name:6, persistent:true, monitor:DP-1"
+      "7, name:7, persistent:true, monitor:DP-1"
+      "8, name:8, persistent:true, monitor:DP-1"
+      "9, name:9, persistent:true, monitor:HDMI-A-1, layoutopt:orientation:left"
+      "10,name:0, persistent:true, monitor:DP-2"
     ];
   };
   shaa = {
     monitor = [ "eDP-1, 1920x1080@60Hz, auto, 1" ];
     workspace = [
-      "1, name:Web, persistent:true, monitor:eDP-1"
-      "2, name:Work, persistent:true, monitor:eDP-1"
-      "3, name:Chat, persistent:true, monitor:eDP-1"
-      "4, name:Code, persistent:true, monitor:eDP-1"
-      "5, name:Git, persistent:true, monitor:eDP-1"
-      "6, name:Note, persistent:true, monitor:eDP-1"
-      "7, name:Make, persistent:true, monitor:eDP-1"
-      "8, name:Fun, persistent:true, monitor:eDP-1"
+      "1, name:1, persistent:true, monitor:eDP-1"
+      "2, name:2, persistent:true, monitor:eDP-1"
+      "3, name:3, persistent:true, monitor:eDP-1"
+      "4, name:4, persistent:true, monitor:eDP-1"
+      "5, name:5, persistent:true, monitor:eDP-1"
+      "6, name:6, persistent:true, monitor:eDP-1"
+      "7, name:7, persistent:true, monitor:eDP-1"
+      "8, name:8, persistent:true, monitor:eDP-1"
     ];
   };
   atrius = {
     monitor = [ "eDP-1, 1920x1080@60Hz, auto, 1" ];
     workspace = [
-      "1, name:Web, persistent:true, monitor:eDP-1"
-      "2, name:Work, persistent:true, monitor:eDP-1"
-      "3, name:Chat, persistent:true, monitor:eDP-1"
-      "4, name:Code, persistent:true, monitor:eDP-1"
-      "5, name:Git, persistent:true, monitor:eDP-1"
-      "6, name:Note, persistent:true, monitor:eDP-1"
-      "7, name:Make, persistent:true, monitor:eDP-1"
-      "8, name:Fun, persistent:true, monitor:eDP-1"
+      "1, name:1, persistent:true, monitor:eDP-1"
+      "2, name:2, persistent:true, monitor:eDP-1"
+      "3, name:3, persistent:true, monitor:eDP-1"
+      "4, name:4, persistent:true, monitor:eDP-1"
+      "5, name:5, persistent:true, monitor:eDP-1"
+      "6, name:6, persistent:true, monitor:eDP-1"
+      "7, name:7, persistent:true, monitor:eDP-1"
+      "8, name:8, persistent:true, monitor:eDP-1"
     ];
   };
   bane = {
     monitor = [ "eDP-1, 2560x1600@60Hz, auto, 1" ];
     workspace = [
-      "1, name:Web, persistent:true, monitor:eDP-1"
-      "2, name:Work, persistent:true, monitor:eDP-1"
-      "3, name:Chat, persistent:true, monitor:eDP-1"
-      "4, name:Code, persistent:true, monitor:eDP-1"
-      "5, name:Git, persistent:true, monitor:eDP-1"
-      "6, name:Note, persistent:true, monitor:eDP-1"
-      "7, name:Make, persistent:true, monitor:eDP-1"
-      "8, name:Fun, persistent:true, monitor:eDP-1"
+      "1, name:1, persistent:true, monitor:eDP-1"
+      "2, name:2, persistent:true, monitor:eDP-1"
+      "3, name:3, persistent:true, monitor:eDP-1"
+      "4, name:4, persistent:true, monitor:eDP-1"
+      "5, name:5, persistent:true, monitor:eDP-1"
+      "6, name:6, persistent:true, monitor:eDP-1"
+      "7, name:7, persistent:true, monitor:eDP-1"
+      "8, name:8, persistent:true, monitor:eDP-1"
     ];
   };
   tanis = {
     monitor = [ "eDP-1, 1920x1200@60Hz, auto, 1" ];
     workspace = [
-      "1, name:Web, persistent:true, monitor:eDP-1"
-      "2, name:Work, persistent:true, monitor:eDP-1"
-      "3, name:Chat, persistent:true, monitor:eDP-1"
-      "4, name:Code, persistent:true, monitor:eDP-1"
-      "5, name:Git, persistent:true, monitor:eDP-1"
-      "6, name:Note, persistent:true, monitor:eDP-1"
-      "7, name:Make, persistent:true, monitor:eDP-1"
-      "8, name:Fun, persistent:true, monitor:eDP-1"
+      "1, name:1, persistent:true, monitor:eDP-1"
+      "2, name:2, persistent:true, monitor:eDP-1"
+      "3, name:3, persistent:true, monitor:eDP-1"
+      "4, name:4, persistent:true, monitor:eDP-1"
+      "5, name:5, persistent:true, monitor:eDP-1"
+      "6, name:6, persistent:true, monitor:eDP-1"
+      "7, name:7, persistent:true, monitor:eDP-1"
+      "8, name:8, persistent:true, monitor:eDP-1"
     ];
   };
   vader = {
@@ -77,16 +77,16 @@ _: {
       "DP-3, 1920x1080@60Hz, 320x2880, 1"
     ];
     workspace = [
-      "1, name:Web, persistent:true, monitor:DP-1"
-      "2, name:Work, persistent:true, monitor:DP-1"
-      "3, name:Chat, persistent:true, monitor:DP-2"
-      "4, name:Code, persistent:true, monitor:DP-2"
-      "5, name:Git, persistent:true, monitor:DP-2"
-      "6, name:Note, persistent:true, monitor:DP-2"
-      "7, name:Make, persistent:true, monitor:DP-1"
-      "8, name:Fun, persistent:true, monitor:DP-1"
-      "9, name:Shell, persistent:true, monitor:DP-1"
-      "10,name:Dummy, persistent:true, monitor:DP-3"
+      "1, name:1, persistent:true, monitor:DP-1"
+      "2, name:2, persistent:true, monitor:DP-1"
+      "3, name:3, persistent:true, monitor:DP-2"
+      "4, name:4, persistent:true, monitor:DP-2"
+      "5, name:5, persistent:true, monitor:DP-2"
+      "6, name:6, persistent:true, monitor:DP-2"
+      "7, name:7, persistent:true, monitor:DP-1"
+      "8, name:8, persistent:true, monitor:DP-1"
+      "9, name:9, persistent:true, monitor:DP-1"
+      "10,name:0, persistent:true, monitor:DP-3"
     ];
   };
 }
