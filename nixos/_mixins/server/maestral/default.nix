bb3716e3a17c49f18e5c1c431bbcbf55ee771f18
Martin Wimpress <code@wimpress.io>
2026-02-25 02:27:59 +0000

    refactor(server/maestral): gate maestral mixin by dropbox tag
    - Replace hard-coded host list with noughtyLib.hostHasTag "dropbox"
    gating
    - Add "dropbox" tag to hosts that previously had maestral enabled:
      bane, phasma, tanis, revan, sidious, vader
    - Preserve existing behaviour while making selection extensible by tag
    
    Signed-off-by: Martin Wimpress <code@wimpress.io>

--- nixos/_mixins/server/maestral/default.nix
+++ nixos/_mixins/server/maestral/default.nix
@@ -8,37 +8,28 @@
 let
   inherit (config.noughty) host;
 in
-lib.mkIf
-  (noughtyLib.isHost [
-    "bane"
-    "phasma"
-    "tanis"
-    "revan"
-    "sidious"
-    "vader"
-  ])
-  {
-    environment.systemPackages = with pkgs; [ maestral ];
+lib.mkIf (noughtyLib.hostHasTag "dropbox") {
+  environment.systemPackages = with pkgs; [ maestral ];
 
-    systemd.user.services.maestral = {
-      description = "Maestral";
-      wantedBy = [ "default.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.maestral}/bin/maestral start";
-        ExecReload = "${pkgs.util-linux}/bin/kill -HUP $MAINPID";
-        KillMode = "process";
-        Restart = "on-failure";
-      };
+  systemd.user.services.maestral = {
+    description = "Maestral";
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.maestral}/bin/maestral start";
+      ExecReload = "${pkgs.util-linux}/bin/kill -HUP $MAINPID";
+      KillMode = "process";
+      Restart = "on-failure";
     };
+  };
 
-    systemd.user.services.maestral-gui = lib.mkIf host.is.workstation {
-      description = "Maestral GUI";
-      wantedBy = [ "default.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.maestral-gui}/bin/maestral_qt";
-        ExecReload = "${pkgs.util-linux}/bin/kill -HUP $MAINPID";
-        KillMode = "process";
-        Restart = "on-failure";
-      };
+  systemd.user.services.maestral-gui = lib.mkIf host.is.workstation {
+    description = "Maestral GUI";
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.maestral-gui}/bin/maestral_qt";
+      ExecReload = "${pkgs.util-linux}/bin/kill -HUP $MAINPID";
+      KillMode = "process";
+      Restart = "on-failure";
     };
-  }
+  };
+}
