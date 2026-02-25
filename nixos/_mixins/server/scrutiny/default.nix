26217e0c22d4a6f10a37722fa2e68a2088b454d4
Martin Wimpress <code@wimpress.io>
2026-02-25 02:17:27 +0000

    feat(scrutiny): gate mixin by tag and add scrutiny tags to registry
    - Replace hardcoded host list with noughtyLib.hostHasTag "scrutiny"
      in nixos/_mixins/server/scrutiny/default.nix
    - Add "scrutiny" tag to vader, phasma, malak, maul and revan in
      lib/registry-systems.toml so those systems opt in
    
    Signed-off-by: Martin Wimpress <code@wimpress.io>

--- nixos/_mixins/server/scrutiny/default.nix
+++ nixos/_mixins/server/scrutiny/default.nix
@@ -8,32 +8,24 @@ let
   inherit (config.noughty) host;
   basePath = "/scrutiny";
 in
-lib.mkIf
-  (noughtyLib.isHost [
-    "malak"
-    "maul"
-    "revan"
-    "phasma"
-    "vader"
-  ])
-  {
-    services = {
-      # Reverse proxy scrutiny if Tailscale is enabled.
-      # https://github.com/AnalogJ/scrutiny/blob/master/docs/TROUBLESHOOTING_REVERSE_PROXY.md?plain=1#L62
-      caddy.virtualHosts."${host.name}.${config.noughty.network.tailNet}".extraConfig =
-        lib.mkIf (config.services.scrutiny.enable && config.services.tailscale.enable)
-          ''
-            redir ${basePath} ${basePath}/
-            reverse_proxy ${basePath}/* localhost:8080
-          '';
-      scrutiny = {
+lib.mkIf (noughtyLib.hostHasTag "scrutiny") {
+  services = {
+    # Reverse proxy scrutiny if Tailscale is enabled.
+    # https://github.com/AnalogJ/scrutiny/blob/master/docs/TROUBLESHOOTING_REVERSE_PROXY.md?plain=1#L62
+    caddy.virtualHosts."${host.name}.${config.noughty.network.tailNet}".extraConfig =
+      lib.mkIf (config.services.scrutiny.enable && config.services.tailscale.enable)
+        ''
+          redir ${basePath} ${basePath}/
+          reverse_proxy ${basePath}/* localhost:8080
+        '';
+    scrutiny = {
+      enable = true;
+      collector = {
         enable = true;
-        collector = {
-          enable = true;
-          settings.host.id = "${host.name}";
-          settings.api.endpoint = "http://localhost:8080${basePath}";
-        };
-        settings.web.listen.basepath = basePath;
+        settings.host.id = "${host.name}";
+        settings.api.endpoint = "http://localhost:8080${basePath}";
       };
+      settings.web.listen.basepath = basePath;
     };
-  }
+  };
+}
