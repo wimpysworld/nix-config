ac97cd79deceabb4b4294d254034d2a3b9a6daa4
Martin Wimpress <code@wimpress.io>
2026-04-07 16:19:48 +0100

    feat(disko): add snapshot RAID-0 configuration for skrye and zannah
    - Add disko snapshot disk configuration for skrye and zannah with RAID-0
      backup disks
    - Configure dual 4TB disks in RAID-0 with XFS filesystem mounted at
      /mnt/snapshot
    
    Signed-off-by: Martin Wimpress <code@wimpress.io>

---
+++ nixos/skrye/disks-snapshot.nix
@@ -0,0 +1,65 @@
+# sda     4TB:   Backup RAID-0
+# sdb     4TB:   Backup RAID-0
+_: {
+  disko.devices = {
+    disk = {
+      sda = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-TS4TMTS830S_H986540080";
+        content = {
+          type = "gpt";
+          partitions = {
+            snapshot-sda = {
+              start = "0%";
+              end = "100%";
+              content = {
+                type = "mdraid";
+                name = "snapshot";
+              };
+            };
+          };
+        };
+      };
+      sdb = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-TS4TMTS830S_H986540076";
+        content = {
+          type = "gpt";
+          partitions = {
+            snapshot-sdb = {
+              start = "0%";
+              end = "100%";
+              content = {
+                type = "mdraid";
+                name = "snapshot";
+              };
+            };
+          };
+        };
+      };
+    };
+    mdadm = {
+      snapshot = {
+        type = "mdadm";
+        level = 0;
+        content = {
+          type = "gpt";
+          partitions = {
+            snapshot = {
+              start = "0%";
+              end = "100%";
+              content = {
+                type = "filesystem";
+                # Overwirte the existing filesystem
+                extraArgs = [ "-f" ];
+                format = "xfs";
+                mountpoint = "/mnt/snapshot";
+                mountOptions = [ "defaults" ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
