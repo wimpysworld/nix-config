115f304d0b2c0cf42103fabdfcc834a156aaf7b6
Martin Wimpress <code@wimpress.io>
2026-04-09 14:26:23 +0100

    feat: replace Joplin with Notesnook
    Signed-off-by: Martin Wimpress <code@wimpress.io>

--- home-manager/_mixins/desktop/apps/notes/default.nix
+++ home-manager/_mixins/desktop/apps/notes/default.nix
@@ -10,30 +10,9 @@ let
 in
 lib.mkIf (noughtyLib.isUser [ "martin" ] && host.is.workstation) {
   home = {
-    file = {
-      # Customised Catppuccin Mocha Blue theme for Joplin
-      # - https://github.com/catppuccin/joplin
-      # - https://joplinapp.org/help/apps/custom_css
-      "${config.xdg.configHome}/joplin-desktop/userchrome.css".text = builtins.readFile ./userchrome.css;
-      "${config.xdg.configHome}/joplin-desktop/userstyle.css".text = builtins.readFile ./userstyle.css;
-    };
-    packages = lib.optionals host.is.workstation [ pkgs.heynote ];
-  };
-
-  programs.joplin-desktop = {
-    enable = true;
-    extraConfig = {
-      "markdown.plugin.sub" = true;
-      "markdown.plugin.sup" = true;
-      "revisionService.ttlDays" = 180;
-      "style.editor.fontFamily" = "Work Sans";
-      "style.editor.fontSize" = 16;
-      "style.editor.monospaceFontFamily" = "FiraCode Nerd Font Mono";
-      "theme" = 7;
-    };
-    sync = {
-      interval = "1h";
-      target = "dropbox";
-    };
+    packages = [
+      pkgs.heynote
+      pkgs.unstable.notesnook
+    ];
   };
 }
