f23644d66057fd74dec5f509f44f2087a4c36c1a
Martin Wimpress <code@wimpress.io>
2026-04-13 13:56:28 +0100

    chore(inference): pin ollama and llama.cpp overlays
    - Import the unstable package definitions into local overlays
    - Pin upstream versions and dependency hashes for both packages
    - Add freshener jobs to update the overlay metadata automatically
    
    Signed-off-by: Martin Wimpress <code@wimpress.io>

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,18 @@
 # This file defines overlays
 { inputs, ... }:
+let
+  unstablePkgsPath = inputs.nixpkgs-unstable.outPath + "/pkgs/by-name";
+
+  # Pinned here for the freshener workflow.
+  ollamaVersion = "0.20.6";
+  ollamaHash = "sha256-ol+LsKRxOR37Rpwc9/NouwMg0GOpaeh6zmN5quIBgnA=";
+  ollamaVendorHash = "sha256-Lc1Ktdqtv2VhJQssk8K1UOimeEjVNvDWePE9WkamCos=";
+
+  # Pinned here for the freshener workflow.
+  llamaCppVersion = "8775";
+  llamaCppHash = "sha256-oxQUwXgsr8dDY7wqNxdn7tKyGBowjxLO/xJfdb0J9dk=";
+  llamaCppNpmDepsHash = "sha256-RAFtsbBGBjteCt5yXhrmHL39rIDJMCFBETgzId2eRRk=";
+in
 {
   # This one brings our custom packages from the 'pkgs' directory
   localPackages = final: _prev: import ../pkgs final;
@@ -7,108 +20,218 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
-  modifiedPackages = final: prev: {
-    inherit (final.unstable) llama-cpp llama-cpp-rocm llama-cpp-vulkan ollama ollama-cuda ollama-rocm ollama-vulkan;
-    # Override Python packages to fix Darwin-specific issues
-    python3 = prev.python3.override {
-      packageOverrides = _pyfinal: pyprev: {
-        # Fix setproctitle test failures on Darwin with multiprocessing fork
-        # See: https://github.com/NixOS/nixpkgs/issues/479313
-        setproctitle = pyprev.setproctitle.overridePythonAttrs (old: {
-          disabledTests =
-            (old.disabledTests or [ ])
-            ++ prev.lib.optionals prev.stdenv.isDarwin [
-              "test_fork_segfault"
-              "test_thread_fork_segfault"
-            ];
-        });
-      };
-    };
+  modifiedPackages =
+    final: prev:
+    let
+      ollamaPackageFile = unstablePkgsPath + "/ol/ollama/package.nix";
+      llamaCppPackageFile = unstablePkgsPath + "/ll/llama-cpp/package.nix";
 
-    python313 = prev.python313.override {
-      packageOverrides = _pyfinal: pyprev: {
-        # Fix setproctitle test failures on Darwin with multiprocessing fork
-        # See: https://github.com/NixOS/nixpkgs/issues/479313
-        setproctitle = pyprev.setproctitle.overridePythonAttrs (old: {
-          disabledTests =
-            (old.disabledTests or [ ])
-            ++ prev.lib.optionals prev.stdenv.isDarwin [
-              "test_fork_segfault"
-              "test_thread_fork_segfault"
-            ];
-        });
+      ollamaSrc = prev.fetchFromGitHub {
+        owner = "ollama";
+        repo = "ollama";
+        tag = "v${ollamaVersion}";
+        hash = ollamaHash;
       };
-    };
 
-    # Fix inetutils build failure on Darwin with clang 21
-    # gnulib's error() macro passes dgettext() results as format strings,
-    # triggering -Werror,-Wformat-security in openat-die.c
-    # See: https://github.com/NixOS/nixpkgs/issues/488689
-    inetutils = prev.inetutils.overrideAttrs (
-      old:
-      prev.lib.optionalAttrs prev.stdenv.isDarwin {
-        env = (old.env or { }) // {
-          NIX_CFLAGS_COMPILE = toString [
-            (old.env.NIX_CFLAGS_COMPILE or "")
-            "-Wno-format-security"
-          ];
+      llamaCppSrc = prev.fetchFromGitHub {
+        owner = "ggml-org";
+        repo = "llama.cpp";
+        tag = "b${llamaCppVersion}";
+        hash = llamaCppHash;
+        leaveDotGit = true;
+        postFetch = ''
+          git -C "$out" rev-parse --short HEAD > "$out/COMMIT"
+          find "$out" -name .git -print0 | xargs -0 rm -rf
+        '';
+      };
+    in
+    rec {
+      ollama =
+        (final.callPackage ollamaPackageFile {
+          acceleration = null;
+          inherit
+            ollama
+            ollama-cuda
+            ollama-rocm
+            ollama-vulkan
+            ;
+        }).overrideAttrs
+          (_old: {
+            version = ollamaVersion;
+            src = ollamaSrc;
+            vendorHash = ollamaVendorHash;
+          });
+
+      ollama-cuda =
+        (final.callPackage ollamaPackageFile {
+          acceleration = "cuda";
+          inherit
+            ollama
+            ollama-cuda
+            ollama-rocm
+            ollama-vulkan
+            ;
+        }).overrideAttrs
+          (_old: {
+            version = ollamaVersion;
+            src = ollamaSrc;
+            vendorHash = ollamaVendorHash;
+          });
+
+      ollama-rocm =
+        (final.callPackage ollamaPackageFile {
+          acceleration = "rocm";
+          inherit
+            ollama
+            ollama-cuda
+            ollama-rocm
+            ollama-vulkan
+            ;
+        }).overrideAttrs
+          (_old: {
+            version = ollamaVersion;
+            src = ollamaSrc;
+            vendorHash = ollamaVendorHash;
+          });
+
+      ollama-vulkan =
+        (final.callPackage ollamaPackageFile {
+          acceleration = "vulkan";
+          inherit
+            ollama
+            ollama-cuda
+            ollama-rocm
+            ollama-vulkan
+            ;
+        }).overrideAttrs
+          (_old: {
+            version = ollamaVersion;
+            src = ollamaSrc;
+            vendorHash = ollamaVendorHash;
+          });
+
+      llama-cpp =
+        (final.callPackage llamaCppPackageFile {
+          inherit llama-cpp;
+        }).overrideAttrs
+          (old: {
+            version = llamaCppVersion;
+            src = llamaCppSrc;
+            npmDepsHash = llamaCppNpmDepsHash;
+            npmDeps = prev.fetchNpmDeps {
+              name = "llama-cpp-${llamaCppVersion}-npm-deps";
+              inherit (old) npmRoot patches;
+              src = llamaCppSrc;
+              preBuild = ''
+                pushd ${old.npmRoot}
+              '';
+              hash = llamaCppNpmDepsHash;
+            };
+          });
+
+      llama-cpp-rocm = llama-cpp.override { rocmSupport = true; };
+      llama-cpp-vulkan = llama-cpp.override { vulkanSupport = true; };
+
+      # Override Python packages to fix Darwin-specific issues
+      python3 = prev.python3.override {
+        packageOverrides = _pyfinal: pyprev: {
+          # Fix setproctitle test failures on Darwin with multiprocessing fork
+          # See: https://github.com/NixOS/nixpkgs/issues/479313
+          setproctitle = pyprev.setproctitle.overridePythonAttrs (old: {
+            disabledTests =
+              (old.disabledTests or [ ])
+              ++ prev.lib.optionals prev.stdenv.isDarwin [
+                "test_fork_segfault"
+                "test_thread_fork_segfault"
+              ];
+          });
         };
-      }
-    );
+      };
 
-    linuxPackages_6_12 = prev.linuxPackages_6_12.extend (
-      _lpself: lpsuper: {
-        mwprocapture = lpsuper.mwprocapture.overrideAttrs (_old: rec {
-          pname = "mwprocapture";
-          subVersion = "4420";
-          version = "1.3.${subVersion}";
-          src = prev.fetchurl {
-            url = "http://www.magewell.com/files/support/ProCaptureForLinux_${version}.tar.gz";
-            sha256 = "sha256-aX8vhousQQ48QPgfLjESGbBw26egDB46AmSkruUaM5g=";
-          };
-        });
-      }
-    );
+      python313 = prev.python313.override {
+        packageOverrides = _pyfinal: pyprev: {
+          # Fix setproctitle test failures on Darwin with multiprocessing fork
+          # See: https://github.com/NixOS/nixpkgs/issues/479313
+          setproctitle = pyprev.setproctitle.overridePythonAttrs (old: {
+            disabledTests =
+              (old.disabledTests or [ ])
+              ++ prev.lib.optionals prev.stdenv.isDarwin [
+                "test_fork_segfault"
+                "test_thread_fork_segfault"
+              ];
+          });
+        };
+      };
 
-    linuxPackages = prev.linuxPackages.extend (
-      _lpself: lpsuper: {
-        mwprocapture = lpsuper.mwprocapture.overrideAttrs (_old: rec {
-          pname = "mwprocapture";
-          subVersion = "4429";
-          version = "1.3.${subVersion}";
-          src = prev.fetchurl {
-            url = "https://www.magewell.com/files/drivers/ProCaptureForLinux_${version}.tar.gz";
-            sha256 = "sha256-sYwMVEAvYMKCn4DKQiCtnTxd1chMUd0atgswpC+CZ5g=";
+      # Fix inetutils build failure on Darwin with clang 21
+      # gnulib's error() macro passes dgettext() results as format strings,
+      # triggering -Werror,-Wformat-security in openat-die.c
+      # See: https://github.com/NixOS/nixpkgs/issues/488689
+      inetutils = prev.inetutils.overrideAttrs (
+        old:
+        prev.lib.optionalAttrs prev.stdenv.isDarwin {
+          env = (old.env or { }) // {
+            NIX_CFLAGS_COMPILE = toString [
+              (old.env.NIX_CFLAGS_COMPILE or "")
+              "-Wno-format-security"
+            ];
           };
-        });
-      }
-    );
+        }
+      );
 
-    linuxPackages_latest = prev.linuxPackages_latest.extend (
-      _lpself: lpsuper: {
-        mwprocapture = lpsuper.mwprocapture.overrideAttrs (old: rec {
-          pname = "mwprocapture";
-          subVersion = "4429";
-          version = "1.3.${subVersion}";
-          src = prev.fetchurl {
-            url = "https://www.magewell.com/files/drivers/ProCaptureForLinux_${version}.tar.gz";
-            sha256 = "sha256-sYwMVEAvYMKCn4DKQiCtnTxd1chMUd0atgswpC+CZ5g=";
-          };
-          meta = old.meta // {
-            broken = false;
-          };
-        });
-      }
-    );
+      linuxPackages_6_12 = prev.linuxPackages_6_12.extend (
+        _lpself: lpsuper: {
+          mwprocapture = lpsuper.mwprocapture.overrideAttrs (_old: rec {
+            pname = "mwprocapture";
+            subVersion = "4420";
+            version = "1.3.${subVersion}";
+            src = prev.fetchurl {
+              url = "http://www.magewell.com/files/support/ProCaptureForLinux_${version}.tar.gz";
+              sha256 = "sha256-aX8vhousQQ48QPgfLjESGbBw26egDB46AmSkruUaM5g=";
+            };
+          });
+        }
+      );
 
-    # Override rofi-unwrapped to remove desktop entries (this is where they come from!)
-    rofi-unwrapped = prev.rofi-unwrapped.overrideAttrs (oldAttrs: {
-      postInstall = (oldAttrs.postInstall or "") + ''
-        rm -f $out/share/applications/rofi.desktop
-        rm -f $out/share/applications/rofi-theme-selector.desktop
-      '';
-    });
-  };
+      linuxPackages = prev.linuxPackages.extend (
+        _lpself: lpsuper: {
+          mwprocapture = lpsuper.mwprocapture.overrideAttrs (_old: rec {
+            pname = "mwprocapture";
+            subVersion = "4429";
+            version = "1.3.${subVersion}";
+            src = prev.fetchurl {
+              url = "https://www.magewell.com/files/drivers/ProCaptureForLinux_${version}.tar.gz";
+              sha256 = "sha256-sYwMVEAvYMKCn4DKQiCtnTxd1chMUd0atgswpC+CZ5g=";
+            };
+          });
+        }
+      );
+
+      linuxPackages_latest = prev.linuxPackages_latest.extend (
+        _lpself: lpsuper: {
+          mwprocapture = lpsuper.mwprocapture.overrideAttrs (old: rec {
+            pname = "mwprocapture";
+            subVersion = "4429";
+            version = "1.3.${subVersion}";
+            src = prev.fetchurl {
+              url = "https://www.magewell.com/files/drivers/ProCaptureForLinux_${version}.tar.gz";
+              sha256 = "sha256-sYwMVEAvYMKCn4DKQiCtnTxd1chMUd0atgswpC+CZ5g=";
+            };
+            meta = old.meta // {
+              broken = false;
+            };
+          });
+        }
+      );
+
+      # Override rofi-unwrapped to remove desktop entries (this is where they come from!)
+      rofi-unwrapped = prev.rofi-unwrapped.overrideAttrs (oldAttrs: {
+        postInstall = (oldAttrs.postInstall or "") + ''
+          rm -f $out/share/applications/rofi.desktop
+          rm -f $out/share/applications/rofi-theme-selector.desktop
+        '';
+      });
+    };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
