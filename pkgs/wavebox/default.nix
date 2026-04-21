4374bdc38c70d3934e719d470088f64519c65685
Martin Wimpress <code@wimpress.io>
2026-04-21 18:01:44 +0100

    fix(wavebox): disable GPU in the wrapper
    Signed-off-by: Martin Wimpress <code@wimpress.io>

--- pkgs/wavebox/default.nix
+++ pkgs/wavebox/default.nix
@@ -247,6 +247,7 @@ let
         --prefix XDG_DATA_DIRS   : "$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH:${addDriverRunpath.driverLink}/share" \
         --set CHROME_WRAPPER "wavebox" \
         --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
+        --add-flags "--disable-gpu" \
         --add-flags "--simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT'" \
         --add-flags ${lib.escapeShellArg commandLineArgs}
 
