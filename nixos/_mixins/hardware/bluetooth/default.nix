8358ddaf66dca0f25906b58183d50f95a0d60357
Martin Wimpress <code@wimpress.io>
2026-05-02 11:05:10 +0100

    fix(bluetooth): hide Bluetui desktop entry at package level
    Signed-off-by: Martin Wimpress <code@wimpress.io>

--- nixos/_mixins/hardware/bluetooth/default.nix
+++ nixos/_mixins/hardware/bluetooth/default.nix
@@ -6,19 +6,33 @@
 }:
 let
   inherit (config.noughty) host;
+  bluetuiDesktopEntry = pkgs.writeText "bluetui.desktop" ''
+    [Desktop Entry]
+    Name=Bluetui
+    GenericName=Bluetooth Manager
+    Comment=Manage bluetooth devices
+    Exec=bluetui
+    Terminal=true
+    Type=Application
+    Keywords=bluetooth
+    Categories=Utility;Settings;ConsoleOnly
+    StartupNotify=false
+    NoDisplay=true
+  '';
+  bluetui = pkgs.symlinkJoin {
+    name = "bluetui-hidden";
+    paths = [ pkgs.bluetui ];
+    postBuild = ''
+      rm -f $out/share/applications/bluetui.desktop
+      install -Dm444 ${bluetuiDesktopEntry} $out/share/applications/bluetui.desktop
+    '';
+  };
 in
 {
   environment = {
     systemPackages = lib.optionals (!host.is.iso) [
-      pkgs.bluetui
+      bluetui
     ];
-    # Hide the bluetui desktop entry from application launchers; it's a TUI
-    # tool intended to be launched from a terminal, not a launcher.
-    etc."xdg/applications/bluetui.desktop".text = ''
-      [Desktop Entry]
-      Name=Bluetui
-      NoDisplay=true
-    '';
   };
   hardware = {
     # https://nixos.wiki/wiki/Bluetooth
