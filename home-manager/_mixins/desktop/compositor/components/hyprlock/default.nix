dabe1b6171067b6ee2d052bd535d7d04cf8af01c
Martin Wimpress <code@wimpress.io>
2026-05-12 09:41:49 -0400

    fix(hyprlock): handle laptop lid close without crashing
    - Make the immediate lock command idempotent across Hypridle paths.
    - Avoid pinning Hyprlock UI elements to a monitor on laptops.
    - Keep primary-output placement for non-laptop systems.
    
    Signed-off-by: Martin Wimpress <code@wimpress.io>

--- home-manager/_mixins/desktop/compositor/components/hyprlock/default.nix
+++ home-manager/_mixins/desktop/compositor/components/hyprlock/default.nix
@@ -31,6 +31,8 @@ let
       "0, -316";
   catResolution = toString display.primaryWidth;
   monitor = display.primaryOutput;
+  onPrimaryOutput = lib.optionalAttrs (!host.is.laptop) { inherit monitor; };
+  lockCmd = "${lib.getExe' pkgs.procps "pgrep"} -xu \"$USER\" hyprlock >/dev/null || ${lib.getExe pkgs.hyprlock} --immediate";
   unlockGlyph = if noughtyLib.hostHasTag "fprintd" then "󰈷" else "󰌋";
 in
 lib.mkIf (host.is.linux && host.is.workstation) {
@@ -64,171 +66,195 @@ lib.mkIf (host.is.linux && host.is.workstation) {
           }
         ];
         image = [
-          {
-            # Avatar
-            inherit monitor;
-            path = "$HOME/.face";
-            border_size = 2;
-            border_color = mkRgba "blue" "0.7";
-            size = 140;
-            rounding = -1;
-            rotate = 0;
-            reload_time = -1;
-            position = "0, 40";
-            halign = "center";
-            valign = "center";
-          }
-          {
-            # Catppuccin
-            inherit monitor;
-            path = "/etc/backgrounds/Cat-${catResolution}px.png";
-            border_size = 0;
-            rounding = 0;
-            rotate = 0;
-            reload_time = -1;
-            size = catSize;
-            position = catPosition;
-            halign = "left";
-            valign = "center";
-          }
+          (
+            onPrimaryOutput
+            // {
+              # Avatar
+              path = "$HOME/.face";
+              border_size = 2;
+              border_color = mkRgba "blue" "0.7";
+              size = 140;
+              rounding = -1;
+              rotate = 0;
+              reload_time = -1;
+              position = "0, 40";
+              halign = "center";
+              valign = "center";
+            }
+          )
+          (
+            onPrimaryOutput
+            // {
+              # Catppuccin
+              path = "/etc/backgrounds/Cat-${catResolution}px.png";
+              border_size = 0;
+              rounding = 0;
+              rotate = 0;
+              reload_time = -1;
+              size = catSize;
+              position = catPosition;
+              halign = "left";
+              valign = "center";
+            }
+          )
         ];
         label = [
-          {
-            # Date (1 hour)
-            inherit monitor;
-            text = ''cmd[update:3600000] echo -e "$(date +"%a, %d %b")"'';
-            color = mkRgba "text" "0.9";
-            font_size = 25;
-            font_family = "Work Sans Bold";
-            position = "0, 440";
-            halign = "center";
-            valign = "center";
-          }
-          {
-            # Time Border left
-            inherit monitor;
-            text = "$TIME";
-            color = mkRgba "crust" "0.8";
-            font_size = 120;
-            font_family = "FiraCode Nerd Font Mono Bold";
-            position = "-4, 250";
-            halign = "center";
-            valign = "center";
-            zindex = 0;
-          }
-          {
-            # Time Border right
-            inherit monitor;
-            text = "$TIME";
-            color = mkRgba "crust" "0.8";
-            font_size = 120;
-            font_family = "FiraCode Nerd Font Mono Bold";
-            position = "4, 250";
-            halign = "center";
-            valign = "center";
-            zindex = 0;
-          }
-          {
-            # Time Border up
-            inherit monitor;
-            text = "$TIME";
-            color = mkRgba "crust" "0.8";
-            font_size = 120;
-            font_family = "FiraCode Nerd Font Mono Bold";
-            position = "0, 246";
-            halign = "center";
-            valign = "center";
-            zindex = 0;
-          }
-          {
-            # Time Border down
-            inherit monitor;
-            text = "$TIME";
-            color = mkRgba "crust" "0.8";
-            font_size = 120;
-            font_family = "FiraCode Nerd Font Mono Bold";
-            position = "0, 254";
-            halign = "center";
-            valign = "center";
-            zindex = 0;
-          }
-          {
-            # Time
-            inherit monitor;
-            text = "$TIME";
-            color = mkRgba "text" "0.9";
-            font_size = 120;
-            font_family = "FiraCode Nerd Font Mono Bold";
-            position = "0, 250";
-            halign = "center";
-            valign = "center";
-            zindex = 1;
-          }
-          {
-            # Username
-            inherit monitor;
-            text = ''<span foreground="${mkPangoHex "green"}">󰝴</span> $DESC'';
-            color = mkRgba "overlay2" "1.0";
-            font_size = 18;
-            font_family = "FiraCode Nerd Font Propo";
-            position = "0, -130";
-            halign = "center";
-            valign = "center";
-          }
-          {
-            # Fingerprint prompt (only rendered when fprintd sends a message)
-            inherit monitor;
-            text = "$FPRINTPROMPT";
-            color = mkRgba "green" "0.9";
-            font_size = 14;
-            font_family = "Work Sans";
-            position = "0, -280";
-            halign = "center";
-            valign = "center";
-          }
+          (
+            onPrimaryOutput
+            // {
+              # Date (1 hour)
+              text = ''cmd[update:3600000] echo -e "$(date +"%a, %d %b")"'';
+              color = mkRgba "text" "0.9";
+              font_size = 25;
+              font_family = "Work Sans Bold";
+              position = "0, 440";
+              halign = "center";
+              valign = "center";
+            }
+          )
+          (
+            onPrimaryOutput
+            // {
+              # Time Border left
+              text = "$TIME";
+              color = mkRgba "crust" "0.8";
+              font_size = 120;
+              font_family = "FiraCode Nerd Font Mono Bold";
+              position = "-4, 250";
+              halign = "center";
+              valign = "center";
+              zindex = 0;
+            }
+          )
+          (
+            onPrimaryOutput
+            // {
+              # Time Border right
+              text = "$TIME";
+              color = mkRgba "crust" "0.8";
+              font_size = 120;
+              font_family = "FiraCode Nerd Font Mono Bold";
+              position = "4, 250";
+              halign = "center";
+              valign = "center";
+              zindex = 0;
+            }
+          )
+          (
+            onPrimaryOutput
+            // {
+              # Time Border up
+              text = "$TIME";
+              color = mkRgba "crust" "0.8";
+              font_size = 120;
+              font_family = "FiraCode Nerd Font Mono Bold";
+              position = "0, 246";
+              halign = "center";
+              valign = "center";
+              zindex = 0;
+            }
+          )
+          (
+            onPrimaryOutput
+            // {
+              # Time Border down
+              text = "$TIME";
+              color = mkRgba "crust" "0.8";
+              font_size = 120;
+              font_family = "FiraCode Nerd Font Mono Bold";
+              position = "0, 254";
+              halign = "center";
+              valign = "center";
+              zindex = 0;
+            }
+          )
+          (
+            onPrimaryOutput
+            // {
+              # Time
+              text = "$TIME";
+              color = mkRgba "text" "0.9";
+              font_size = 120;
+              font_family = "FiraCode Nerd Font Mono Bold";
+              position = "0, 250";
+              halign = "center";
+              valign = "center";
+              zindex = 1;
+            }
+          )
+          (
+            onPrimaryOutput
+            // {
+              # Username
+              text = ''<span foreground="${mkPangoHex "green"}">󰝴</span> $DESC'';
+              color = mkRgba "overlay2" "1.0";
+              font_size = 18;
+              font_family = "FiraCode Nerd Font Propo";
+              position = "0, -130";
+              halign = "center";
+              valign = "center";
+            }
+          )
+          (
+            onPrimaryOutput
+            // {
+              # Fingerprint prompt (only rendered when fprintd sends a message)
+              text = "$FPRINTPROMPT";
+              color = mkRgba "green" "0.9";
+              font_size = 14;
+              font_family = "Work Sans";
+              position = "0, -280";
+              halign = "center";
+              valign = "center";
+            }
+          )
         ];
         # Username box
         shape = [
-          {
-            inherit monitor;
-            size = "420, 60";
-            position = "0, -130";
-            color = mkRgba "surface1" "1.0";
-            rounding = 8;
-            border_size = 2;
-            border_color = mkRgba "surface0" "1.0";
-            rotate = 0;
-            xray = false; # do not make a "hole" in the background
-            halign = "center";
-            valign = "center";
-          }
+          (
+            onPrimaryOutput
+            // {
+              size = "420, 60";
+              position = "0, -130";
+              color = mkRgba "surface1" "1.0";
+              rounding = 8;
+              border_size = 2;
+              border_color = mkRgba "surface0" "1.0";
+              rotate = 0;
+              xray = false; # do not make a "hole" in the background
+              halign = "center";
+              valign = "center";
+            }
+          )
         ];
         # Password
         input-field = [
-          {
-            inherit monitor;
-            size = "420, 60";
-            position = "0, -210";
-            outline_thickness = 2;
-            dots_size = 0.35;
-            dots_spacing = 0.25;
-            dots_center = true;
-            fade_on_empty = false;
-            placeholder_text = ''<span foreground="${mkPangoHex "yellow"}"><big>  ${unlockGlyph}  </big></span>'';
-            fail_text = ''<span foreground="${mkPangoHex "red"}">󰀧</span>  <i>$FAIL</i> <span foreground="${mkPangoHex "red"}"><b>($ATTEMPTS)</b></span>'';
-            fail_timeout = 3000; # milliseconds before fail_text and fail_color disappears
-            fail_transition = 500; # transition time in ms between normal outer_color and fail_color
-            hide_input = false;
-            halign = "center";
-            valign = "center";
-            rounding = 8;
-            outer_color = mkRgba "blue" "1.0";
-            inner_color = mkRgba "surface2" "1.0";
-            font_color = mkRgba "text" "1.0";
-            capslock_color = mkRgba "peach" "1.0";
-            check_color = mkRgba "sapphire" "1.0";
-            fail_color = mkRgba "pink" "1.0";
-          }
+          (
+            onPrimaryOutput
+            // {
+              size = "420, 60";
+              position = "0, -210";
+              outline_thickness = 2;
+              dots_size = 0.35;
+              dots_spacing = 0.25;
+              dots_center = true;
+              fade_on_empty = false;
+              placeholder_text = ''<span foreground="${mkPangoHex "yellow"}"><big>  ${unlockGlyph}  </big></span>'';
+              fail_text = ''<span foreground="${mkPangoHex "red"}">󰀧</span>  <i>$FAIL</i> <span foreground="${mkPangoHex "red"}"><b>($ATTEMPTS)</b></span>'';
+              fail_timeout = 3000; # milliseconds before fail_text and fail_color disappears
+              fail_transition = 500; # transition time in ms between normal outer_color and fail_color
+              hide_input = false;
+              halign = "center";
+              valign = "center";
+              rounding = 8;
+              outer_color = mkRgba "blue" "1.0";
+              inner_color = mkRgba "surface2" "1.0";
+              font_color = mkRgba "text" "1.0";
+              capslock_color = mkRgba "peach" "1.0";
+              check_color = mkRgba "sapphire" "1.0";
+              fail_color = mkRgba "pink" "1.0";
+            }
+          )
         ];
       };
     };
@@ -238,13 +264,13 @@ lib.mkIf (host.is.linux && host.is.workstation) {
       enable = true;
       settings = {
         general = {
-          lock_cmd = "${lib.getExe pkgs.hyprlock}";
-          before_sleep_cmd = "${lib.getExe pkgs.hyprlock}";
+          lock_cmd = lockCmd;
+          before_sleep_cmd = lockCmd;
         };
         listener = [
           {
             timeout = 300;
-            on-timeout = "${lib.getExe pkgs.hyprlock}";
+            on-timeout = lockCmd;
           }
           {
             timeout = 305;
