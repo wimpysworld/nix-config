1182512dafd34c33df8d238a71ef800b0c5e0636
Traya <traya@darth.cc>
2026-05-22 03:25:57 +0000

    feat: add Catppuccin themes for Fresh

--- home-manager/_mixins/development/fresh/default.nix
+++ home-manager/_mixins/development/fresh/default.nix
@@ -8,9 +8,29 @@
 let
   inherit (config.noughty) host;
   inherit (pkgs.stdenv.hostPlatform) system;
+
+  catppuccinFresh = pkgs.fetchFromGitHub {
+    owner = "milon";
+    repo = "catppuccin-fresh";
+    rev = "58093e748cbf90e742c913f469fb20dd6aacba2b";
+    hash = "sha256-C98XNiUtT0/cAbGzjaX+i1vlnFLp1Pf2UpEmk4Qsuoc=";
+  };
 in
 lib.mkIf host.is.workstation {
   home.packages = [
     inputs.fresh.packages.${system}.fresh
   ];
+
+  xdg.configFile = {
+    "fresh/config.json".text = lib.mkDefault (
+      builtins.toJSON {
+        theme = "catppuccin-mocha.json";
+      }
+    );
+
+    "fresh/themes/catppuccin-frappe.json".source = "${catppuccinFresh}/catppuccin-frappe.json";
+    "fresh/themes/catppuccin-latte.json".source = "${catppuccinFresh}/catppuccin-latte.json";
+    "fresh/themes/catppuccin-macchiato.json".source = "${catppuccinFresh}/catppuccin-macchiato.json";
+    "fresh/themes/catppuccin-mocha.json".source = "${catppuccinFresh}/catppuccin-mocha.json";
+  };
 }
