5bab76fc9e3a117556b762fe339280f3fc8a7343
Martin Wimpress <code@wimpress.io>
2026-07-17 15:49:18 +0100

    fix(agentic): detect Determinate Nix shared binary

--- home-manager/_mixins/agentic/fence/package.nix
+++ home-manager/_mixins/agentic/fence/package.nix
@@ -1,2 +1,7 @@
 { inputs, pkgs }:
-inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.fence
+inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.fence.overrideAttrs (oldAttrs: {
+  postPatch = (oldAttrs.postPatch or "") + ''
+    substituteInPlace internal/sandbox/runtime_exec_deny.go \
+      --replace-fail $'\t"xargs",' $'\t"xargs",\n\t// Determinate Nix exposes legacy commands as aliases of the nix binary.\n\t"nix",'
+  '';
+})
