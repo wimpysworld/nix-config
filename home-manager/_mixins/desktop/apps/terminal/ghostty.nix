e651df7c50630f0354db708b0ea23a05fa82e5c6
Martin Wimpress <code@wimpress.io>
2026-08-18 23:13:47 +0100

    feat(ghostty): add cursor trail shaders

--- home-manager/_mixins/desktop/apps/terminal/ghostty.nix
+++ home-manager/_mixins/desktop/apps/terminal/ghostty.nix
@@ -7,6 +7,13 @@
 }:
 let
   inherit (config.noughty) host;
+  cursorTrailShader = "tinkle"; # Valid values: "boo", "tinkle", and "wisp".
+  cursorTrailShaders = pkgs.fetchFromGitHub {
+    owner = "hced";
+    repo = "ghostty-cursor-trails";
+    rev = "78f597cf66427bc382077e5e33f26981a86bb207";
+    hash = "sha256-NHeCd/avyJ8SaYW8pYWcetwVroFQNokN7saiWCMu3TM=";
+  };
   hideWindowDecorations =
     if config.wayland.windowManager.wayfire.enable then
       false
@@ -23,6 +30,12 @@ lib.mkIf
   {
     catppuccin.ghostty.enable = config.programs.ghostty.enable;
 
+    xdg.configFile = {
+      "ghostty/shaders/boo-cursor.glsl".source = "${cursorTrailShaders}/boo-cursor.glsl";
+      "ghostty/shaders/tinkle-cursor.glsl".source = "${cursorTrailShaders}/tinkle-cursor.glsl";
+      "ghostty/shaders/wisp-cursor.glsl".source = "${cursorTrailShaders}/wisp-cursor.glsl";
+    };
+
     programs.ghostty = {
       enable = true;
       enableBashIntegration = false;
@@ -34,6 +47,8 @@ lib.mkIf
       package = if host.is.linux then pkgs.ghostty else null;
 
       settings = {
+        custom-shader = "shaders/${cursorTrailShader}-cursor.glsl";
+        custom-shader-animation = "always";
         cursor-style = "block";
         cursor-style-blink = true;
         font-family = "FiraCode Nerd Font Mono";
